4b23ba84e3205bf9eadf2dd144ef26978ee587aa
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2011-08-12 10:20:57 +0000

    Update for SX220 & IXUS310 (may be needed for SX230 as well). These cameras need a delay added in capt_seq_hook_raw_here to allow the image file_counter to be updated consistently before calling raw_savefile. Delay is only added if PAUSE_FOR_FILE_COUNTER is defined, and delay length can be adjusted for each camera if needed.

--- platform/generic/capt_seq.c
+++ platform/generic/capt_seq.c
@@ -19,6 +19,14 @@ void __attribute__((naked,noinline)) capt_seq_hook_raw_here()
 {
  asm volatile("STMFD   SP!, {R0-R12,LR}\n");
 
+#ifdef PAUSE_FOR_FILE_COUNTER
+    // Some cameras need a slight delay for the file counter to be updated correctly
+    // before raw_savefile tries to get the file name & directory.
+    // Add '#define PAUSE_FOR_FILE_COUNTER 100' in the camera firmware capt_seq.c file.
+    // The value can be adjusted as needed for different cameras.
+    _SleepTask(PAUSE_FOR_FILE_COUNTER);
+#endif
+
     raw_save_stage = RAWDATA_AVAILABLE;
     core_rawdata_available();
     while (raw_save_stage != RAWDATA_SAVED){
