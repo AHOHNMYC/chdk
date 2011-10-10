c5aeb4e867e0b00478fc39a5b5350e4c23f661c3
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2011-10-10 10:37:50 +0000

    Some more cleanup of the Makefile batch build: - removed some test entries from camera_list.csv - changed auto_build.sh to stop on build errors - fixed up the dummy empty recipes in the makefiles to stop gmake from recursively trying to rebuild the makefiles themselves. this speeds up the build process. - fixed handling or PLATFORM/PLATFORMSUB error message to that the batch targets can be specified without these; but the default target will produce an error if either is undefined.

--- branches/philmoz/auto_build.sh
+++ branches/philmoz/auto_build.sh
@@ -8,6 +8,6 @@ while IFS=, read cam fw state copy skip
 do
   if [ ${cam} != "CAMERA" ] && [ "${skip}" = "" ]; then \
     if [ "${state}" != "" ]; then state=_${state}; fi; \
-    $1 -s --no-print-directory PLATFORM=${cam} PLATFORMSUB=${fw} STATE=${state} COPY_TO=${copy} $2; \
+    $1 -s --no-print-directory PLATFORM=${cam} PLATFORMSUB=${fw} STATE=${state} COPY_TO=${copy} $2 || exit 1; \
   fi;
 done < $3
