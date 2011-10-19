48c4a87d35067729575a11fe1be86318284c3874
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2011-10-19 08:41:22 +0000

    A3000 port from ERR99 - http://chdk.setepontos.com/index.php?topic=650.msg74533#msg74533 Firmware versions 1.00b, 1.00c and 1.00d.

--- trunk/core/gui.c
+++ trunk/core/gui.c
@@ -2191,7 +2191,7 @@ void other_kbd_process(){
      get_property_case(PROPCASE_DIGITAL_ZOOM_STATE, &x, sizeof(x));
      if (x) {
       get_property_case(PROPCASE_DIGITAL_ZOOM_POSITION, &x, sizeof(x));
-#if defined (CAMERA_s90) || defined (CAMERA_s95) || defined (CAMERA_g12)
+#if defined (CAMERA_s90) || defined (CAMERA_s95) || defined (CAMERA_g12) || defined (CAMERA_a3000)
 	  if (x==0) zoom_status=ZOOM_OPTICAL_MAX; //ERR99: No zoom back from digital to optical zoom possible if set to medium
 #else
 	  if (x==0) zoom_status=ZOOM_OPTICAL_MEDIUM;
