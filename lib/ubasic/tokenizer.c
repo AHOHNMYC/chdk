d83a60db0595b71a8f48d7fb46aad1e13d9a0780
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2011-11-24 06:11:56 +0000

    add script control of ubasic scheduling, based on code by tsvstar in http://chdk.setepontos.com/index.php?topic=7060.msg75631#msg75631 with modification to make it controllable from script. Adds two new ubasic commands: set_yield_lines N - run up to N lines in each physw_task iteration. Default is 1, matching previous behavior (>=10ms per ubasic line) set_yield_ms N - sets the maximum number of milliseconds to run ubasic before yielding. Default 10. Precision is 10ms, and is only checked once per ubasic line.
    ubasic yields when either limit is exceeded. -1 can be used to set either value to unlimited.

--- lib/ubasic/tokenizer.c
+++ lib/ubasic/tokenizer.c
@@ -188,6 +188,8 @@ static const struct keyword_token keywords[] = {
   {"set_zoom",                TOKENIZER_SET_ZOOM},
   {"set_record",              TOKENIZER_SET_RECORD},
   {"set_config_value",        TOKENIZER_SET_CONFIG_VALUE},
+  {"set_yield_lines",         TOKENIZER_SET_YIELD_LINES},
+  {"set_yield_ms",            TOKENIZER_SET_YIELD_MS},
   
   
   {"wait_click",              TOKENIZER_WAIT_CLICK},
