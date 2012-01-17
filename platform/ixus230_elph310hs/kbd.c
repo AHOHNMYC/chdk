6a375c8ed0fe08f17a8ecc2eb3da5ef4a9e59651
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2012-01-17 03:27:07 +0000

    update the ixus230 for the new USB remote functionality from waterwingz in http://chdk.setepontos.com/index.php?topic=650.msg80036#msg80036

--- platform/ixus230_elph310hs/kbd.c
+++ platform/ixus230_elph310hs/kbd.c
@@ -27,18 +27,15 @@ typedef struct {
 	long canonkey;
 } KeyMap;
 
-long kbd_new_state[3];
-static long kbd_prev_state[3];
-static long kbd_mod_state[3];
+long kbd_new_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+static long kbd_prev_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+static long kbd_mod_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
 
 static long last_kbd_key = 0;
 static long alt_mode_key_mask = 0x00004000; // KEY_VIDEO
-static int usb_power=0;
-static int remote_key, remote_count;
-static int shoot_counter=0;
+
 extern void _GetKbdState(long*);
 
-#define DELAY_TIMEOUT 10000
 
 #define KEYS_MASK0 (0x00004000|0X00000004)
 #define KEYS_MASK1 (0x00000000)
@@ -49,8 +46,18 @@ extern void _GetKbdState(long*);
 #define SD_READONLY_FLAG (0x00020000)
 #define SD_READONLY_REG 2
 
-#define USB_REG 2
+
 #define USB_MASK (0x04000000)
+#define USB_IDX  2
+
+extern void usb_remote_key( int ) ;
+int get_usb_bit() 
+{
+	long usb_physw[3];
+	usb_physw[USB_IDX] = 0;
+	_kbd_read_keys_r2(usb_physw);
+	return(( usb_physw[USB_IDX] & USB_MASK)==USB_MASK) ; 
+}
 
 #ifndef MALLOCD_STACK
 static char kbd_stack[NEW_SS];
@@ -80,128 +87,6 @@ static KeyMap keymap[] = {
 
 
 long __attribute__((naked)) wrap_kbd_p1_f() ;
-
-void wait_until_remote_button_is_released(void)
-{
-int count1;
-int count2;
-int tick,tick2,tick3;
-int nSW;
-int prev_usb_power,cur_usb_power;
- // ------ add by Masuji SUTO (start) --------------
-    static int nMode;
- // ------ add by Masuji SUTO (end)   --------------
-
-asm volatile ("STMFD SP!, {R0-R11,LR}\n"); // store R0-R11 and LR in stack
-debug_led(1);
-tick = get_tick_count();
-tick2 = tick;
-static long usb_physw[3];
-if (conf.synch_enable && conf.ricoh_ca1_mode && conf.remote_enable && (!shooting_get_drive_mode()|| ((shooting_get_drive_mode()==2) && state_shooting_progress != SHOOTING_PROGRESS_PROCESSING)))  // synch mode enable so wait for USB to disconnect
-  {
-// ------ add by Masuji SUTO (start) --------------
-        nMode=0;
-        usb_physw[2] = 0;                                             // makes sure USB bit is cleared.
-        _kbd_read_keys_r2(usb_physw);
-        if((usb_physw[2] & USB_MASK)==USB_MASK) nMode=1;
-// ------ add by Masuji SUTO (end)   --------------
-     if(conf.ricoh_ca1_mode && conf.remote_enable)
-     {
-	if(shooting_get_drive_mode()==1 && state_shooting_progress == SHOOTING_PROGRESS_PROCESSING){			//continuous-shooting mode
-		if(conf.bracket_type>2){
-			if(shoot_counter<2) shutter_int=3;
-			shoot_counter--;
-			}
-		else{
-			prev_usb_power=0;
-			nSW = 0;
-			do
-				{
-				usb_physw[2] = 0;                                             // makes sure USB bit is cleared.
-				_kbd_read_keys_r2(usb_physw);
-				cur_usb_power = (usb_physw[2] & USB_MASK)==USB_MASK;
-				if(cur_usb_power){
-					if(!prev_usb_power){
-						tick2 = get_tick_count();
-						prev_usb_power=cur_usb_power;
-						}
-					else{
-						if((int)get_tick_count()-tick2>1000) {debug_led(0);}
-						}
-					}
-				else{
-					if(prev_usb_power){
-						tick3 = (int)get_tick_count()-tick2;
-						if(nSW==10) {
-							if(tick3>50) shutter_int=1;
-							nSW=20;
-							}
-						if(nSW==0 && tick3>0) {
-							if(tick3<50) {
-							nSW=10;
-							}
-						else{
-							if(tick3>1000) shutter_int=1;
-								nSW=20;
-							}
-						}
-						prev_usb_power=cur_usb_power;
-						}
-					}
-				if((int)get_tick_count()-tick >= DELAY_TIMEOUT) {nSW=20;shutter_int=2;}
-				}
-			 while(nSW<20);
-			 }
-		} 		//continuous-shooting mode
-		else{		//nomal mode
-			shoot_counter=0;
-			if(conf.bracket_type>2){
-				shoot_counter=(conf.bracket_type-2)*2;
-				}
-        do
-           {
-         //  _GetKbdState(x);
-           usb_physw[2] = 0;
-          _kbd_read_keys_r2(usb_physw);
-           }
-
-// ------ modif by Masuji SUTO (start) --------------
-    while(((((usb_physw[2] & USB_MASK)!=USB_MASK) && (nMode==0)) || (((usb_physw[2] & USB_MASK)==USB_MASK) && (nMode==1))) && ((int)get_tick_count()-tick < DELAY_TIMEOUT));
-// ------ modif by Masuji SUTO (end)   --------------
-	}
-       } // ricoh ca-1 mode
-
-else
-
-       {
-         do
-          {
-         //  _GetKbdState(x);
-           usb_physw[2] = 0;
-          _kbd_read_keys_r2(usb_physw);
-           }
-        while((usb_physw[2]&USB_MASK) &&  ((int)get_tick_count()-tick < DELAY_TIMEOUT));
-
-        }
-
-  } // synch enable
-
-
-if (conf.synch_delay_enable && conf.synch_delay_value>0)       // if delay is switched on and greater than 0
-  {
-    for (count1=0;count1<conf.synch_delay_value+(conf.synch_delay_coarse_value*1000);count1++) // wait delay_value * 0.1ms
-    {
-      for (count2=0;count2<1400;count2++)            // delay approx. 0.1ms
-        {
-        }
-     }
-  }
-
-debug_led(0);
-
-asm volatile ("LDMFD SP!, {R0-R11,LR}\n"); // restore R0-R11 and LR from stack
-}
-
 
 static void __attribute__((noinline)) mykbd_task_proceed()
 {
@@ -294,19 +179,12 @@ void my_kbd_read_keys()
         // draw_txt_string(8, 12, osd_buf, conf.osd_color);
 		
 		
-	remote_key = (physw_status[2] & USB_MASK)==USB_MASK;
-	if (remote_key) {
-		remote_count += 1;
-	}
-	else if (remote_count) {
-		usb_power = remote_count;
-		remote_count = 0;
-	}
+	usb_remote_key(physw_status[USB_IDX]) ;
 
 	if (conf.remote_enable) {
-		physw_status[2] = physw_status[SD_READONLY_REG] & ~(SD_READONLY_FLAG | USB_MASK);
+		physw_status[USB_IDX] = physw_status[USB_IDX] & ~(SD_READONLY_FLAG | USB_MASK);
 	} else {
-		physw_status[2] = physw_status[SD_READONLY_REG] & ~SD_READONLY_FLAG;
+		physw_status[USB_IDX] = physw_status[USB_IDX] & ~SD_READONLY_FLAG;
 	}
 }
 
@@ -439,18 +317,6 @@ long kbd_get_autoclicked_key() {
 		}
 	}
 }
-
-
-int get_usb_power(int edge)
-{
-	int x;
-
-	if (edge) return remote_key;
-	x = usb_power;
-	usb_power = 0;
-	return x;
-}
-
 
 long kbd_use_zoom_as_mf() {
 	return 0;
