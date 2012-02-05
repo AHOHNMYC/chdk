087221bbbcec8c876f22360751569920fc52c5a4
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2012-02-05 07:42:58 +0000

    Merged revision(s) 1640 from branches/release-1_0:

--- trunk/platform/ixus95_sd1200/lib.c
+++ trunk/platform/ixus95_sd1200/lib.c
@@ -50,11 +50,11 @@ void debug_led(int state)
 
 // from sx10
 void JogDial_CW(void){
- _PostLogicalEventForNotPowerType(0x874, 2);  // RotateJogDialRight
+ _PostLogicalEventForNotPowerType(0x874, 1);  // RotateJogDialRight
 }
 
 void JogDial_CCW(void){
- _PostLogicalEventForNotPowerType(0x875, 2);  // RotateJogDialLeft
+ _PostLogicalEventForNotPowerType(0x875, 1);  // RotateJogDialLeft
 }
 
 
--- trunk/platform/sx230hs/lib.c
+++ trunk/platform/sx230hs/lib.c
@@ -4,7 +4,8 @@
 
 void vid_bitmap_refresh()
 {
-	extern int full_screen_refresh;
+ 	extern int full_screen_refresh;
+
 	extern void _LockAndRefresh(); // wrapper function for screen lock
 	extern void _UnlockAndRefresh(); // wrapper function for screen unlock
 
@@ -48,11 +49,11 @@ int get_flash_params_count(void){
 }
 
 void JogDial_CW(void){
- _PostLogicalEventForNotPowerType(0x86E, 2);  // @FF416880 (100c)
+ _PostLogicalEventForNotPowerType(0x86E, 1);  // @FF416880 (100c)
 }
 
 void JogDial_CCW(void){
- _PostLogicalEventForNotPowerType(0x86F, 2);  // @FF41688C (100c)
+ _PostLogicalEventForNotPowerType(0x86F, 1);  // @FF41688C (100c)
 }
 
 
@@ -87,3 +88,4 @@ int vid_get_viewport_xoffset()
 }
 
 long vid_get_viewport_height(){ return 240; }
+
