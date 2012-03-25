5e931eeff8b59dc68de0ed08aaa16149c8c98e59
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2012-03-25 00:12:56 +0000

    Merged revision(s) 1766 from branches/release-1_0: add dancingbits encoding for dryos R50 (untested)

--- tools/dancingbits.h
+++ tools/dancingbits.h
@@ -2,7 +2,7 @@
 // Dancing bits data for encoding diskboot.bin file
 // Used by dancingbits.c and finsig_dryos.c
 
-#define VITALY 7
+#define VITALY 8
 unsigned char _chr_[VITALY][8] = {
 									{ 4,6,1,0,7,2,5,3 }, // original flavor
 									{ 5,3,6,1,2,7,0,4 }, // nacho cheese sx200is, ixus100_sd780, ixu95_sd1200, a1100, d10
@@ -11,5 +11,6 @@ unsigned char _chr_[VITALY][8] = {
 									{ 3,2,7,5,1,4,6,0 }, // cajun chicken s95, g12, sx30 (dryos r45)
 									{ 0,4,2,7,3,6,5,1 }, // spicy wasabi sx220, sx230, ixus310 (dryos r47)
 									{ 7,1,5,3,0,6,4,2 }, // sea salt & vinegar sx40hs, sx150is (dryos r49)
+									{ 6,3,1,0,5,7,2,4 }, // spicy habenaro sx260hs (dryos r50)
 								};
 
