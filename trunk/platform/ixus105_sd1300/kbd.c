6d715dc8b739de8d3efd1fccc3e8190e14aee13a
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2012-04-29 03:23:51 +0000

    add unfinished alpha port of ixus105_sd1300 100d from c10ud in http://chdk.setepontos.com/index.php?topic=650.msg84777#msg84777

---
+++ trunk/platform/ixus105_sd1300/kbd.c
@@ -0,0 +1,231 @@
+#include "lolevel.h"
+#include "platform.h"
+#include "core.h"
+#include "conf.h"
+#include "keyboard.h"
+
+typedef struct {
+	short grp;
+	short hackkey;
+	long canonkey;
+} KeyMap;
+
+static long kbd_new_state[3];
+static long kbd_prev_state[3];
+static long kbd_mod_state[3];
+
+static long last_kbd_key = 0;
+extern void _GetKbdState(long*);
+
+#define KEYS_MASK0 (0x00000500)
+#define KEYS_MASK1 (0x38000000)
+#define KEYS_MASK2 (0x00005C0B)
+
+#define SD_READONLY_FLAG    0x00020000 // Found @0xffea7b60, levent 0x90a
+#define SD_READONLY_IDX     2
+#define USB_MASK            0x00040000 // Found @0xffea7b6c, levent 0x902
+#define USB_IDX             2
+
+extern void usb_remote_key( int ) ;
+int get_usb_bit() 
+{
+	long usb_physw[3];
+	usb_physw[USB_IDX] = 0;
+	_kbd_read_keys_r2(usb_physw);
+	return(( usb_physw[USB_IDX] & USB_MASK)==USB_MASK) ; 
+}
+
+static KeyMap keymap[] = {
+	/* tiny bug: key order matters. see kbd_get_pressed_key()
+	 * for example
+	 */
+
+	{ 0, KEY_MENU       , 0x00000400 },
+    { 0, KEY_DISPLAY    , 0x00000100 }, // Playback button
+
+	{ 1, KEY_LEFT		, 0x08000000 },
+	{ 1, KEY_ZOOM_IN	, 0x20000000 },
+	{ 1, KEY_ZOOM_OUT	, 0x10000000 },
+
+	{ 2, KEY_SHOOT_FULL	, 0x00005000 }, // 0101
+    { 2, KEY_SHOOT_HALF	, 0x00004000 }, // 0100
+	
+	{ 2, KEY_UP		    , 0x00000800 },
+	{ 2, KEY_DOWN		, 0x00000002 },
+	{ 2, KEY_RIGHT		, 0x00000400 },
+	{ 2, KEY_SET		, 0x00000001 },
+	{ 2, KEY_PRINT		, 0x00000008 },   // I used DISP. key for this.
+    { 0, 0              , 0x00000000 }
+};
+
+
+long __attribute__((naked)) wrap_kbd_p1_f() ;
+
+static void __attribute__((noinline)) mykbd_task_proceed()
+{
+	while (physw_run){
+        _SleepTask(10);
+
+		if (wrap_kbd_p1_f() == 1){ // autorepeat ?
+			_kbd_p2_f();
+		}
+	}
+}
+
+long __attribute__((naked,noinline)) wrap_kbd_p1_f()
+{
+
+	asm volatile(
+		"STMFD   SP!, {R1-R5,LR}\n"
+		"MOV     R4, #0\n"
+		"BL      my_kbd_read_keys\n"
+		"B       _kbd_p1_f_cont\n"
+	);
+	return 0; // shut up the compiler
+}
+
+
+void my_kbd_read_keys()
+{
+	kbd_prev_state[0] = kbd_new_state[0];
+	kbd_prev_state[1] = kbd_new_state[1];
+	kbd_prev_state[2] = kbd_new_state[2];
+
+	_GetKbdState(kbd_new_state);
+	_kbd_read_keys_r2(kbd_new_state);
+
+	if (kbd_process() == 0){
+        // leave it alone...
+        physw_status[0] = kbd_new_state[0];
+        physw_status[1] = kbd_new_state[1];
+        physw_status[2] = kbd_new_state[2];
+	} else {
+		// override keys
+	 	physw_status[0] = (kbd_new_state[0] & (~KEYS_MASK0)) | (kbd_mod_state[0] & KEYS_MASK0);
+		physw_status[1] = (kbd_new_state[1] & (~KEYS_MASK1)) | (kbd_mod_state[1] & KEYS_MASK1);
+		physw_status[2] = (kbd_new_state[2] & (~KEYS_MASK2)) | (kbd_mod_state[2] & KEYS_MASK2);
+	}
+
+	usb_remote_key(physw_status[USB_IDX]);
+
+	if (conf.remote_enable) {
+		physw_status[USB_IDX] = physw_status[USB_IDX] & ~(SD_READONLY_FLAG | USB_MASK);
+	} else {
+		physw_status[USB_IDX] = physw_status[USB_IDX] & ~SD_READONLY_FLAG;
+	}
+
+}
+
+
+
+/****************/
+
+void kbd_key_press(long key)
+{
+	int i;
+	for (i=0;keymap[i].hackkey;i++){
+		if (keymap[i].hackkey == key){
+			kbd_mod_state[keymap[i].grp] &= ~keymap[i].canonkey;
+			return;
+		}
+	}
+}
+
+void kbd_key_release(long key)
+{
+	int i;
+	for (i=0;keymap[i].hackkey;i++){
+		if (keymap[i].hackkey == key){
+			kbd_mod_state[keymap[i].grp] |= keymap[i].canonkey;
+			return;
+		}
+	}
+}
+
+void kbd_key_release_all()
+{
+	kbd_mod_state[0] |= KEYS_MASK0;
+	kbd_mod_state[1] |= KEYS_MASK1;
+	kbd_mod_state[2] |= KEYS_MASK2;
+}
+
+long kbd_is_key_pressed(long key)
+{
+	int i;
+	for (i=0;keymap[i].hackkey;i++){
+		if (keymap[i].hackkey == key){
+			return ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0) ? 1:0;
+		}
+	}
+	return 0;
+}
+
+long kbd_is_key_clicked(long key)
+{
+	int i;
+	for (i=0;keymap[i].hackkey;i++){
+		if (keymap[i].hackkey == key){
+			return ((kbd_prev_state[keymap[i].grp] & keymap[i].canonkey) != 0) &&
+			       ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0);
+		}
+	}
+	return 0;
+}
+
+long kbd_get_pressed_key()
+{
+	int i;
+	for (i=0;keymap[i].hackkey;i++){
+		if ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0){
+			return keymap[i].hackkey;
+		}
+	}
+	return 0;
+}
+
+long kbd_get_clicked_key()
+{
+	int i;
+	for (i=0;keymap[i].hackkey;i++){
+		if (((kbd_prev_state[keymap[i].grp] & keymap[i].canonkey) != 0) &&
+		    ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0)) {
+			return keymap[i].hackkey;
+		}
+	}
+	return 0;
+}
+
+void kbd_reset_autoclicked_key() {
+	last_kbd_key = 0;
+}
+
+long kbd_get_autoclicked_key() {
+	static long last_kbd_time = 0, press_count = 0;
+	register long key, t;
+
+	key=kbd_get_clicked_key();
+	if (key) {
+		last_kbd_key = key;
+		press_count = 0;
+		last_kbd_time = get_tick_count();
+		return key;
+	} else {
+		if (last_kbd_key && kbd_is_key_pressed(last_kbd_key)) {
+			t = get_tick_count();
+			if (t-last_kbd_time>((press_count)?175:500)) {
+				++press_count;
+				last_kbd_time = t;
+				return last_kbd_key;
+			} else {
+				return 0;
+			}
+		} else {
+			last_kbd_key = 0;
+			return 0;
+		}
+	}
+}
+
+long kbd_use_zoom_as_mf() {
+ return 0;
+}
---
+++ trunk/platform/ixus105_sd1300/platform_camera.h
@@ -0,0 +1,74 @@
+// Camera - ixus105_sd1300 - platform_camera.h
+
+// This file contains the various settings values specific to the ixus105_sd1300 camera.
+// This file is referenced via the 'include/camera.h' file and should not be loaded directly.
+
+// If adding a new settings value put a suitable default in 'include/camera.h',
+// along with documentation on what the setting does and how to determine the correct value.
+// If the setting should not have a default value then add it in 'include/camera.h'
+// using the '#undef' directive along with appropriate documentation.
+
+// Override any default values with your camera specific values in this file. Try and avoid
+// having override values that are the same as the default value.
+
+// When overriding a setting value there are two cases:
+// 1. If removing the value, because it does not apply to your camera, use the '#undef' directive.
+// 2. If changing the value it is best to use an '#undef' directive to remove the default value
+//    followed by a '#define' to set the new value.
+
+// When porting CHDK to a new camera, check the documentation in 'include/camera.h'
+// for information on each setting. If the default values are correct for your camera then
+// don't override them again in here.
+
+
+    #define CAM_DRYOS         1
+    #define CAM_DRYOS_2_3_R39 1 // Defined for cameras with DryOS version R39 or higher
+    #define CAM_RAW_ROWPIX    4104 // Found @0xffcf4dc4
+    #define CAM_RAW_ROWS      3048 // Found @0xffcf4dd0
+    #undef  CAM_UNCACHED_BIT
+    #define CAM_UNCACHED_BIT  0x40000000 // Found @0xffc29e80
+    #define CAM_PROPSET                 3
+    #define CAM_NEED_HP                 1
+    #define CAM_MULTIPART               1
+    #define CAM_HAS_PLAYBACK_PB         1
+    #define CAM_MOVIE_PLE               1
+    #define CAM_USES_EXMEM              1
+    #define CAM_USE_SET_BUTTON          1 
+    #undef  CAM_HAS_MANUAL_FOCUS
+    #undef  CAM_CAN_UNLOCK_OPTICAL_ZOOM_IN_VIDEO
+    #define CAM_VIDEO_QUALITY_ONLY          1
+    #undef  CAM_VIDEO_CONTROL
+    #undef  CAM_USE_ZOOM_FOR_MF
+    #undef CAM_HAS_ERASE_BUTTON
+    #define  CAM_SHOW_OSD_IN_SHOOT_MENU  1
+    #define CAM_EXT_TV_RANGE            1
+    #define CAM_HAS_ND_FILTER           1
+
+    // pattern
+    #define cam_CFAPattern 0x02010100 // Red  Green  Green  Blue
+    // color
+
+    #define CAM_COLORMATRIX1                               \
+      827547, 1000000, -290458, 1000000, -126086, 1000000, \
+     -12829,  1000000, 530507,  1000000, 50537,   1000000, \
+      5181,   1000000, 48183,   1000000, 245014,  1000000
+
+    #define cam_CalibrationIlluminant1 1 // Daylight
+    // cropping
+    #define CAM_JPEG_WIDTH  3648
+    #define CAM_JPEG_HEIGHT 2736
+    #define CAM_ACTIVE_AREA_X1 6
+    #define CAM_ACTIVE_AREA_Y1 12
+    #define CAM_ACTIVE_AREA_X2 3690
+    #define CAM_ACTIVE_AREA_Y2 2772
+
+    // camera name
+    #define PARAM_CAMERA_NAME 4 // parameter number for GetParameterData
+    #undef  CAM_SENSOR_BITS_PER_PIXEL
+    #undef  CAM_WHITE_LEVEL
+    #undef  CAM_BLACK_LEVEL
+    #define CAM_SENSOR_BITS_PER_PIXEL   12
+    #define CAM_WHITE_LEVEL             ((1<<CAM_SENSOR_BITS_PER_PIXEL)-1)
+    #define CAM_BLACK_LEVEL             127
+    
+
