32e72e43eec6348127e0df814cdbaababdab3816
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2012-06-03 00:12:59 +0000

    liveview - sx220hs untested play check for aspect width/offset

--- branches/reyalp-ptp-live/platform/sx220hs/lib.c
+++ branches/reyalp-ptp-live/platform/sx220hs/lib.c
@@ -78,6 +78,9 @@ int vid_get_viewport_width()
 	// viewport width table for each image size
 	// 0 = 4:3, 1 = 16:9, 2 = 3:2, 3 = 1:1
 	static long vp_w[4] = { 360, 480, 360, 272 };
+    if((mode_get()&MODE_MASK) == MODE_PLAY) {
+        return 480;
+    }
 	return vp_w[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
 }
 
@@ -86,6 +89,9 @@ int vid_get_viewport_display_xoffset()
 	// viewport width offset table for each image size
 	// 0 = 4:3, 1 = 16:9, 2 = 3:2, 3 = 1:1
 	static long vp_w[4] = { 60, 0, 36, 104 };				// should all be even values for edge overlay
+    if((mode_get()&MODE_MASK) == MODE_PLAY) {
+        return 0;
+    }
 	return vp_w[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
 }
 
