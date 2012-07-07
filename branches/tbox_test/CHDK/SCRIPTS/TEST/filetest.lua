c4b4ffac41f78803b50e1fa1e7f6aade52a8543f
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2012-07-07 02:04:29 +0000

    Delete old test branch.

--- branches/tbox_test/CHDK/SCRIPTS/TEST/filetest.lua
+++
@@ -1,7 +0,0 @@
---[[
-@title file browser test
-]]
-
-f = file_browser("A/CHDK/SCRIPTS")
-
-print(f)
--- branches/tbox_test/include/limits.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIMITS_H
-#define LIMITS_H
-
-#define INT_MAX 2147483647
-#define SHRT_MAX 32767
-
-#define UCHAR_MAX 255
-
-#endif
