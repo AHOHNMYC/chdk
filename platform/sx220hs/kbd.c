36f355367b8dfdad4b562545f94aa535b661e4b1
msl <msl@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2012-08-17 09:19:26 +0000

    SX220 + added 'physw_sleep_delay' instead of hardwired delay + added 'PARAM_OWNER_NAME' in platform_camera.h + cleanup kbd.c, platform_camera.h
    All
    + changed color definition for DOF+ displaying in misc (safe green in record mode)

--- platform/sx220hs/kbd.c
+++ platform/sx220hs/kbd.c
@@ -22,12 +22,12 @@ extern void _GetKbdState(long*);
 // override key and feather bits to avoid feather osd messing up chdk display in ALT mode
 #define KEYS_MASK0 (0x000FFC0F)     // sx220 physw_status[0]
 #define KEYS_MASK1 (0x00200000)
-#define KEYS_MASK2 (0x00002182)  	// sx220 physw_status[2]
+#define KEYS_MASK2 (0x00002182)     // sx220 physw_status[2]
 
 #define NEW_SS (0x2000)
 #define SD_READONLY_FLAG (0x20000)
-#define SD_READONLY_IDX     2         
-#define USB_IDX             2 
+#define SD_READONLY_IDX     2
+#define USB_IDX             2
 
 #define USB_MASK (0x4000000)
 #define USB_IDX  2
@@ -35,10 +35,10 @@ extern void _GetKbdState(long*);
 extern void usb_remote_key( void ) ;
 int get_usb_bit() 
 {
-	long usb_physw[3];
-	usb_physw[USB_IDX] = 0;
-	_kbd_read_keys_r2(usb_physw);
-	return(( usb_physw[USB_IDX] & USB_MASK)==USB_MASK) ; 
+    long usb_physw[3];
+    usb_physw[USB_IDX] = 0;
+    _kbd_read_keys_r2(usb_physw);
+    return(( usb_physw[USB_IDX] & USB_MASK)==USB_MASK) ;
 }
 
 #ifndef MALLOCD_STACK
@@ -63,14 +63,14 @@ static KeyMap keymap[] = {
     { 0, KEY_ZOOM_IN         , 0x0000000C },
     { 0, KEY_ZOOM_IN2        , 0x0000000C },
     { 0, KEY_DISPLAY         , 0x00000800 },
-    { 0, KEY_UP              , 0x00001000 }, 
+    { 0, KEY_UP              , 0x00001000 },
     { 0, KEY_RIGHT           , 0x00006000 },
     { 0, KEY_SET             , 0x00010000 },
     { 0, KEY_DOWN            , 0x00020000 },
     { 0, KEY_MENU            , 0x00040000 },
     { 0, KEY_VIDEO           , 0x00080000 },
- //   { 0, KEY_RIGHT_SOFT      , 0x00002000 },	//soft keys commented out but still counted in the keymasks, seems to disable them like we want.
- //   { 0, KEY_UP_SOFT         , 0x00000400 },	//if enabled can cause unintentional button presses in alt menu when rotating the jogdial really fast.
+ //   { 0, KEY_RIGHT_SOFT      , 0x00002000 },  //soft keys commented out but still counted in the keymasks, seems to disable them like we want.
+ //   { 0, KEY_UP_SOFT         , 0x00000400 },  //if enabled can cause unintentional button presses in alt menu when rotating the jogdial really fast.
  //   { 0, KEY_DOWN_SOFT       , 0x00008000 },
 
     { 1, KEY_PRINT           , 0x00200000 }, // playback = alt button
@@ -91,20 +91,20 @@ void kbd_set_alt_mode_key_mask(long key)
 }
 
 void my_blinkk(void) {
-	int i;
-	while(1) {
-		*((volatile int *) 0xC0220130) = 0x46; // Turn on LED
-		for (i=0; i<0x200000; i++) { asm volatile ( "nop\n" ); }
+    int i;
+    while(1) {
+        *((volatile int *) 0xC0220130) = 0x46; // Turn on LED
+        for (i=0; i<0x200000; i++) { asm volatile ( "nop\n" ); }
 
-		*((volatile int *) 0xC0220130) = 0x44; // Turn off LED
-		for (i=0; i<0x200000; i++) { asm volatile ( "nop\n" ); }
+        *((volatile int *) 0xC0220130) = 0x44; // Turn off LED
+        for (i=0; i<0x200000; i++) { asm volatile ( "nop\n" ); }
 
-		*((volatile int *) 0xC0220130) = 0x46; // Turn on LED
-		for (i=0; i<0x200000; i++) { asm volatile ( "nop\n" ); }
+        *((volatile int *) 0xC0220130) = 0x46; // Turn on LED
+        for (i=0; i<0x200000; i++) { asm volatile ( "nop\n" ); }
 
-		*((volatile int *) 0xC0220130) = 0x44; // Turn off LED
-		for (i=0; i<0x900000; i++) { asm volatile ( "nop\n" ); }
-	}
+        *((volatile int *) 0xC0220130) = 0x44; // Turn off LED
+        for (i=0; i<0x900000; i++) { asm volatile ( "nop\n" ); }
+    }
 }
 
 // Set to 1 to disable jogdial events from being processed in firmware
@@ -114,10 +114,10 @@ long __attribute__((naked)) wrap_kbd_p1_f();
 
 static void __attribute__((noinline)) mykbd_task_proceed() {
     while (physw_run) {
-        _SleepTask(*((int*)(0x1c40+0x8)));//10); @FF0244F4
-		
-		if (wrap_kbd_p1_f() == 1) {   // autorepeat ?
-        	_kbd_p2_f();
+        _SleepTask(physw_sleep_delay);
+
+        if (wrap_kbd_p1_f() == 1) {   // autorepeat ?
+            _kbd_p2_f();
         }
     }
 }
@@ -125,24 +125,24 @@ static void __attribute__((noinline)) mykbd_task_proceed() {
 // no stack manipulation needed here, since we create the task directly
 void __attribute__((naked,noinline)) mykbd_task() {
 
-	mykbd_task_proceed(); 
+    mykbd_task_proceed();
 
 // function can be modified to restore SP here...
 
-	_ExitTask();
+    _ExitTask();
 }
 
 // 
 long __attribute__((naked,noinline)) wrap_kbd_p1_f() {
     asm volatile(
                 "STMFD   SP!, {R1-R7,LR}\n"       //SX220 modified
-                "MOV     R5, #0\n"					
+                "MOV     R5, #0\n"
             //    "BL      _kbd_read_keys\n"       // replaces kbd_fetch_data()
                 "BL      my_kbd_read_keys\n"     // +
                 "B       _kbd_p1_f_cont\n"       // continue
     );
-	
-	return 0;   // shut up the compiler
+
+    return 0;   // shut up the compiler
 }
 
 // copied from g12 and sx30, thx to philmoz
@@ -163,28 +163,27 @@ void jogdial_control(int n)
 
 // like SX30 and g12
 void my_kbd_read_keys() {
-	
+
     kbd_prev_state[0] = kbd_new_state[0];
-	kbd_prev_state[1] = kbd_new_state[1];
+    kbd_prev_state[1] = kbd_new_state[1];
     kbd_prev_state[2] = kbd_new_state[2];
 
-	_GetKbdState(kbd_new_state);
-	_kbd_read_keys_r2(kbd_new_state);
-	
+    _GetKbdState(kbd_new_state);
+    _kbd_read_keys_r2(kbd_new_state);
 
     if (kbd_process() == 0) {
         // we read keyboard state with _kbd_read_keys()
-		  physw_status[0] = kbd_new_state[0];
-		  physw_status[1] = kbd_new_state[1];
-          physw_status[2] = kbd_new_state[2];
-		 jogdial_control(0);
+        physw_status[0] = kbd_new_state[0];
+        physw_status[1] = kbd_new_state[1];
+        physw_status[2] = kbd_new_state[2];
+        jogdial_control(0);
     } else {
         // override keys
         physw_status[0] = (kbd_new_state[0] | KEYS_MASK0) & (~KEYS_MASK0 | kbd_mod_state[0]); 
-		physw_status[1] = (kbd_new_state[1] | KEYS_MASK1) & (~KEYS_MASK1 | kbd_mod_state[1]); 
+        physw_status[1] = (kbd_new_state[1] | KEYS_MASK1) & (~KEYS_MASK1 | kbd_mod_state[1]); 
         physw_status[2] = (kbd_new_state[2] | KEYS_MASK2) & (~KEYS_MASK2 | kbd_mod_state[2]);
         
-		if ((jogdial_stopped==0) && !state_kbd_script_run) {
+        if ((jogdial_stopped==0) && !state_kbd_script_run) {
             jogdial_control(1);
             get_jogdial_direction();
         }
@@ -192,13 +191,13 @@ void my_kbd_read_keys() {
             jogdial_control(0);
     }
 
-	usb_remote_key() ;
+    usb_remote_key() ;
 
-	if (conf.remote_enable) {
-		physw_status[USB_IDX] = physw_status[USB_IDX] & ~(SD_READONLY_FLAG | USB_MASK);
-	} else {
-		physw_status[USB_IDX] = physw_status[USB_IDX] & ~SD_READONLY_FLAG;
-	}
+    if (conf.remote_enable) {
+        physw_status[USB_IDX] = physw_status[USB_IDX] & ~(SD_READONLY_FLAG | USB_MASK);
+    } else {
+        physw_status[USB_IDX] = physw_status[USB_IDX] & ~SD_READONLY_FLAG;
+    }
 
 }
 
@@ -223,8 +222,8 @@ void kbd_key_release(long key) {
 }
 
 void kbd_key_release_all() {
-	
-	kbd_mod_state[0] |= KEYS_MASK0;
+
+    kbd_mod_state[0] |= KEYS_MASK0;
     kbd_mod_state[1] |= KEYS_MASK1;
     kbd_mod_state[2] |= KEYS_MASK2;
 
@@ -258,7 +257,7 @@ long kbd_get_pressed_key() {
             return keymap[i].hackkey;
         }
     }
-	
+
     return 0;
 }
 
