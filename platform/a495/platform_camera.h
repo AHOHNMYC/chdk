b1a649ee4457887c71f822f0f81f806b8dc393b4
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2012-09-17 01:24:43 +0000

    a495 modemap corrections, optical zoom fixes from nafraf in http://chdk.setepontos.com/index.php?topic=650.msg90983#msg90983

--- platform/a495/platform_camera.h
+++ platform/a495/platform_camera.h
@@ -81,6 +81,12 @@
     #define CAM_DRIVE_MODE_FROM_TIMER_MODE  1   // use PROPCASE_TIMER_MODE to check for multiple shot custom timer.
                                                 // Used to enabled bracketing in custom timer, required on many recent cameras
                                                 // see http://chdk.setepontos.com/index.php/topic,3994.405.html
+
+    #define CAM_HAS_ZOOM_LEVER               1    // Camera has dedicated zoom buttons
+    #define CAM_USE_ZOOM_FOR_MF              1    // Zoom lever can be used for manual focus adjustments
+    #define CAM_NEED_SET_ZOOM_DELAY          150  // Define to add a delay after setting the zoom position
+    #define CAM_USE_OPTICAL_MAX_ZOOM_STATUS  1    // Use ZOOM_OPTICAL_MAX to reset zoom_status when switching from digital to optical zoom
+
 //	#define REMOTE_SYNC_STATUS_LED 	0xC0xxyyyy		// specifies an LED that turns on while camera waits for USB remote to sync
 												
 //----------------------------------------------------------
