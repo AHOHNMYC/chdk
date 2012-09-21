c6ff1ccce14eedb9b41b4f8569ebd8091b09bd68
srsa <srsa@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2012-09-21 21:44:46 +0000

    merged revision 2166 from trunk update for A460, A450 (tested on A460, adapted to A450) - fast md buffer support - enabled CAM_FIRMWARE_MEMINFO - liveview functions - RAM size definition corrected

--- platform/a450/platform_camera.h
+++ platform/a450/platform_camera.h
@@ -58,6 +58,7 @@
     #define CAM_ACTIVE_AREA_Y2              1962
     // camera name
     #define PARAM_CAMERA_NAME               4 // parameter number for GetParameterData
+    #define CAM_FIRMWARE_MEMINFO            1
 
 	#define REMOTE_SYNC_STATUS_LED 	0xc0220084				// specifies an LED that turns on while camera waits for USB remote to sync
 //----------------------------------------------------------
