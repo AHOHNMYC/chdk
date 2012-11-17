3143f142f54ac803fd6f245c45fd26a2c34531f2
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2012-11-17 10:32:44 +0000

    Merged revision(s) 2285 from trunk: A1300 1.00d port from nafraf. http://chdk.setepontos.com/index.php?topic=650.msg93324#msg93324

--- core/gui.c
+++ core/gui.c
@@ -1461,7 +1461,7 @@ const char* gui_histo_show_enum(int change, int arg) {
     #elif defined(CAMERA_a650)
         static const char* names[]={ "Print", "ISO"};
         static const int keys[]={ KEY_PRINT, KEY_ISO };
-    #elif defined(CAMERA_a810) 
+    #elif defined(CAMERA_a810) || defined(CAMERA_a1300) 
         static const char* names[]={ "Playback", "Help"}; 
         static const int keys[]={ KEY_PRINT, KEY_HELP }; 
     #elif defined(CAMERA_a3300) || defined(CAMERA_a3200)
