44c79e40cefc2ff3f8d12c40a078ee5a301e1401
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2012-11-27 09:51:16 +0000

    Some makefile cleanup and changes.

--- trunk/tools/auto_build.sh
+++ trunk/tools/auto_build.sh
@@ -9,6 +9,6 @@ while IFS=, read cam fw state copy skip
 do
   if [ ${cam} != "CAMERA" ] && ( [ "$4" = "-noskip" ] || [ "${skip}" = "" ] ); then \
     if [ "${state}" != "" ]; then state=_${state}; fi; \
-    $1 -s --no-print-directory PLATFORM=${cam} PLATFORMSUB=${fw} STATE=${state} COPY_TO=${copy} $2 || exit 1; \
+    $1 -s --no-print-directory PLATFORM=${cam} PLATFORMSUB=${fw} STATE=${state} COPY_TO=${copy} SKIP_AUTOBUILD=${skip} $2 || exit 1; \
   fi;
 done < $3
