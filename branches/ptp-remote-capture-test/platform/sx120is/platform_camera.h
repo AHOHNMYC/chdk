be3ddda09d6a39c279eb8513f04d5cd18ebd0900
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-01-05 05:37:41 +0000

    update remote capture branch from trunk through r2446 (up to but not including philmoz reorg)

--- branches/ptp-remote-capture-test/platform/sx120is/platform_camera.h
+++ branches/ptp-remote-capture-test/platform/sx120is/platform_camera.h
@@ -22,7 +22,7 @@
 
     #define CAM_PROPSET                     3
     #define CAM_DRYOS                       1
-    #define CAM_DRYOS_2_3_R39               1 // Defined for cameras with DryOS version R39 or higher
+    //#define CAM_DRYOS_2_3_R39               1 // Don't define this - even though cam is R39 is still needs old CHDK code
 
     #define CAM_RAW_ROWPIX                  3728    // see platform/sub/*/lib.c
     #define CAM_RAW_ROWS                    2778
