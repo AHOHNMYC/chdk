7339e6832a7668487c75a54110b7d3203c5505dc
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-01-05 08:48:34 +0000

    Add G1X 1.01a.

---
+++ platform/g1x/sub/101a/lib.c
@@ -0,0 +1,31 @@
+#include "platform.h"
+
+char *hook_raw_image_addr()
+{
+	// Appears to be two raw buffers (this returns active buffer)
+	// Addresses below found in table at FF47686C used in sub_FF0EF360 
+	// [0x40D0 + 0xC] --> defines which buffer is active
+
+	if (*((int*)0x40DC) != 0)
+		return (char*) 0x4A6BA6A0;
+	else
+		return (char*) 0x44000000;	// found at (FF40ADE8) [search CRAW BUF]
+}
+
+char *hook_alt_raw_image_addr()
+{
+	// Appears to be two raw buffers (this returns inactive buffer)
+	// Addresses below found in table at FF47686C used in sub_FF0EF360 
+	// [0x40D0 + 0xC] --> defines which buffer is active
+
+	if (*((int*)0x40DC) == 0)
+		return (char*) 0x4A6BA6A0;
+	else
+		return (char*) 0x44000000;	// found at (FF40ADE8) [search CRAW BUF]
+}
+
+
+char *camera_jpeg_count_str()    { return (char*)0x000cc108; }             // Found @0xff28c9f0
+long hook_raw_size()             { return 0x01941be8; }                    // Found @0xff40ae40
+
+void *vid_get_viewport_fb_d()    { return (void*)(*(int*)(0x3d90+0x58)); } // Found @0xff06e538 & 0xff06e570
