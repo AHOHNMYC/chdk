fd6f6fc9c6514e57691915c7d2b49bab0812df36
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-01-13 04:38:07 +0000

    remove old remotecap test branch

--- branches/ptp-remote-capture-test/CHDK/BOOKS/readme.txt
+++
@@ -1,3 +0,0 @@
-nothing in here yet.
-
-insert tutorials and links to tutorials (ubasic, raw average, etc)
--- branches/ptp-remote-capture-test/include/stddef.h
+++
@@ -1,9 +0,0 @@
-#ifndef STDDEF_H
-#define STDDEF_H
-
-typedef unsigned int size_t;
-typedef int ssize_t;
-typedef int ptrdiff_t;
-#define NULL ((void*)0)
-
-#endif
