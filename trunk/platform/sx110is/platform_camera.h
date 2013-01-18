b9fed6ef1dba29b15a3f2d3864d5a935a72a9fc2
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-01-18 08:10:42 +0000

    SX110IS update from nafraf, new loader + exp_drv_task implementation. http://chdk.setepontos.com/index.php?topic=650.msg95837#msg95837

--- trunk/platform/sx110is/platform_camera.h
+++ trunk/platform/sx110is/platform_camera.h
@@ -62,6 +62,8 @@
 
     #define CAM_STARTUP_CRASH_FILE_OPEN_FIX 1   // enable fix for camera crash at startup when opening the conf / font files see http://chdk.setepontos.com/index.php?topic=6179.0
 
+    #define CAM_EXT_TV_RANGE                1
+
    //	#define REMOTE_SYNC_STATUS_LED 	0xC0xxyyyy		// specifies an LED that turns on while camera waits for USB remote to sync
   
 //----------------------------------------------------------
