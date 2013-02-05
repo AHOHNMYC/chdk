556744ee1b3f45224ef22dd80bd67e39cc706f0f
msl <msl@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-02-05 12:20:10 +0000

    SX220
    * Remove vid_get_viewport_fb(), vid_get_viewport_live_fb(), vid_get_bitmap_fb() in sub/lib.c and add in platform/lib.c
    * Enable #define DRAW_ON_ACTIVE_BITMAP_BUFFER_ONLY
    * Reorganized platform_camera.h

--- platform/sx220hs/sub/100a/lib.c
+++ platform/sx220hs/sub/100a/lib.c
@@ -34,28 +34,6 @@ long hook_raw_size()
 	
 }
 
-void *vid_get_viewport_live_fb()
-{
-	return (void*)(void*)(*(int*)(0x2258));
-	
-  	// Found by searching RAM locations that referenced 0x405D7980 (viewport buffer)
-	// and also changed to the other buffer addresses over time. Then used each here
-	// and selected value that gave the fastest Motion Detect response using http://dataghost.com/chdk/md_meter.html.
-}
-
-// OSD buffer
-void *vid_get_bitmap_fb()
-{	
-	//FF050F24                 LDR     R0, =0x40471000
-	return (void*)0x40471000;
-}	                           
-
-// Live picture buffer (shoot half-pressed)
-void *vid_get_viewport_fb()
-{
-	return (void*)0x405D7980;  					                   // found by search for VRAM Address @FF371E8C
-}
-
 void *vid_get_viewport_fb_d()
 {
 	//FF06C580 dword_FF06C580  DCD 0x3C70
