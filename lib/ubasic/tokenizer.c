0d598cea4e1681e31bdc1c8c996fa54a5829ae94
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-02-16 21:48:43 +0000

    Rename function for controlling AF LED in MD test script.

--- lib/ubasic/tokenizer.c
+++ lib/ubasic/tokenizer.c
@@ -206,7 +206,7 @@ static const struct keyword_token keywords[] = {
 
   {"md_detect_motion",        TOKENIZER_MD_DETECT_MOTION},
   {"md_get_cell_diff",        TOKENIZER_MD_GET_CELL_DIFF},
-  {"md_af_on_time",           TOKENIZER_MD_AF_ON_TIME},
+  {"md_af_led_control",       TOKENIZER_MD_AF_LED_CONTROL},
   {"autostarted",             TOKENIZER_SCRIPT_AUTOSTARTED},
   {"get_autostart",           TOKENIZER_GET_SCRIPT_AUTOSTART},
   {"set_autostart",           TOKENIZER_SET_SCRIPT_AUTOSTART},
