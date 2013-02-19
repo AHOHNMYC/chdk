b878578c732384fc1696956b79e4536c7873b25b
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-02-19 05:05:19 +0000

    define CAM_AF_LED for a540 and d10

--- platform/a540/platform_camera.h
+++ platform/a540/platform_camera.h
@@ -60,5 +60,8 @@
 
     #define CAM_FIRMWARE_MEMINFO            1       // Use 'GetMemInfo' to get free memory size.
 //	#define REMOTE_SYNC_STATUS_LED 	0xC0xxyyyy		// specifies an LED that turns on while camera waits for USB remote to sync
+//
+    #undef CAM_AF_LED
+    #define CAM_AF_LED                      9
 
 //----------------------------------------------------------
