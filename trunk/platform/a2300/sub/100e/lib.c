d8faa79d73cd4db1c8524cb5491b3039847dc657
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-03-02 05:34:10 +0000

    A2300 1.00c port from nafraf: http://chdk.setepontos.com/index.php?topic=650.msg97386#msg97386

--- trunk/platform/a2300/sub/100e/lib.c
+++ trunk/platform/a2300/sub/100e/lib.c
@@ -1,7 +1,7 @@
 #include "platform.h"
 
-char *hook_raw_image_addr()     {return (char*) 0x420B7FC0;}   //Found @0xffae0828 a2300
-char *hook_alt_raw_image_addr() {return (char*) 0x420B7FC0;}   //Found @0xffae0828 a2300
+char *hook_raw_image_addr()     {return (char*) 0x420B7FC0;}   //Search for BJPEG BUFF 
+char *hook_alt_raw_image_addr() {return (char*) 0x420B7FC0;}   //Search for BJPEG BUFF
 
 /*Auto generated for a2300 100e*/
 void *vid_get_bitmap_fb()        { return (void*)0x40411000; }             // Found @0xff852ebc
