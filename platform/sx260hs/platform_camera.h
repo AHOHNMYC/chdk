209bbba23414199f2f1642cb77bcab048f16b817
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-03-25 04:03:27 +0000

    sx240 + sx260 usb remote fix from waterwingz in http://chdk.setepontos.com/index.php?topic=650.msg98330#msg98330 - note, disables debug LED - trunk 2653

--- platform/sx260hs/platform_camera.h
+++ platform/sx260hs/platform_camera.h
@@ -121,5 +121,6 @@
 
     #define MKDIR_RETURN_ONE_ON_SUCCESS         1    // mkdir() return 1 on success, 0 on fail.
 
-    #define REMOTE_SYNC_STATUS_LED              0xC022C30C  // TODO specifies an LED that turns on while camera waits for USB remote to sync
+    #undef  REMOTE_SYNC_STATUS_LED
+    // #define REMOTE_SYNC_STATUS_LED              0xC022C30C  // TODO specifies an LED that turns on while camera waits for USB remote to sync
 //----------------------------------------------------------
