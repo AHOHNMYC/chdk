602315869d2f079a3b6b7b9b8f1558194706250a
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-03-31 00:58:11 +0000

    CAM_HAS_VIDEO_BUTTON patch from nafraf: http://chdk.setepontos.com/index.php?topic=650.msg98546#msg98546

--- branches/release-1_1/platform/a3400/platform_camera.h
+++ branches/release-1_1/platform/a3400/platform_camera.h
@@ -63,6 +63,8 @@
 
     #define CAM_EXT_TV_RANGE                1
 
+    #define CAM_HAS_VIDEO_BUTTON            1
+    #undef  CAM_VIDEO_CONTROL
     #define CAM_VIDEO_QUALITY_ONLY          1
 
     #define DNG_VERT_RLE_BADPIXELS          1
@@ -78,8 +80,6 @@
     #undef  CAM_HAS_ERASE_BUTTON
 
     #undef  CAM_HAS_MANUAL_FOCUS
-
-    #undef  CAM_VIDEO_CONTROL
 
     #undef  CAM_BITMAP_PALETTE
     #define CAM_BITMAP_PALETTE              13      // palette number reserved in core/gui_draw.h
