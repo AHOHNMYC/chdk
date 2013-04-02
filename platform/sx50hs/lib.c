7f31d15ff26af9b23954c4b34398d913c5ba6d40
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-04-02 07:04:29 +0000

    SX50HS update from yukia10: http://chdk.setepontos.com/index.php?topic=650.msg98732#msg98732

--- platform/sx50hs/lib.c
+++ platform/sx50hs/lib.c
@@ -41,6 +41,8 @@ void debug_led(int state)
 }
 
 
+
+
 // Power Led = first entry in table (led 0)
 // AF Assist Lamp = second entry in table (led 1)
 void camera_set_led(int led, int state, int bright) {
@@ -48,6 +50,7 @@ void camera_set_led(int led, int state, int bright) {
  _LEDDrive(led_table[led%sizeof(led_table)], state<=1 ? !state : state);
 }
 
+int get_flash_params_count(void) { return 0xa6; }                          // Found @0xff205ce4
 
 void JogDial_CW(void){
  _PostLogicalEventForNotPowerType(0x872, 1);  // RotateJogDialRight (in table @ ff62f34c, fw 1.00b)
@@ -64,22 +67,6 @@ void JogDial_CCW(void){
 extern char active_viewport_buffer;
 extern void* viewport_buffers[];
 
-// Y multiplier for cameras with 480 pixel high viewports (CHDK code assumes 240)
-int vid_get_viewport_yscale() {
-    return 2;
-}
-
-//void *vid_get_viewport_live_fb()
-//{
-//    // Hopefully return the most recently used viewport buffer so that motion detect, histogram, zebra and edge overly are using current image data
-//    int pos=active_viewport_buffer&0xF;
-//	return viewport_buffers[pos];
-//}
-
-// Defined in stubs_min.S
-extern char active_viewport_buffer;
-extern void* viewport_buffers[];
-
 void *vid_get_viewport_fb()
 {
     // Return first viewport buffer - for case when vid_get_viewport_live_fb not defined
@@ -88,18 +75,25 @@ void *vid_get_viewport_fb()
 
 void *vid_get_viewport_live_fb()
 {
-    if (MODE_IS_VIDEO(mode_get()))
+    if (MODE_IS_VIDEO(mode_get()) || is_video_recording())
         return viewport_buffers[0];     // Video only seems to use the first viewport buffer.
 
     // Hopefully return the most recently used viewport buffer so that motion detect, histogram, zebra and edge overly are using current image data
-    return viewport_buffers[(active_viewport_buffer-1)&3];
+    return viewport_buffers[(active_viewport_buffer-1)&7];
 }
-
 
 // Defined in stubs_min.S
 extern int active_bitmap_buffer;
 extern char* bitmap_buffer[];
 
+// SX50 Values below go in 'lib.c':
+void *vid_get_bitmap_fb()        { return (void*)0x406c5000; }             // Found @0xff056b6c
+
+
+// Y multiplier for cameras with 480 pixel high viewports (CHDK code assumes 240)
+int vid_get_viewport_yscale() {
+	return 2;
+}
 
 int vid_get_viewport_width()
 {
@@ -112,8 +106,8 @@ int vid_get_viewport_width()
 }
 
 // viewport width offset table for each image size
-// 0 = 4:3, 1 = 16:9, 2 = 3:2, 3 = 1:1
-static long vp_xo[5] = { 0, 0, 0, 44 };				// should all be even values for edge overlay
+// 0 = 4:3, 1 = 16:9, 2 = 3:2, 3 = 1:1, 4 = 4:5
+static long vp_xo[5] = { 0, 0, 0, 44, 72 };				// should all be even values for edge overlay
 
 int vid_get_viewport_xoffset()
 {
@@ -127,7 +121,7 @@ int vid_get_viewport_xoffset()
     }
     else
     {
-	    return vp_xo[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
+        return vp_xo[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
     }
 }
 
@@ -152,7 +146,7 @@ int vid_get_viewport_display_xoffset()
     }
     else
     {
-	    return vp_xo[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
+        return vp_xo[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
     }
 }
 
@@ -163,49 +157,50 @@ long vid_get_viewport_height()
         return 240;
     }
     extern int _GetVRAMVPixelsSize();
-    return _GetVRAMVPixelsSize();
+    return _GetVRAMVPixelsSize() >> 1;
 }
 
-// viewport height offset table for each image size
-// 0 = 4:3, 1 = 16:9, 2 = 3:2, 3 = 1:1
-static long vp_yo[5] = { 0, 30, 13, 0 };
-
-int vid_get_viewport_yoffset()
+static int vp_yoffset(int stitch)
 {
-    if ((mode_get() & MODE_MASK) == MODE_PLAY)
+    // viewport height offset table for each image size
+    // 0 = 4:3, 1 = 16:9, 2 = 3:2, 3 = 1:1, 4 = 4:5
+    static long vp_yo[5] = { 0, 30, 13, 0, 0 };
+
+    int m = mode_get();
+    if ((m & MODE_MASK) == MODE_PLAY)
     {
         return 0;
     }
-    else if (shooting_get_prop(PROPCASE_SHOOTING_MODE) == 16909) // Stitch mode
+    else if (shooting_get_prop(PROPCASE_SHOOTING_MODE) == 16908) // Stitch mode
     {
-        return 0;
+        return stitch;
+    }
+    else if (mode_is_video(m))
+    {
+        return 30;
     }
     else
     {
 	    return vp_yo[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
     }
+}
+
+int vid_get_viewport_yoffset()
+{
+    return vp_yoffset(0);
 }
 
 int vid_get_viewport_display_yoffset()
 {
-    if ((mode_get() & MODE_MASK) == MODE_PLAY)
-    {
-        return 0;
-    }
-    else if (shooting_get_prop(PROPCASE_SHOOTING_MODE) == 16909) // Stitch mode
-    {
-        return 72;
-    }
-    else
-    {
-	    return vp_yo[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
-    }
+    return vp_yoffset(72);
 }
 
 // Functions for PTP Live View system
-
+int vid_get_viewport_display_xoffset_proper()   { return vid_get_viewport_display_xoffset() * 2; }
+int vid_get_viewport_display_yoffset_proper()   { return vid_get_viewport_display_yoffset() * 2; }
 int vid_get_viewport_width_proper()             { return vid_get_viewport_width() * 2; }
-int vid_get_viewport_height_proper()            { return vid_get_viewport_height(); }
+int vid_get_viewport_height_proper()            { return vid_get_viewport_height() * 2; }
+int vid_get_viewport_fullscreen_height()        { return 480; }
 int vid_get_palette_type()                      { return 5; }
 int vid_get_palette_size()                      { return 256 * 4; }
 
@@ -224,7 +219,6 @@ void *vid_get_bitmap_active_palette()
 // Function to load CHDK custom colors into active Canon palette
 void load_chdk_palette()
 {
-
     extern int active_palette_buffer;
     // Only load for the standard record and playback palettes
     if ((active_palette_buffer == 0) || (active_palette_buffer == 5) || (active_palette_buffer == 6))
@@ -252,8 +246,3 @@ void load_chdk_palette()
         }
     }
 }
-
-// SX50 Values below go in 'lib.c':
-void *vid_get_bitmap_fb()        { return (void*)0x406c5000; }             // Found @0xff056b6c
-//void *vid_get_viewport_fb()      { return (void*)0x4081ab80; }             // Found @0xff431694
-int get_flash_params_count(void) { return 0xa6; }                          // Found @0xff205ce4
