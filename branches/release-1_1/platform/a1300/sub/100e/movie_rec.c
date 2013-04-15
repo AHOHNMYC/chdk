47cd9a1cb0dba38d867d91aeebeae5ce333e75f0
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-04-15 00:09:45 +0000

    Merged revision(s) 2699 from trunk: Add microphone mute for A810, A1300 & a4000 from nafraf: http://chdk.setepontos.com/index.php?topic=650.msg99258#msg99258

--- branches/release-1_1/platform/a1300/sub/100e/movie_rec.c
+++ branches/release-1_1/platform/a1300/sub/100e/movie_rec.c
@@ -86,6 +86,7 @@ asm volatile (
 //begin patch
       "LDR	   R0, =0x637C \n"		// added, 0x6380-0x4 taken from  "loc_FF95B484:" see commented function below.
       "BL      set_quality \n"		// added
+      "BL      mute_on_zoom\n"  // added
 //end patch      
       "B       loc_FF95B6E0 \n"
 "loc_FF95B6A4:\n"  //Jump Table entry 07
