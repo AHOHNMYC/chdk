83ce53c45d605644d6ee3c06c541fff69cd35c83
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-05-10 21:57:40 +0000

    Updated loader code & fix for 'Extra Operations' menu crash bug for S5IS. http://chdk.setepontos.com/index.php?topic=9356.msg96213#msg96213

--- trunk/platform/s5is/wrappers.c
+++ trunk/platform/s5is/wrappers.c
@@ -2,15 +2,21 @@
 
 long lens_get_focus_pos()
 {
-	return _GetFocusLensSubjectDistance();
+    static long v=MAX_DIST;
+    return (kbd_is_key_pressed(KEY_SHOOT_HALF) || camera_info.state.state_kbd_script_run  || shooting_get_focus_mode())?v=_GetFocusLensSubjectDistance():v;
+//	return _GetFocusLensSubjectDistance();
 }
 
 long lens_get_focus_pos_from_lens()
 {
-	return _GetFocusLensSubjectDistanceFromLens(); 
+    static long v=MAX_DIST;
+    return (kbd_is_key_pressed(KEY_SHOOT_HALF) || camera_info.state.state_kbd_script_run  || shooting_get_focus_mode())?v=_GetFocusLensSubjectDistanceFromLens():v;
+//	return _GetFocusLensSubjectDistanceFromLens(); 
 }
 
 long lens_get_target_distance()
 {
-	return _GetCurrentTargetDistance();
+    static long v=MAX_DIST;
+    return (kbd_is_key_pressed(KEY_SHOOT_HALF) || camera_info.state.state_kbd_script_run  || shooting_get_focus_mode())?v=_GetCurrentTargetDistance():v;
+//	return _GetCurrentTargetDistance();
 }
