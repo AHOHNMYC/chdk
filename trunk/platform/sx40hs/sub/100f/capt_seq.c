af27fe1bd7f8a096569f31e3509404934b2c1ce8
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-06-07 02:30:39 +0000

    Add _nrflag definition to stubs_entry.S so it can be used like other stubs values. Update generic/capt_seq.c to optionally use the stubs value (enabled on G12, G1X, IXUS310, SX40 & SX50).

--- trunk/platform/sx40hs/sub/100f/capt_seq.c
+++ trunk/platform/sx40hs/sub/100f/capt_seq.c
@@ -5,9 +5,9 @@
 #include "platform.h"
 #include "core.h"
 
-static long *nrflag = (long*)(0xC3F8+0x0);  // See stubs_entry.S
-#define NR_AUTO (0)							// have to explictly reset value back to 0 to enable auto
-#define PAUSE_FOR_FILE_COUNTER 150          // Enable delay in capt_seq_hook_raw_here to ensure file counter is updated
+#define USE_STUBS_NRFLAG 1          // see stubs_entry.S
+#define NR_AUTO (0)                 // have to explictly reset value back to 0 to enable auto
+#define PAUSE_FOR_FILE_COUNTER 150  // Enable delay in capt_seq_hook_raw_here to ensure file counter is updated
 
 #include "../../../generic/capt_seq.c"
 
