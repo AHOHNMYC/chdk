92310a0c884a6282adc4d5a2dd6977c1118e5a7b
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-06-09 03:46:04 +0000

    Custom CHDK palette colors for G10 & IXUS120 from waterwingz: http://chdk.setepontos.com/index.php?topic=650.msg101456#msg101456

--- platform/ixus120_sd940/platform_camera.h
+++ platform/ixus120_sd940/platform_camera.h
@@ -49,7 +49,10 @@
     // color
 
     #undef  CAM_BITMAP_PALETTE
-    #define CAM_BITMAP_PALETTE              3
+    #define CAM_BITMAP_PALETTE              16
+
+    #define CAM_LOAD_CUSTOM_COLORS          1      // Enable loading CHDK colors into the camera palette memory/hardware
+    #define CHDK_COLOR_BASE                 0xB3   // Start color index for CHDK colors loaded into camera palette.
 
     #undef  CAM_MENU_BORDERWIDTH
     #define CAM_MENU_BORDERWIDTH            10
