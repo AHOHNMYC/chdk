bce37b887f5cb18b386ed29ae4c279680d9f014e
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-06-10 01:59:16 +0000

    remote capture - add a1300 and a4000 filewrite support, clean up hook code for a810, from nafraf in http://chdk.setepontos.com/index.php?topic=4338.msg101503#msg101503

--- branches/ptp-remote-capture-test4/platform/a810/sub/100b/boot.c
+++ branches/ptp-remote-capture-test4/platform/a810/sub/100b/boot.c
@@ -14,17 +14,18 @@ extern void task_InitFileModules();
 extern void task_MovieRecord();
 extern void task_ExpDrv();
 extern void task_PhySw();
+extern void task_FileWrite();
 
 void taskHook(context_t **context) {
 
     task_t *tcb=(task_t*)((char*)context-offsetof(task_t, context));
 
-	if(tcb->entry == (void*)task_PhySw)            tcb->entry = (void*)mykbd_task;
-	if(tcb->entry == (void*)task_CaptSeq)          tcb->entry = (void*)capt_seq_task; 
-	if(tcb->entry == (void*)task_InitFileModules)  tcb->entry = (void*)init_file_modules_task;
-	if(tcb->entry == (void*)task_MovieRecord)      tcb->entry = (void*)movie_record_task;
-	if(tcb->entry == (void*)task_ExpDrv)           tcb->entry = (void*)exp_drv_task;
-	if(tcb->entry == (void*)0xFFA523F4)            tcb->entry = (void*)filewritetask;
+    if(tcb->entry == (void*)task_PhySw)            tcb->entry = (void*)mykbd_task;
+    if(tcb->entry == (void*)task_CaptSeq)          tcb->entry = (void*)capt_seq_task;
+    if(tcb->entry == (void*)task_InitFileModules)  tcb->entry = (void*)init_file_modules_task;
+    if(tcb->entry == (void*)task_MovieRecord)      tcb->entry = (void*)movie_record_task;
+    if(tcb->entry == (void*)task_ExpDrv)           tcb->entry = (void*)exp_drv_task;
+    if(tcb->entry == (void*)task_FileWrite)        tcb->entry = (void*)filewritetask;
 }
 
 #define LED_GREEN 		0xC0220120
