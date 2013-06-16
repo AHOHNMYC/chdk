64d1bdccb7ef3d23c4c2bc43fdfaaf3bf610f6a5
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-06-16 00:13:40 +0000

    Palette updates for IXUS100 & IXUS120 from waterwingz: http://chdk.setepontos.com/index.php?topic=650.msg101649#msg101649

--- trunk/platform/ixus120_sd940/lib.c
+++ trunk/platform/ixus120_sd940/lib.c
@@ -79,7 +79,7 @@ void load_chdk_palette()
     extern char** palette_buffer_ptr;
     extern int active_palette_buffer;
 
-    if ((active_palette_buffer == 0) || (active_palette_buffer == 5))
+    if ((active_palette_buffer == 0) || (active_palette_buffer == 5)|| (active_palette_buffer == 8))  // Note : palette 4 (Canon Set Button Menu) colors conflict so not included here
     {
         int *pal = (int*)vid_get_bitmap_active_palette();
         if (pal[CHDK_COLOR_BASE+0] != 0x33ADF62)
