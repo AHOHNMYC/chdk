1973cb0c13b647cd735ca808d86f22d4f7911eb8
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-08-12 00:37:27 +0000

    preliminary fix for various remote capture issues, especially in continuous mode. blocks raw hook until preceding jpeg is completely done. discussion: http://chdk.setepontos.com/index.php?topic=4338.msg103785#msg103785

--- include/remotecap_core.h
+++ include/remotecap_core.h
@@ -11,6 +11,7 @@ void remotecap_is_ready(int *available_type,int *image_num);
 // call from task hooks to notify remotecap of data
 void remotecap_raw_available(char *rawadr);
 void remotecap_jpeg_available(void);
+void remotecap_type_complete(int type);
 // called by ptp code to get next chunk address/size for the format (fmt) that is being currently worked on
 int remotecap_get_data_chunk( int fmt, char **addr, unsigned int *size, int *pos );
 // called by ptp code to notify remotecap that chunk send is complete
