6a5a15a4f190cff3dd57692cd8407a82bd152569
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-09-04 05:35:10 +0000

    ixus140_elph130 - implement some viewport functions, palette for PTP

--- platform/ixus140_elph130/lib.c
+++ platform/ixus140_elph130/lib.c
@@ -1,37 +1,25 @@
-void *vid_get_bitmap_fb()        { return (void*)0x406b1000; }             // Found @0xff08b550
-void *vid_get_viewport_fb()      { return (void*)0x40806b80; }             // Found @0xff412680
-void *vid_get_viewport_live_fb() { return (void *)0;}
-void *vid_get_viewport_fb_d()
+#include "platform.h"
+#include "lolevel.h"
+
+void vid_bitmap_refresh()
 {
-    extern char *viewport_fb_d;
-	return viewport_fb_d;
+    extern int full_screen_refresh;
+    extern void _ScreenLock();
+    extern void _ScreenUnlock();
+
+    full_screen_refresh |= 3;
+    _ScreenLock();
+    _ScreenUnlock();
 }
 
-char *camera_jpeg_count_str()
+// TODO not really complete, last call from task_Bye
+void shutdown()
 {
-    extern char jpeg_count_str[];
-	return jpeg_count_str;
+    extern void _finish_shutdown(void);
+    _finish_shutdown();
+    while(1);
 }
 
-char *hook_raw_image_addr()
-{
-    return (char*)0x42365c30; //(Found @0xff413f98)
-}
-
-// TODO C&P, likely wrong
-int vid_get_viewport_width()
-{
-	return 360;
-}
-
-long vid_get_viewport_height()
-{
-    return 240;
-}
-
-
-long hook_raw_size()             { return 0x01794300; }                    // Found @0xff413ff0
-int get_flash_params_count(void) { return 0x91; }                          // Found @0xff2075ac
 
 // TODO AF led for now
 #define LED_PR 0xc0223030 // red LED, no DP
@@ -45,14 +33,6 @@ void debug_led(int state)
 	p[0]=0x44;
 }
 
-// TODO not really complete, last call from task_Bye
-void shutdown()
-{
-    extern void _finish_shutdown(void);
-    _finish_shutdown();
-    while(1);
-}
-
 // TODO
 void camera_set_led(int led, int state, int bright) {
  /*
@@ -61,14 +41,172 @@ void camera_set_led(int led, int state, int bright) {
  */
 }
 
-void vid_bitmap_refresh()
+char *camera_jpeg_count_str()
 {
-    extern int full_screen_refresh;
-    extern void _ScreenLock();
-    extern void _ScreenUnlock();
-
-    full_screen_refresh |= 3;
-    _ScreenLock();
-    _ScreenUnlock();
+    extern char jpeg_count_str[];
+	return jpeg_count_str;
 }
 
+int get_flash_params_count(void) { return 0x91; }                          // Found @0xff2075ac
+
+
+void *vid_get_bitmap_fb()        { return (void*)0x406b1000; }             // Found @0xff08b550
+void *vid_get_viewport_fb()      { return (void*)0x40806b80; }             // Found @0xff412680
+void *vid_get_viewport_live_fb() { return (void *)0;} // TODO
+void *vid_get_viewport_fb_d()
+{
+    extern char *viewport_fb_d;
+	return viewport_fb_d;
+}
+
+
+char *hook_raw_image_addr()
+{
+    return (char*)0x42365c30; //(Found @0xff413f98)
+}
+
+long hook_raw_size()             { return 0x01794300; }                    // Found @0xff413ff0
+
+// TODO 
+// Y multiplier for cameras with 480 pixel high viewports (CHDK code assumes 240)
+int vid_get_viewport_yscale() {
+	return 2;               // G12 viewport is 480 pixels high
+}
+
+int vid_get_viewport_width()
+{
+    if ((mode_get() & MODE_MASK) == MODE_PLAY)
+    {
+        return 360;
+    }
+    extern int _GetVRAMHPixelsSize();
+    return _GetVRAMHPixelsSize() >> 1;
+}
+
+long vid_get_viewport_height()
+{
+    if ((mode_get() & MODE_MASK) == MODE_PLAY)
+    {
+        return 240;
+    }
+    extern int _GetVRAMVPixelsSize();
+    return _GetVRAMVPixelsSize() >> 1;
+}
+
+// viewport width offset table for each image size
+// 0 = 4:3, 1 = 16:9, 2 = 3:2, 3 = 1:1
+static long vp_xo[5] = { 0, 0, 0, 44 };				// should all be even values for edge overlay
+
+int vid_get_viewport_xoffset()
+{
+    int m = mode_get();
+    if ((m & MODE_MASK) == MODE_PLAY)
+    {
+        return 0;
+    }
+    else if ((m & MODE_SHOOTING_MASK) == MODE_STITCH)
+    {
+        return 0;
+    }
+    else
+    {
+        return 0;
+//	    return vp_xo[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
+    }
+}
+
+int vid_get_viewport_display_xoffset()
+{
+    int m = mode_get();
+    if ((m & MODE_MASK) == MODE_PLAY)
+    {
+        return 0;
+    }
+    else if ((m & MODE_SHOOTING_MASK) == MODE_STITCH)
+    {
+        if (shooting_get_prop(PROPCASE_STITCH_DIRECTION) == 0)      // Direction check
+            if (shooting_get_prop(PROPCASE_STITCH_SEQUENCE) == 0)   // Shot already taken?
+                return 40;
+            else
+                return 140;
+        else
+            if (shooting_get_prop(PROPCASE_STITCH_SEQUENCE) == 0)   // Shot already taken?
+                return 140;
+            else
+                return 40;
+    }
+    else
+    {
+	    return vp_xo[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
+    }
+}
+
+// viewport height offset table for each image size
+// 0 = 4:3, 1 = 16:9, 2 = 3:2, 3 = 1:1
+static long vp_yo[5] = { 0, 30, 13, 0 };
+
+int vid_get_viewport_yoffset()
+{
+    int m = mode_get();
+    if ((m & MODE_MASK) == MODE_PLAY)
+    {
+        return 0;
+    }
+    else if ((m & MODE_SHOOTING_MASK) == MODE_STITCH)
+    {
+        return 0;
+    }
+    else if (mode_is_video(m))
+    {
+        return 0;
+    }
+    else
+    {
+	    return vp_yo[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
+    }
+}
+
+int vid_get_viewport_display_yoffset()
+{
+    int m = mode_get();
+    if ((m & MODE_MASK) == MODE_PLAY)
+    {
+        return 0;
+    }
+    else if ((m & MODE_SHOOTING_MASK) == MODE_STITCH)
+    {
+        return 72;
+    }
+    else if (mode_is_video(m))
+    {
+        return 30;
+    }
+    else
+    {
+	    return vp_yo[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
+    }
+}
+
+int vid_get_viewport_display_yoffset_proper()   { return vid_get_viewport_display_yoffset() * 2; }
+int vid_get_viewport_height_proper()            { return vid_get_viewport_height() * 2; }
+int vid_get_viewport_fullscreen_height()        { return 480; }
+
+int vid_get_palette_type()                      { return 3; }
+int vid_get_palette_size()                      { return 256 * 4; }
+
+// TODO not found
+/*
+void *vid_get_bitmap_active_buffer()
+{
+    return bitmap_buffer[active_bitmap_buffer];
+}
+*/
+
+void *vid_get_bitmap_active_palette()
+{
+    extern int active_palette_buffer;
+    extern int** palette_buffer_ptr;
+    return (palette_buffer_ptr[active_palette_buffer]+1);
+}
+
+// TODO void load_chdk_palette(), palette_control not found
