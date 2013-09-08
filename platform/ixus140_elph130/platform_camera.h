4943b01cbdba5fb6e457f0ffadabfc15e832763d
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-09-08 06:39:27 +0000

    ixus140_elph130 - implement custom palette and active bitmap buffer functions

--- platform/ixus140_elph130/platform_camera.h
+++ platform/ixus140_elph130/platform_camera.h
@@ -85,12 +85,15 @@
     #undef CAM_BITMAP_WIDTH
     #define CAM_BITMAP_WIDTH                720 // Actual width of bitmap screen in bytes
 
-//    #undef  CAM_BITMAP_PALETTE
-//    #define CAM_BITMAP_PALETTE                  13
+    #define DRAW_ON_ACTIVE_BITMAP_BUFFER_ONLY   1   // Draw pixels on active bitmap buffer only.
 
-//    TODO
-//    #define CAM_LOAD_CUSTOM_COLORS              1   // Enable loading CHDK colors into the camera palette memory/hardware
-//    #define CHDK_COLOR_BASE                     0xCE// Start color index for CHDK colors loaded into camera palette.
+
+    #undef  CAM_BITMAP_PALETTE
+    #define CAM_BITMAP_PALETTE                  13
+
+    #define CAM_LOAD_CUSTOM_COLORS              1   // Enable loading CHDK colors into the camera palette memory/hardware
+    // doesn't seem to conflict badly with anything
+    #define CHDK_COLOR_BASE                     0x73 // Start color index for CHDK colors loaded into camera palette.
 
     // TODO - likely, not tested
     #define CAM_DRIVE_MODE_FROM_TIMER_MODE  1   // use PROPCASE_TIMER_MODE to check for multiple shot custom timer.
