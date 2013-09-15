29d6527c415d75b0fea6b5a1bd5e2c854f7bdebc
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-09-15 21:30:51 +0000

    ixus140_elph130 - implement extra long exposure support

--- trunk/platform/ixus140_elph130/platform_camera.h
+++ trunk/platform/ixus140_elph130/platform_camera.h
@@ -67,6 +67,8 @@
     #undef  CAM_HAS_USER_TV_MODES
     #define CAM_SHOW_OSD_IN_SHOOT_MENU      1
 
+    #define  CAM_EXT_TV_RANGE               1    // CHDK can make exposure time longer than 64s
+
     #undef  CAM_VIDEO_CONTROL
 // not working
     #undef CAM_CHDK_HAS_EXT_VIDEO_MENU
