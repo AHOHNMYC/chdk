790c0f4ee30df5ae183a90e76a136e21f323dc19
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-11-11 00:22:08 +0000

    a495 update: fix liveview, remove KEY_PRINT alias

--- platform/a495/lib.c
+++ platform/a495/lib.c
@@ -1,6 +1,16 @@
 #include "platform.h"
 #include "lolevel.h"
 
+char *hook_raw_image_addr()
+{
+    return (char*) 0x10E52420;
+}
+
+/*long hook_raw_size() {
+    //CAM_RAW_ROWS * ((CAM_RAW_ROWPIX*CAM_SENSOR_BITS_PER_PIXEL)/8)
+    return 0x00ec04f0;
+}*/
+
 // only two LEDs in A495:
 #define LED_PR 0xC0220088  // green LED
 #define LED_AF 0xC0220080  // orange AF LED
@@ -36,6 +46,13 @@ void camera_set_led(int led, int state, int bright) {
     if(state<=1) _LEDDrive(led_table[led%sizeof(led_table)], (!state)&1);
 }
 
+char *camera_jpeg_count_str()
+{
+    extern char jpeg_count_str[];
+    return jpeg_count_str;
+}
+
+
 void vid_bitmap_refresh() {
     extern int enabled_refresh_physical_screen;
     extern int full_screen_refresh;
@@ -54,9 +71,9 @@ void vid_bitmap_refresh() {
     _ScreenUnlock();
 }
 
-// Near "PropertyTableManagerCore.c" ROM:FFD2ED94
+
 int get_flash_params_count(void){
-    return 0x74; 
+    return 0x94;  //found by finsig
 }
 
 
@@ -66,27 +83,11 @@ int _GetBatteryTemperature()
       return -99;
 }
 
-/*void vid_turn_off_updates()
-{
-    extern void _LockAndRefresh();   // wrapper function for screen lock
-
-    _LockAndRefresh();
-}
-
-void vid_turn_on_updates()
-{
-    extern void _UnlockAndRefresh();   // wrapper function for screen unlock
-
-    //_RefreshPhysicalScreen(1);
-    _UnlockAndRefresh();
-}*/
-
 // Functions for PTP Live View system
 
 void *vid_get_bitmap_active_palette() {
         return (void*)(*(int*)(0x4970+0x20));  //Found @ 0xffcd1e80 a495 100f
 }
-
 
 // 64 entry palette based on 100f 0xffcd1e80
 int vid_get_palette_type()                      { return 4; }
@@ -96,3 +97,77 @@ void *vid_get_bitmap_active_buffer()
 {
     return (void*)(*(int*)(0x4970+0xC)); //found @ 0xffcd1f3c a495 100f
 }
+
+
+// Viewport and Bitmap values that shouldn't change across firmware versions.
+// Values that may change are in lib.c for each firmware version.
+
+// Defined in stubs_min.S
+extern char active_viewport_buffer;
+extern void* viewport_buffers[];
+
+void *vid_get_viewport_fb()
+{
+    // Return first viewport buffer - for case when vid_get_viewport_live_fb not defined
+    return viewport_buffers[0];
+}
+
+void *vid_get_viewport_live_fb()
+{
+    if (MODE_IS_VIDEO(mode_get()) || is_video_recording())
+        return viewport_buffers[0];     // Video only seems to use the first viewport buffer.
+
+    // Hopefully return the most recently used viewport buffer so that motion detect, histogram, zebra and edge overly are using current image data
+    return viewport_buffers[(active_viewport_buffer)&3];
+}
+
+void *vid_get_viewport_fb_d()
+{
+    extern char *viewport_fb_d;
+    return viewport_fb_d;
+}
+
+void *vid_get_bitmap_fb(){
+    return (void*)0x10361000; // Found @0xffc3f020
+}
+
+int vid_get_viewport_yoffset() {
+    return 0;
+}
+
+extern int _GetVRAMHPixelsSize();
+extern int _GetVRAMVPixelsSize();
+
+int vid_get_viewport_width() {
+    return 360;
+}
+
+int vid_get_viewport_width_proper() {
+    return ((mode_get()&MODE_MASK) == MODE_PLAY)?720:_GetVRAMHPixelsSize();
+}
+
+long vid_get_viewport_height() {
+    return 240;
+}
+
+int vid_get_viewport_height_proper() {
+    int m = mode_get();
+    // special case: W resolution
+    if ((m&MODE_MASK) != MODE_PLAY && shooting_get_prop(PROPCASE_RESOLUTION) == 8) {
+        return 240;
+    }
+    return ((mode_get()&MODE_MASK) == MODE_PLAY)?240:_GetVRAMVPixelsSize();
+}
+
+int vid_get_viewport_fullscreen_height() {
+    int m = mode_get();
+    // special case: W resolution
+    if ((m&MODE_MASK) != MODE_PLAY && shooting_get_prop(PROPCASE_RESOLUTION) == 8) {
+        return 120;
+    }
+    return ((mode_get()&MODE_MASK) == MODE_PLAY)?240:_GetVRAMVPixelsSize();
+}
+
+int vid_get_viewport_display_xoffset_proper()   { return vid_get_viewport_display_xoffset(); }
+int vid_get_viewport_display_yoffset_proper()   { return vid_get_viewport_display_yoffset(); }
+
