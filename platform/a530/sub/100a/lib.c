530d2a86f6e5466c05edef413ee26ef3476a2670
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2013-12-15 02:21:09 +0000

    a530 - ptp live view functions from blackhole in http://chdk.setepontos.com/index.php?topic=650.msg107920#msg107920

--- platform/a530/sub/100a/lib.c
+++ platform/a530/sub/100a/lib.c
@@ -63,3 +63,44 @@ char *camera_jpeg_count_str() /* XXXA530XXX */
 {
     return (char*)0x6E820;
 }
+
+//PTP
+extern int _GetVRAMHPixelsSize();
+extern int _GetVRAMVPixelsSize();
+
+int vid_get_palette_type() { return 1; }
+int vid_get_palette_size() { return 16*4; }
+
+void *vid_get_bitmap_active_palette() {
+    return (void *)0x63450; // GetPaletteFromPhysicalScreen -> FFD12B30
+}
+
+void *vid_get_bitmap_active_buffer()
+{
+    return (void*)(*(int*)0x5EFC); // DisplayPhysicalScreenWithYUVPalette -> FFD12AE4
+}
+
+int vid_get_viewport_width_proper() { 
+    return ((mode_get()&MODE_MASK) == MODE_PLAY)?720:_GetVRAMHPixelsSize();
+}
+int vid_get_viewport_height_proper() {
+    return ((mode_get()&MODE_MASK) == MODE_PLAY)?240:_GetVRAMVPixelsSize();
+}
+
+int vid_get_viewport_fullscreen_height() {
+    // except for stitch, always full screen
+    int m = mode_get();
+    if((m&MODE_MASK) != MODE_PLAY && (m&MODE_SHOOTING_MASK) == MODE_SCN_STITCH) {
+        return 240;
+    }
+    return vid_get_viewport_height_proper();
+}
+int vid_get_viewport_fullscreen_width() {
+    // except for stitch, always full screen
+    int m = mode_get();
+    if((m&MODE_MASK) != MODE_PLAY && (m&MODE_SHOOTING_MASK) == MODE_SCN_STITCH) {
+        return 720;
+    }
+    return vid_get_viewport_width_proper();
+}
+
