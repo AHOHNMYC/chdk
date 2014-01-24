d5dcb8dd6d87247521a42b2e173ac9baaccb1478
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-01-24 04:47:32 +0000

    ixus230_elph310hs - fix mode switching in PTP

--- platform/ixus230_elph310hs/platform_camera.h
+++ platform/ixus230_elph310hs/platform_camera.h
@@ -103,3 +103,5 @@
 #define REMOTE_SYNC_STATUS_LED 	0xC0220130		// specifies an LED that turns on while camera waits for USB remote to sync
 #define CAM_HAS_CMOS                   1
 
+#undef  CAM_USB_EVENTID
+#define CAM_USB_EVENTID                         0x202 // Levent ID for USB control. Changed in DryOS R49 so needs to be overridable.
