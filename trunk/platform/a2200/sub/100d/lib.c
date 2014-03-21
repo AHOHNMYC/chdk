d986a5caa8c4b1674d86b078d9261414892cd3c4
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-03-21 05:51:44 +0000

    Fix a2200 liveview

--- trunk/platform/a2200/sub/100d/lib.c
+++ trunk/platform/a2200/sub/100d/lib.c
@@ -29,7 +29,8 @@ void *vid_get_bitmap_fb() {
 	
 	return (void*)0x403F1000;	// Found @0xFF85B81C
 }	
-	
+// Those functions were replaced by their generic implementation in ../lib.c
+/*
 void *vid_get_viewport_live_fb() {
 	
 	return (void*)(*(int*)(0x20D0+0x138));		//FF850C88 + FF850CD8
@@ -39,20 +40,10 @@ void *vid_get_viewport_live_fb() {
 void *vid_get_viewport_fb() {
 	
 	return (void*)0x40546B80;	// Found @0xFFB17310
-}		
+}
 		
 void *vid_get_viewport_fb_d() {
 	
 	return (void*)(*(int*)(0x29A0+0x58));     // FF870CB0 +  FF870D38
 }
-
-void vid_bitmap_refresh() {
-	
-	extern int full_screen_refresh;
-	extern void _ScreenUnlock();
-	extern void _ScreenLock();
-
-	full_screen_refresh |= 3;
-	_ScreenLock();
-	_ScreenUnlock();
-}
+*/
