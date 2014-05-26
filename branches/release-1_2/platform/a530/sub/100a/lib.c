0f27386cbabba7108bd29426a33cd6fe5c920525
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-05-26 14:29:56 +0000

    Merged revision 3447 from trunk: a530: Fix vid_get_viewport_live_fb()

--- branches/release-1_2/platform/a530/sub/100a/lib.c
+++ branches/release-1_2/platform/a530/sub/100a/lib.c
@@ -30,8 +30,16 @@ long hook_raw_size()//
 }
 
 void *vid_get_viewport_live_fb()
-{
-    return (void*)0x0;
+{   //Found at 0xffc902d4 - similar to a530
+    void **fb=(void **)0x5288;
+    unsigned char buff = *((unsigned char*)0x5298);
+    if (buff == 0) {
+        buff = 2;
+    }
+    else {
+        buff--;
+    }
+    return fb[buff];
 }
 
 void *vid_get_bitmap_fb()//
