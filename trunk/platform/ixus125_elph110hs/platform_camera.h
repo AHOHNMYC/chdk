d3b5c57e537efe876faf9dd4ac4d4f7c74657f42
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-06-22 23:18:44 +0000

    IXUS125 updates from waterwingz: http://chdk.setepontos.com/index.php?topic=650.msg113792#msg113792

--- trunk/platform/ixus125_elph110hs/platform_camera.h
+++ trunk/platform/ixus125_elph110hs/platform_camera.h
@@ -117,5 +117,7 @@
     #define CAM_ADJUSTABLE_ALT_BUTTON       	1
     #define CAM_ALT_BUTTON_NAMES            	{ "Playback", "Video" }
     #define CAM_ALT_BUTTON_OPTIONS          	{ KEY_PLAYBACK, KEY_VIDEO }
+    #define CAM_DISP_BUTTON_NAME                "Video"
+    #define SHORTCUT_TOGGLE_RAW                 KEY_VIDEO
  
 //--------------------------------------------------
