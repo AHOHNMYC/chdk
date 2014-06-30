6b70b376c4cd4ead114fd6e0081dcbc87349e8fa
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-06-30 16:06:32 +0000

    a2200 update: zoom definitions, CAM_DNG_LENS_INFO, and conversion factor lens numbers.

--- trunk/platform/a2200/main.c
+++ trunk/platform/a2200/main.c
@@ -24,37 +24,35 @@ void startup() {
 	boot();
 }
 
-//static const int fl_tbl[] = {5000, 6400, 7400, 9100, 10900, 13000, 15700, 20000};
-//#define NUM_FL (sizeof(fl_tbl)/sizeof(fl_tbl[0]))
-
 // Focus length table in firmware @FFFE2A98 (1.00B)
 #define NUM_FL 8
-extern int focus_len_table[NUM_FL*3];
+#define NUM_DATA    3   // 3 words each entry, first is FL
+extern int focus_len_table[NUM_FL*NUM_DATA];
 
-// A2200 focal length range is 5 - 20 mm (28 - 112 mm with lense), so CF_EFL = 28/5*10000=56000
-#define CF_EFL 56000
-
+// Conversion factor lens FL --> 35mm equiv
+// lens      35mm     CF
+// ----      ----     --
+//  5.0       28      ( 28/ 5.0) * 50 = 280  (min FL)
+// 20.0      112      (112/20.0) * 50 = 280  (max FL)
+#define CF_EFL      280
+#define CF_EFL_DIV  50
 const int zoom_points = NUM_FL;
 
 int get_effective_focal_length(int zp) {
-	
-    return (CF_EFL*get_focal_length(zp))/10000;
+    return (CF_EFL*get_focal_length(zp))/CF_EFL_DIV;
 }
 
 int get_focal_length(int zp) {
-	
 	if (zp < 0) zp = 0;
 	else if (zp >= NUM_FL) zp = NUM_FL-1;
-	return focus_len_table[zp*3];
+	return focus_len_table[zp*NUM_DATA];
 }
 
 int get_zoom_x(int zp) {
-	
 	return get_focal_length(zp)*10/focus_len_table[0];
 }
 
 long get_vbatt_min() {
-	
 	return 3000;	// Measured on the battery without load (3350)
 }
 
--- trunk/platform/a2200/platform_camera.h
+++ trunk/platform/a2200/platform_camera.h
@@ -40,7 +40,7 @@
 	#define CAM_ACTIVE_AREA_X2	4352
 	#define CAM_ACTIVE_AREA_Y2	3275
 	
-	#define	CAM_DNG_LENS_INFO	{ 50,10, 200,10, 27,10, 56,10 } // See comments in camera.h
+	#define	CAM_DNG_LENS_INFO	{ 50,10, 200,10, 28,10, 59,10 } // See comments in camera.h
 	
 	// Almost sure values
 	//
@@ -118,7 +118,10 @@
     #define CAM_HAS_ZOOM_LEVER               1    // Camera has dedicated zoom buttons
     #define CAM_USE_ZOOM_FOR_MF              1    // Zoom lever can be used for manual focus adjustments
     #define CAM_NEED_SET_ZOOM_DELAY          300  // Define to add a delay after setting the zoom position
-    #define CAM_USE_OPTICAL_MAX_ZOOM_STATUS  1    // Use ZOOM_OPTICAL_MAX to reset zoom_status when switching from digital to optical zoo
+    #define CAM_USE_OPTICAL_MAX_ZOOM_STATUS  1    // Use ZOOM_OPTICAL_MAX to reset zoom_status when switching from digital to optical zoom
+    #define CAM_USE_ALT_SET_ZOOM_POINT       1    // Define to use the alternate code in lens_set_zoom_point()
+    #define CAM_USE_ALT_PT_MoveOpticalZoomAt 1    // Define to use the PT_MoveOpticalZoomAt() function in lens_set_zoom_point()
+
     #define CAM_CAN_MUTE_MICROPHONE          1
 
     #define CAM_HAS_FILEWRITETASK_HOOK       1
