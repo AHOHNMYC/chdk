b609d5e5c90e7e024297cabcc1ca8e56421c35f5
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-07-15 09:42:06 +0000

    Merge revision 3508 from trunk. Updated stubs_entry.S & bin_compat.h files (from full firmware dumps supplied by koshy).

--- platform/s2is/sub/100f/bin_compat.h
+++ platform/s2is/sub/100f/bin_compat.h
@@ -4,4 +4,5 @@ ver_sig_t ver_sigs[]={
     {(const char *)0xff81adad, "GM1.00F"},
 };
 pid_sig_t pid_sigs[]={
+    {(short *)0xffd70130, PLATFORMID},
 };
--- platform/tx1/sub/101b/bin_compat.h
+++ platform/tx1/sub/101b/bin_compat.h
@@ -4,4 +4,5 @@ ver_sig_t ver_sigs[]={
     {(const char *)0xff8180b9, "GM1.01B"},
 };
 pid_sig_t pid_sigs[]={
+    {(short *)0xfffe0130, PLATFORMID},
 };
