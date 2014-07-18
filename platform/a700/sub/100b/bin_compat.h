2d9aa97158b9c22a4c6a29eec430784bb4b3c3d3
srsa <srsa@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-07-18 21:13:47 +0000

    updated stubs_entry.S and bin_compat.h (a560 100a, a640 100b, a700 100b, a710 100a, ixus800_sd700 100b) from full dumps provided by koshy - trunk r3519

--- platform/a700/sub/100b/bin_compat.h
+++ platform/a700/sub/100b/bin_compat.h
@@ -4,4 +4,5 @@ ver_sig_t ver_sigs[]={
     {(const char *)0xffc0a211, "GM1.00B"},
 };
 pid_sig_t pid_sigs[]={
+    {(short *)0xfffe0130, PLATFORMID},
 };
--- platform/ixus800_sd700/sub/100b/bin_compat.h
+++ platform/ixus800_sd700/sub/100b/bin_compat.h
@@ -4,4 +4,5 @@ ver_sig_t ver_sigs[]={
     {(const char *)0xff81a5d1, "GM1.00B"},
 };
 pid_sig_t pid_sigs[]={
+    {(short *)0xfffe0130, PLATFORMID},
 };
