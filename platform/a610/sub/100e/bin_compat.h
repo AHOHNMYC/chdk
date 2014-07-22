89717c7a899fab77a19c03b16ef1e2f44914cc08
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-07-22 08:52:05 +0000

    Updated stubs_entry.S & bin_compat.h files (from full firmware dumps supplied by koshy).

--- platform/a610/sub/100e/bin_compat.h
+++ platform/a610/sub/100e/bin_compat.h
@@ -5,4 +5,5 @@ ver_sig_t ver_sigs[]={
     {(const char *)0xffc0a1b9, "GM1.00D"},
 };
 pid_sig_t pid_sigs[]={
+    {(short *)0xfffe0130, PLATFORMID},
 };
--- platform/ixus750_sd550/sub/100g/bin_compat.h
+++ platform/ixus750_sd550/sub/100g/bin_compat.h
@@ -4,4 +4,5 @@ ver_sig_t ver_sigs[]={
     {(const char *)0xff81addd, "GM1.00G"},
 };
 pid_sig_t pid_sigs[]={
+    {(short *)0xfffe0130, PLATFORMID},
 };
