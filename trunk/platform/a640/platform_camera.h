69e466efe4ce6dac04a294e9994b28235c0ddec7
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-07-23 18:47:51 +0000

    a640 update: - Simpler loader - capt_seq.c rewritten using code_gen. exp_drv_task implemented

--- trunk/platform/a640/platform_camera.h
+++ trunk/platform/a640/platform_camera.h
@@ -56,6 +56,8 @@
     // camera name
     #define PARAM_CAMERA_NAME               4       // parameter number for GetParameterData
     #define DNG_EXT_FROM                    ".DPS"
+
+    #define CAM_EXT_TV_RANGE                1
 	
 //	#define REMOTE_SYNC_STATUS_LED 	0xC0xxyyyy		// specifies an LED that turns on while camera waits for USB remote to sync
 	
