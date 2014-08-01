fe9271b8cb747400230c6192b955ece7e2eec63c
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-08-01 15:12:33 +0000

    a650 update: - Simpler loader code - remote capture supported - boot.c, and capt_seq.c rewritten using code_gen

--- trunk/platform/a650/platform_camera.h
+++ trunk/platform/a650/platform_camera.h
@@ -63,6 +63,10 @@
 
     #define CAM_MULTIPART                   1
 
+    #define CAM_EXT_TV_RANGE                1
+
+    #define CAM_HAS_FILEWRITETASK_HOOK      1
+
 	#define REMOTE_SYNC_STATUS_LED 	0xc02200C4		// specifies an LED that turns on while camera waits for USB remote to sync
 	
  
