5fb1b957d2f3f25541e9ff17b0a800b388972ba5
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-08-05 15:47:08 +0000

    ixus900_sd900 updated: - Simpler loader code - Fix modemap table - capt_seq.c rewritten using code_gen - Add long time exposure support - Add remote capture support - stubs_entry_2.S cleanup Test report: http://chdk.setepontos.com/index.php?topic=11723.msg115324#msg115324

--- trunk/platform/ixus900_sd900/platform_camera.h
+++ trunk/platform/ixus900_sd900/platform_camera.h
@@ -68,6 +68,10 @@
     #define PARAM_CAMERA_NAME               4       // camera param id of EXIF camera model
 
     #define DNG_EXT_FROM                    ".DPS"
+
+    #define CAM_EXT_TV_RANGE                1
+
+    #define CAM_HAS_FILEWRITETASK_HOOK      1
 	
 //	#define REMOTE_SYNC_STATUS_LED 	0xC0xxyyyy		// specifies an LED that turns on while camera waits for USB remote to sync
 	
