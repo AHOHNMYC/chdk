0004f77965a466956f44370ab0a45e2851e2eb7d
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-08-09 01:43:49 +0000

    powershot N updates from waterwingz in http://chdk.setepontos.com/index.php?topic=650.msg115469#msg115469 * fix image directory names * correct zoomin / zoomout key assignment

--- platform/n/shooting.c
+++ platform/n/shooting.c
@@ -110,7 +110,7 @@ long get_target_file_num() {
 void get_target_dir_name(char *out) {
 
     extern void _GetImageFolder(char*,int,int,int);
-    _GetImageFolder(out,get_file_next_counter(),0x400,time(NULL));
+    _GetImageFolder(out,get_file_next_counter(),CAM_DATE_FOLDER_NAMING,time(NULL));
 }
 #else
 long get_target_dir_num() {
