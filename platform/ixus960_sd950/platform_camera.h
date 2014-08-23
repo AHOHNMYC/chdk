884fd825594955d8700c47519d1f4f0c2b4a42e4
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-08-23 16:43:08 +0000

    ixus960_sd950 update: * Simpler loader code * boot.c, capt_seq.c and movie_rec.c rewritten using code_gen * add long time exposure support * add filewrite task support Tested by koshy: http://chdk.setepontos.com/index.php?topic=11723.msg116055#msg116055

--- platform/ixus960_sd950/platform_camera.h
+++ platform/ixus960_sd950/platform_camera.h
@@ -59,6 +59,10 @@
 
     #define CAM_MULTIPART                   1
 
+    #define CAM_EXT_TV_RANGE                1
+
+    #define CAM_HAS_FILEWRITETASK_HOOK      1
+    
 	#define REMOTE_SYNC_STATUS_LED 	0xC02200CC		// specifies an LED that turns on while camera waits for USB remote to sync
 	
  
