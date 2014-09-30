a4a21865aa75c3ab8f22ab717fe21174bb2c4350
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-09-30 00:33:15 +0000

    sx170 100a/101a port. Autobuild disabled due to RAW capture errors.

---
+++ platform/sx170is/sub/boot_hdr.c
@@ -0,0 +1,30 @@
+#include "lolevel.h"
+#include "platform.h"
+#include "core.h"
+#include "dryos31.h"
+
+#define offsetof(TYPE, MEMBER) ((int) &((TYPE *)0)->MEMBER)
+
+const char * const new_sa = &_end;
+
+// Forward declarations
+extern volatile int jogdial_stopped;
+
+/*----------------------------------------------------------------------
+    CreateTask_spytask
+-----------------------------------------------------------------------*/
+void CreateTask_spytask()
+{
+    _CreateTask("SpyTask", 0x19, 0x2000, core_spytask, 0);
+}
+
+///*----------------------------------------------------------------------
+// Pointer to stack location where jogdial task records previous and current
+// jogdial positions
+short *jog_position;
+
+/*----------------------------------------------------------------------
+    boot()
+
+    Main entry point for the CHDK code
+-----------------------------------------------------------------------*/
