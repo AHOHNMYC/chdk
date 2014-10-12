e6f81bf13e5c5df4df96bf1933a7cc25a10716e2
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-10-12 15:40:00 +0000

    ixus80_sd1100 update: - port rewritten using code_gen tool - add fwt support - simpler loader code - CHDK loaded in ARAM Tested by koshy: http://chdk.setepontos.com/index.php?topic=11831.msg117502#msg117502

--- platform/ixus80_sd1100/lib.c
+++ platform/ixus80_sd1100/lib.c
@@ -43,5 +43,19 @@ void camera_set_led(int led, int state, int bright)
 
 
 int get_flash_params_count(void){
- return 114; //???
+ return 0x73;
+}
+
+// PTP display stuff
+int vid_get_palette_type() { return 1; }
+int vid_get_palette_size() { return 16*4; }
+
+void *vid_get_bitmap_active_palette() 
+{
+    return (void *)0x3E9B0;        //Found @ 0xff8e7e94 - Two refs to "BmpDDev"
+}
+
+void *vid_get_bitmap_active_buffer() 
+{
+    return (void*)(*(int*)0x7ACC); //Found @ 0xff8e7e94 - Two refs to "BmpDDev"
 }
