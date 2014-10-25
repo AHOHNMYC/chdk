c256fdf9d20a244e5937f033e2ea1378f3ec35af
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-10-25 04:09:09 +0000

    Move color test title text to language file so menu item can be added to user menu.

--- core/gui.c
+++ core/gui.c
@@ -1705,7 +1705,7 @@ static void gui_menu_test_palette(int arg)
 
 static CMenuItem visual_submenu_items[] = {
     MENU_ITEM(0x65,LANG_MENU_MISC_PALETTE,            MENUITEM_PROC,      gui_menu_run_palette, 0 ),
-    MENU_ITEM(0x65,(int)"CHDK Color Test",            MENUITEM_PROC,      gui_menu_test_palette, 0 ),
+    MENU_ITEM(0x65,LANG_MENU_COLOR_TEST,              MENUITEM_PROC,      gui_menu_test_palette, 0 ),
     MENU_ITEM(0x0,LANG_MENU_VIS_COLORS,               MENUITEM_SEPARATOR, 0, 0 ),
     MENU_ITEM(0x65,LANG_MENU_VIS_OSD_TEXT,            MENUITEM_COLOR_FG,  &conf.osd_color, 0 ),
     MENU_ITEM(0x65,LANG_MENU_VIS_OSD_BKG,             MENUITEM_COLOR_BG,  &conf.osd_color, 0 ),
