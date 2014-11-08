7f4c28afcb29c9f795592a29c2a0a0ec9872519c
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-11-08 21:47:39 +0000

    a1000: fix fwt and UI overlay colors Tested by koshy: http://chdk.setepontos.com/index.php?topic=11831.msg118311#msg118311

--- trunk/platform/a1000/sub/filewrite_hdr.c
+++ trunk/platform/a1000/sub/filewrite_hdr.c
@@ -6,7 +6,7 @@ typedef struct {
     unsigned int length;
 } cam_ptp_data_chunk; //camera specific structure
 
-#define MAX_CHUNKS_FOR_JPEG 4 // filewritetask is prepared for this many chunks
+#define MAX_CHUNKS_FOR_JPEG 3 // filewritetask is prepared for this many chunks
 /*
  * fwt_data_struct: defined here as it's camera dependent
  * unneeded members are designated with unkn
@@ -18,9 +18,8 @@ typedef struct
 {
     int unkn1[5];
     cam_ptp_data_chunk pdc[MAX_CHUNKS_FOR_JPEG];
-    int unkn6;
-    char name[32];                               // open stage: sub_FFDE69C0 (1.00f) --> "ADD     R0, R0, #0x38"
-                                                 // offset of 'name' from start = 0x38 = 56 bytes = 14 words
+    char name[32];                               // open stage: sub_FFDD8E74 (1.00a) --> "ADD     R0, R0, #0x2C"
+                                                 // offset of 'name' from start = 0x2C = 44 bytes = 11 words
 } fwt_data_struct;
 
 #include "../../../generic/filewrite.c"
