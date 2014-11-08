7785eb5532c7d28437147bb1b0fb515570f3dbfd
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-11-08 21:47:39 +0000

    a1000: fix fwt and UI overlay colors Tested by koshy: http://chdk.setepontos.com/index.php?topic=11831.msg118311#msg118311

--- platform/a1000/lib.c
+++ platform/a1000/lib.c
@@ -35,6 +35,12 @@ int get_flash_params_count(void){
 	return 119; // Found at ROM:FF972F9C (S5IS 1.01b) => ROM:FF93D3E4
 }
 
+char *camera_jpeg_count_str()
+{
+    extern char jpeg_count_str[];
+    return jpeg_count_str;
+}
+
 void camera_set_led(int led, int state, int bright) { 
         struct led_control led_c; 
         char convert_table[11]={0,1,2,3,0,2,3,1,8,10,10};  // s3 to a710 (and a720) convert table
@@ -61,3 +67,76 @@ void vid_bitmap_refresh() //as used in other models
         enabled_refresh_physical_screen=1;
         _RefreshPhysicalScreen(1);
 }
+
+// PTP display stuff
+// 64 entry palette based on 100a 0xffcc4f24 by comparison to a495 100f
+int vid_get_palette_type() { return 4; }
+int vid_get_palette_size() { return 16*4; }
+
+void *vid_get_bitmap_active_palette() {
+    return (void *)*(unsigned int*)(0x7534+0x20);  // 100a/100b: sub_ffcc4f24, via sub_ffd75e50 two refs to "Palette Class."
+}
+void *vid_get_bitmap_active_buffer()
+{
+    return (void*)(*(int*)(0x7534+0x0c)); // 100a/100b: sub_ffcc4fe8, via "Add: %p Width : %ld Hight : %ld"
+}
+
+
+// Viewport and Bitmap values that shouldn't change across firmware versions.
+// Values that may change are in lib.c for each firmware version.
+
+void *vid_get_viewport_fb_d()
+{
+    extern char *viewport_fb_d;
+    return viewport_fb_d;
+}
+
+void *vid_get_bitmap_fb(){
+    return (void*)0x10361000;
+}
+
+void *vid_get_viewport_fb()
+{
+	return (void*)0x10659DE0;
+}
+
+int vid_get_viewport_yoffset() {
+    return 0;
+}
+
+extern int _GetVRAMHPixelsSize();
+extern int _GetVRAMVPixelsSize();
+
+int vid_get_viewport_width() {
+    return 360;
+}
+
+int vid_get_viewport_width_proper() {
+    return ((mode_get()&MODE_MASK) == MODE_PLAY)?720:_GetVRAMHPixelsSize();
+}
+
+long vid_get_viewport_height() {
+    return 240;
+}
+
+int vid_get_viewport_height_proper() {
+    int m = mode_get();
+    // special case: W resolution
+    if ((m&MODE_MASK) != MODE_PLAY && shooting_get_prop(PROPCASE_RESOLUTION) == 8) {
+        return 240;
+    }
+    return ((mode_get()&MODE_MASK) == MODE_PLAY)?240:_GetVRAMVPixelsSize();
+}
+
+int vid_get_viewport_fullscreen_height() {
+    int m = mode_get();
+    // special case: W resolution
+    if ((m&MODE_MASK) != MODE_PLAY && shooting_get_prop(PROPCASE_RESOLUTION) == 8) {
+        return 120;
+    }
+    return ((mode_get()&MODE_MASK) == MODE_PLAY)?240:_GetVRAMVPixelsSize();
+}
+
+int vid_get_viewport_display_xoffset_proper()   { return vid_get_viewport_display_xoffset(); }
+int vid_get_viewport_display_yoffset_proper()   { return vid_get_viewport_display_yoffset(); }
+
