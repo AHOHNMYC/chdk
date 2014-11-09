8d04b5ada3cd2d59c8a83ca6d1ebcf3ca9cc34f7
srsa <srsa@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-11-09 17:48:11 +0000

    s3is: disallow calling focus distance related fw functions in playback mode, see http://chdk.setepontos.com/index.php?topic=265.msg118314#msg118314

--- trunk/platform/s3is/wrappers.c
+++ trunk/platform/s3is/wrappers.c
@@ -6,19 +6,19 @@
 long lens_get_focus_pos()
 {
     static long v=MAX_DIST;
-    return (kbd_is_key_pressed(KEY_SHOOT_HALF) || camera_info.state.state_kbd_script_run  || shooting_get_focus_mode())?v=_GetFocusLensSubjectDistance():v;
+    return (!camera_info.state.mode_play && (kbd_is_key_pressed(KEY_SHOOT_HALF) || camera_info.state.state_kbd_script_run  || shooting_get_focus_mode()))?v=_GetFocusLensSubjectDistance():v;
 }
 
 long lens_get_focus_pos_from_lens()
 {
     static long v=MAX_DIST;
-    return (kbd_is_key_pressed(KEY_SHOOT_HALF) || camera_info.state.state_kbd_script_run  || shooting_get_focus_mode())?v=_GetFocusLensSubjectDistanceFromLens():v;
+    return (!camera_info.state.mode_play && (kbd_is_key_pressed(KEY_SHOOT_HALF) || camera_info.state.state_kbd_script_run  || shooting_get_focus_mode()))?v=_GetFocusLensSubjectDistanceFromLens():v;
 }
 
 long lens_get_target_distance()
 {
     static long v=MAX_DIST;
-    return (kbd_is_key_pressed(KEY_SHOOT_HALF) || camera_info.state.state_kbd_script_run  || shooting_get_focus_mode())?v=_GetCurrentTargetDistance():v;
+    return (!camera_info.state.mode_play && (kbd_is_key_pressed(KEY_SHOOT_HALF) || camera_info.state.state_kbd_script_run  || shooting_get_focus_mode()))?v=_GetCurrentTargetDistance():v;
 }
  
  //--------------------------------------------------
