7a63ec087e58db6ff750fe7478dc5b985bf17519
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-11-17 09:34:12 +0000

    Small space saving and code reduction. Generate font and codepage tables statically during build instead of dynamically at run-time. Done when font data is compressed in font_8x16_pack.c.

--- lib/font/font_8x16.c
+++ lib/font/font_8x16.c
@@ -1,61 +1,24 @@
-#include "camera_info.h"
-#include "stdlib.h"
-#include "gui.h"
-#include "font.h"
-#include "codepages.h"
 #include "font_8x16_uni_packed.h"
 
 //-------------------------------------------------------------------
-#define DEFAULT_SYMBOL          0xFFFD
+// Currently selected codepage
+static int current_cp = 0;
 
 //-------------------------------------------------------------------
-// Offsets to font character data stored in the font_data array
-unsigned short current_font[256];
-
-unsigned char *get_current_font_data(char ch)
+unsigned char *get_current_font_data(unsigned char ch)
 {
-    return font_data + current_font[(unsigned char)ch];
+    // Test for 'common' char?
+    if (ch < 128)
+        return font_data + cp_common[ch];
+    // If not return value from current codepage
+    return font_data + codepages[current_cp][ch-128];
 }
 
 //-------------------------------------------------------------------
-static FontData* font_find_data (int charcode) {
-    int i=0, c;
-
-    while (1)
-    {
-        FontData *f = (FontData*)(font_data+i);
-        c = (f->charcode[1] << 8) | f->charcode[0];
-        if (c == 0xFFFF) return 0;
-        if (c == charcode) return f;
-        i = i + f->size + sizeof(FontData);
-    }
-    return 0;
-}
-
-//-------------------------------------------------------------------
-static void font_init_data(const unsigned short *src, int st, int num) {
-    int i;
-    FontData *f;
-
-    for (i=0; i<num; ++i) {
-        f = font_find_data(src[i]);
-        if (f == 0) 
-        {
-            f = font_find_data(DEFAULT_SYMBOL);
-        }
-        current_font[st+i] = (short)((unsigned char*)f - font_data);
-    }
-}
-
-//-------------------------------------------------------------------
-void font_init() {
-    font_init_data(cp_common, 0, 128);
-//    font_init_data(codepages[FONT_CP_WIN_1251], 128, 128);
-}
-
-//-------------------------------------------------------------------
-void font_set(int codepage) {
-    font_init_data(codepages[codepage], 128, 128);
+void font_set(int codepage)
+{
+    // Save selected codepage
+    current_cp = codepage;
 }
 
 //-------------------------------------------------------------------
