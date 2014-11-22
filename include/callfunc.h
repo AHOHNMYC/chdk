5b9dc8228570705d7d29cd0899cad1724c1a11f6
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-11-22 00:52:57 +0000

    Fix incorrect calling of 'call_func_ptr' on OS/X with GCC 4.7.3.

--- include/callfunc.h
+++ include/callfunc.h
@@ -7,5 +7,8 @@
 // Note: used in modules and platform independent code. 
 // Do not add platform dependent stuff in here (#ifdef/#endif compile options or camera dependent values)
 
-unsigned call_func_ptr(void *func, const unsigned *args, unsigned n_args);
+// 'long_call' attribute required to ensure correct calling from thumb code
+// GCC 4.7.3 on OS/X does not generate thumb-interwork for this function ????
+unsigned __attribute__((long_call)) call_func_ptr(void *func, const unsigned *args, unsigned n_args);
+
 #endif // CALLFUNC_H
