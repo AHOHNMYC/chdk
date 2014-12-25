beca30dacee030fbcc257f61aa2ed1dbe74e6f51
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-12-25 00:23:39 +0000

    fix comment that caused propset2 lua file to be generated incorrectly, add warnings

--- include/propset2.h
+++ include/propset2.h
@@ -25,12 +25,10 @@ WARNING:
 #define PROPCASE_SUBJECT_DIST1              245
 #define PROPCASE_SUBJECT_DIST2              65
 #define PROPCASE_ISO_MODE                   149
-/*
-For details, see http://chdk.setepontos.com/index.php?topic=11604.0
-#define PROPCASE_EXPOSURE_LOCK              206     // Correct name for PROPCASE_SHOOTING - gets set when set_aelock called or AEL button pressed
-#define PROPCASE_SHOOTING                   115     // sx100, a470 (gets set to 1 when camera has focused and set exposure, returns to 0 after shot)
-#define PROPCASE_SHOOTING                   297     // g10 (gets set to 1 when camera has focused and set exposure, returns to 0 after shot)
-*/
+//For details, see http://chdk.setepontos.com/index.php?topic=11604.0
+//#define PROPCASE_EXPOSURE_LOCK              206     // Correct name for PROPCASE_SHOOTING - gets set when set_aelock called or AEL button pressed
+//#define PROPCASE_SHOOTING                   115     // sx100, a470 (gets set to 1 when camera has focused and set exposure, returns to 0 after shot)
+//#define PROPCASE_SHOOTING                   297     // g10 (gets set to 1 when camera has focused and set exposure, returns to 0 after shot)
 #define PROPCASE_SHOOTING                   206     // until a better solution is found
 #define PROPCASE_IS_FLASH_READY             208
 #define PROPCASE_OVEREXPOSURE               103
