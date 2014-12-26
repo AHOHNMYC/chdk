95e1a1fbcedaee02dcb86009635d00a0e9971356
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2014-12-26 08:17:34 +0000

    Fix SX510 custom colors overwriting Canon colors (reported by Yoti): http://chdk.setepontos.com/index.php?topic=10984.msg119271#msg119271

--- branches/release-1_3/platform/sx510hs/platform_palette.h
+++ branches/release-1_3/platform/sx510hs/platform_palette.h
@@ -7,3 +7,18 @@
     // Cameras use custom colors (CAM_LOAD_CUSTOM_COLORS) - CHDK colors set in core/gui_draw.h
 
     #define CHDK_COLOR_BASE                 0xB0  // Start color index for CHDK colors loaded into camera palette.
+
+    // CHDK colors loaded into these locations in the camera palette by load_chdk_palette()
+    #define COLOR_RED           (CHDK_COLOR_BASE+0)
+    #define COLOR_RED_DK        (CHDK_COLOR_BASE+1)
+    #define COLOR_RED_LT        (CHDK_COLOR_BASE+2)
+    #define COLOR_GREEN         (CHDK_COLOR_BASE+5)
+    #define COLOR_GREEN_DK      (CHDK_COLOR_BASE+6)
+    #define COLOR_GREEN_LT      (CHDK_COLOR_BASE+7)
+    #define COLOR_BLUE          (CHDK_COLOR_BASE+8)
+    #define COLOR_BLUE_DK       (CHDK_COLOR_BASE+9)
+    #define COLOR_BLUE_LT       (CHDK_COLOR_BASE+10)
+    #define COLOR_MAGENTA       (CHDK_COLOR_BASE+11)
+    #define COLOR_YELLOW        (CHDK_COLOR_BASE+12)
+    #define COLOR_YELLOW_DK     (CHDK_COLOR_BASE+13)
+    #define COLOR_YELLOW_LT     (CHDK_COLOR_BASE+14)
