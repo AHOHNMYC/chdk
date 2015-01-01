b16c900b2eabd405cb6b61bd3263707ee1809a9b
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-01-01 21:50:49 +0000

    generic boolean to number return wrapper, test script for 1.3 wrappers.

--- trunk/CHDK/LUALIB/wrap13.lua
+++ trunk/CHDK/LUALIB/wrap13.lua
@@ -1,6 +1,22 @@
-local _get_raw = get_raw; function get_raw() if _get_raw() then return 1 else return 0 end end
-local _get_video_button = get_video_button; function get_video_button() if _get_video_button() then return 1 else return 0 end end
-local _get_focus_ok = get_focus_ok; function get_focus_ok() if _get_focus_ok() then return 1 else return 0 end end
-local _save_config_file = save_config_file; function save_config_file(...) if _save_config_file(...) then return 1 else return 0 end end
-local _load_config_file = load_config_file; function load_config_file(...) if _load_config_file(...) then return 1 else return 0 end end
-local _set_mf = set_mf; function set_mf(v) if _set_mf(v) then return 1 else return 0 end end
+local function ret_bool_to_num(names)
+	for i,name in ipairs(names) do
+		local f=_G[name]
+		if type(f) ~= 'function' then
+			error('wrapper for non-function '..tostring(name))
+		end
+		_G[name]=function(...)
+			if f(...) then
+				return 1
+			end
+			return 0
+		end
+	end
+end
+ret_bool_to_num{
+	'get_raw',
+	'get_video_button',
+	'get_focus_ok',
+	'save_config_file',
+	'load_config_file',
+	'set_mf',
+}
