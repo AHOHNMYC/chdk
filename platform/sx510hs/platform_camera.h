cc0f3d35e649a9602c49a0939b2a7cd52ea677fa
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-01-08 04:01:04 +0000

    set CAM_MARKET_ISO_BASE for a1400, sx510hs - from trunk r3897

--- platform/sx510hs/platform_camera.h
+++ platform/sx510hs/platform_camera.h
@@ -127,5 +127,8 @@
     #define DRAW_ON_ACTIVE_BITMAP_BUFFER_ONLY 1
 
     #define CAM_ZEBRA_NOBUF                 1
+
+    #undef  CAM_MARKET_ISO_BASE
+    #define CAM_MARKET_ISO_BASE                 200 // Override base 'market' ISO value, from isobase.lua
     
 //----------------------------------------------------------
