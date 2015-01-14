1cb1860cc58296760d38e0af8da97c3442bb6611
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-01-14 03:17:06 +0000

    sx10 update: - add fwt support - fix liveview colors - capt_seq.c rewritten using code_gen Tested by koshy: http://chdk.setepontos.com/index.php?topic=11831.msg119877#msg119877

--- trunk/platform/sx10/sub/101a/lib.c
+++ trunk/platform/sx10/sub/101a/lib.c
@@ -23,35 +23,6 @@ void *vid_get_viewport_fb()
 	return (void*)0x4070D9D0;
 }
 
-void *vid_get_viewport_fb_d()
-{
-	return (void*)(*(int*)(0x5260+0x58)); 
-}
-
-long vid_get_viewport_height()
-{
-	return 240;
-}
-
-char *camera_jpeg_count_str()
-{
-	return (char*)0x4C138;
-}
-
-
-// PTP display stuff, untested, adapted from ewavr chdkcam patch
-// reyalp - type probably wrong, looks like 2 with opposite order
-int vid_get_palette_type() { return 2; }
-int vid_get_palette_size() { return 16*4; }
-
-void *vid_get_bitmap_active_palette() {
-    return (void *)*(unsigned int*)(0x8378+0x20);  // sub_FF8F3A10
-}
-void *vid_get_bitmap_active_buffer()
-{
-    return (void*)(*(int*)(0x8378+0x0C)); //"Add: %p Width : %ld Hight : %ld", sub_FF8F3AD4
-}
-
 // values from chdkcam patch
 // commented for now, protocol changes needed to handle correctly
 // note, play mode may be 704, needs to be tested
