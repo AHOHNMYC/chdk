ed7332db0ba2ae1546d22119c316aa9e1ed0b2b9
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-01-18 00:57:48 +0000

    Modemap updates from waterwingz: http://chdk.setepontos.com/index.php?topic=650.msg120000#msg120000

--- platform/a810/platform_camera.h
+++ platform/a810/platform_camera.h
@@ -61,6 +61,8 @@
     #define CAM_HAS_ND_FILTER               1
 
     #define CAM_HAS_VIDEO_BUTTON            1
+    #undef  CAM_MASK_VID_REC_ACTIVE                 // allow video recording state to change reported shooting mode
+    
     #undef  CAM_VIDEO_CONTROL
     #define CAM_VIDEO_QUALITY_ONLY          1
     #define CAM_HAS_MOVIE_DIGEST_MODE       1   //Camera doesn't actually have MOVIE_DIGEST_MOVIE, but this is required by is_video_recording.
