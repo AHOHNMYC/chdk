f652b8f66ae417f21951ad060478762609fdad68
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-01-21 05:08:57 +0000

    rawop * remove shift parameter create_histogram, now specified as bits in histo:update() * histogram now only allocates memory on update * add histo:free() method to immediately free histogram data * fix incorrect total pixels calculation * update drtest.lua to use rawop histogram

--- trunk/CHDK/SCRIPTS/TEST/drtest.lua
+++ trunk/CHDK/SCRIPTS/TEST/drtest.lua
@@ -1,6 +1,6 @@
 --[[
 @title dynamic range test
-@chdk_version 1.4.0.3867
+@chdk_version 1.4.0.3937
 #overstops=4 "+ stops"
 #understops=6 "- stops"
 #draw_meter=true "draw meter"
@@ -10,6 +10,12 @@ require'rawoplib'
 
 props=require'propcase'
 capmode=require'capmode'
+
+-- centered 500 px square
+meter_size = 500
+
+meter_left = rawop.fb.width/2 - meter_size/2
+meter_top = rawop.fb.height/2 - meter_size/2
 
 save_raw=get_raw()
 set_raw(1) 
@@ -24,16 +30,9 @@ end
 get rgb and combined meter
 ]]
 function get_meter()
-	-- centered 500 px square
-	local meter_size = 500
 
-	local x1 = rawop.fb.width/2 - meter_size/2
-
-	local y1 = rawop.fb.height/2 - meter_size/2
-
-	-- TODO a meter matching raw_histogram area / step might be interesting
-	local m = rawop.meter(x1,y1,meter_size,meter_size,1,1)
-	local r,g1,g2,b = rawop.meter_rgbg(x1,y1,meter_size,meter_size,2,2)
+	local m = rawop.meter(meter_left,meter_top,meter_size,meter_size,1,1)
+	local r,g1,b,g2 = rawop.meter_rgbg(meter_left,meter_top,meter_size,meter_size,2,2)
 	if draw_meter then
 		local low = rawop.fb.black_level + rawop.fb.black_level / 2
 		local high = rawop.fb.white_level - rawop.fb.black_level
@@ -45,10 +44,10 @@ function get_meter()
 			c = high
 		end
 		-- box around meter area
-		rawop.fill_rect_rgbg(x1 - 2,y1 - 2,meter_size+4,2,c,c,c)
-		rawop.fill_rect_rgbg(x1 - 2,y1 + meter_size,meter_size+4,2,c,c,c)
-		rawop.fill_rect_rgbg(x1 - 2,y1,2,meter_size,c,c,c)
-		rawop.fill_rect_rgbg(x1 + meter_size,y1,2,meter_size,c,c,c)
+		rawop.fill_rect_rgbg(meter_left - 2,meter_top - 2,meter_size+4,2,c,c,c)
+		rawop.fill_rect_rgbg(meter_left - 2,meter_top + meter_size,meter_size+4,2,c,c,c)
+		rawop.fill_rect_rgbg(meter_left - 2,meter_top,2,meter_size,c,c,c)
+		rawop.fill_rect_rgbg(meter_left + meter_size,meter_top,2,meter_size,c,c,c)
 
 		-- draw max scale
 		rawop.fill_rect_rgbg(100,90,500,4,high,high,high)
@@ -77,27 +76,34 @@ there will usually be two peaks due to greater green sensitivity,
 more pronounced with greater exposure
 ]]
 function get_histo_stats()
+	-- TODO just reproduce shot_histogram behavior for now
+	histo:update(rawop.fb.active_area.x1,rawop.fb.active_area.y1,
+						rawop.fb.active_area.x2 - rawop.fb.active_area.x1,
+						rawop.fb.active_area.y2 - rawop.fb.active_area.y1,
+						31,31,10)
+
 	local bin=32
 	local peak=0
-	local peak_pct=0
+	local peak_count=0
 	-- find the "bin" sized range with highest % of pixels
 	for v=0,1024-bin do
-		local pct=get_histo_range(v,v+bin-1)
-		if pct > peak_pct then
+		local count=histo:range(v,v+bin-1,'count')
+		if count > peak_count then
 			peak = v
-			peak_pct = pct
+			peak_count = count
 		end
 	end
+	local peak_pct = histo:range(peak,peak+bin-1)
 	-- find the peak within the bin
-	local pct_max=0
+	local count_max=0
 	for v=peak,peak+bin-1 do
-		local pct=get_histo_range(v,v)
-		if pct > pct_max then
+		local count=histo:range(v,v,'count')
+		if count > count_max then
 			peak = v
-			ptc_max = pct
+			count_max = count
 		end
 	end
-	return peak,peak_pct,get_histo_range(0,32),get_histo_range(1023,1023)
+	return peak,peak_pct,histo:range(1,32),histo:range(1023,1023)
 end
 
 function logline(desc)
@@ -108,12 +114,13 @@ function logline(desc)
 		error(err)
 	end
 	if not st then
-		f:write('desc,date,tick,exp,tv96,sv96,av96,bv96,m,m96,r,r96,g1,g1_96,g2,g2_96,b,b96,peak,peak%,bl%,wl%,ccd temp\n')
+		f:write('desc,date,tick,exp,tv96,sv96,av96,bv96,m,m96,r,r96,g1,g1_96,g2,g2_96,b,b96,peak,peak%,bl%,wl%,ccd temp,t1,t2\n')
 		local bi=get_buildinfo()
 		f:write(string.format("%s %s %s %s %s %s %s %s 0x%x\n",
 					bi.platform,bi.platsub,bi.version,bi.build_number,bi.build_revision,
 					bi.build_date,bi.build_time,bi.os,bi.platformid))
 	end
+	-- pct are actually per thousand
 	local peak,peak_pct,bl_pct,wl_pct = 0,0,0,0
 	local m,m96,r,r96,g1,g1_96,g2,g2_96,b,b96 = 0,0,0,0,0,0,0,0,0,0
 	if desc ~='base' then
@@ -125,7 +132,7 @@ function logline(desc)
 		g2_96=rawop.raw_to_ev96(g2)
 		b96=rawop.raw_to_ev96(b)
 	end
-	f:write(string.format('%s,%s,%u,%u,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n',
+	f:write(string.format('%s,%s,%u,%u,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%s,%s,%s,%d\n',
 				desc,
 				os.date('%m/%d/%Y %H:%M:%S'),
 				get_tick_count(),
@@ -140,9 +147,9 @@ function logline(desc)
 				g2,g2_96,
 				b,b96,
 				peak,
-				peak_pct,
-				bl_pct,
-				wl_pct,
+				string.format("%d.%d",peak_pct/10,peak_pct%10),
+				string.format("%d.%d",bl_pct/10,bl_pct%10),
+				string.format("%d.%d",wl_pct/10,wl_pct%10),
 				get_temperature(1)
 			)
 		)
@@ -165,7 +172,7 @@ if capmode.get_name() ~= 'P' then
 end
 
 -- TODO should focus at inf, possibly set zoom
-shot_histo_enable(1)
+histo=rawop.create_histogram()
 
 hook_raw.set(20000)
 press('shoot_half')
