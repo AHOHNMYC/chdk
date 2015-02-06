da7a502191a7b6b57ae62406d744a4fe90b28b29
msl <msl@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-02-06 14:09:42 +0000

    A720: color tables adjustments

--- trunk/platform/a720/platform_palette.c
+++ trunk/platform/a720/platform_palette.c
@@ -11,20 +11,20 @@ unsigned char ply_colors[] =
         COLOR_BLACK,                // Placeholder for script colors
         0x11,                       // White
         0x22,                       // Red
-        0x2E,                       // Dark Red
-        0x21,                       // Light Red
+        0x2F,                       // Dark Red
+        0x28,                       // Light Red
         0x55,                       // Green
         0x25,                       // Dark Green
-        0x51,                       // Light Green
-        0xCC,                       // Blue
-        0xDF,                       // Dark Blue
-        0xDD,                       // Light Blue / Cyan
+        0x85,                       // Light Green
+        0x9F,                       // Blue
+        0xAF,                       // Dark Blue
+        0x97,                       // Light Blue / Cyan
         0x3F,                       // Grey
-        0x44,                       // Dark Grey
+        0x7F,                       // Dark Grey
         0x1F,                       // Light Grey
         0x66,                       // Yellow
         0x6F,                       // Dark Yellow
-        0x66,                       // Light Yellow
+        0x61,                       // Light Yellow
         0x44,                       // Transparent Dark Grey
         0xE2,                       // Magenta
 };
@@ -36,21 +36,21 @@ unsigned char rec_colors[] =
         COLOR_BLACK,                // Placeholder for script colors
         0x11,                       // White
         0x22,                       // Red
-        0x2E,                       // Dark Red
-        0x21,                       // Light Red
+        0x2F,                       // Dark Red
+        0x2C,                       // Light Red
         0x55,                       // Green
-        0x25,                       // Dark Green
-        0x51,                       // Light Green
+        0x5F,                       // Dark Green
+        0xC5,                       // Light Green
         0xDF,                       // Blue
         0xDF,                       // Dark Blue
         0xDD,                       // Light Blue / Cyan
         0x3F,                       // Grey
-        0x44,                       // Dark Grey
+        0xAF,                       // Dark Grey
         0x1F,                       // Light Grey
-        0xEE,                       // Yellow
+        0x66,                       // Yellow
         0x6F,                       // Dark Yellow
-        0x66,                       // Light Yellow
+        0x6C,                       // Light Yellow
         0x44,                       // Transparent Dark Grey
-        0x66,                       // Magenta
+        0xE2,                       // Magenta
 };
 
