c8b4b2b6e2d5996caea88d425ab39a2f9b9a86bd
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-02-19 03:40:25 +0000

    sx510hs: filewrite.c - Fix wrong call to fwt_close

--- platform/sx510hs/sub/101a/filewrite.c
+++ platform/sx510hs/sub/101a/filewrite.c
@@ -97,8 +97,8 @@ asm volatile (
 "loc_FF3487B0:\n"
 "    LDR     R0, [R4, #4] \n"
 "    CMN     R0, #1 \n"
-"    BEQ     fwt_close \n"  // --> Patched. Old value = 0xFF3487D8.
-"    BL      _Close \n"
+"    BEQ     loc_FF3487D8 \n"
+"    BL      fwt_close \n"  // --> Patched. Old value = _Close.
 "    MVN     R0, #0 \n"
 "    STR     R0, [R4, #4] \n"
 "    LDR     R0, =0x125CCC \n"
