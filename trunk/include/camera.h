bf6f6788a6fac1c1400aa827daabde518aac8b8b
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-04 04:48:09 +0000

    update CAM_MARKET_ISO_BASE comment

--- trunk/include/camera.h
+++ trunk/include/camera.h
@@ -225,7 +225,7 @@
     #undef  CAM_USE_ALT_PT_MoveOpticalZoomAt    // Define to use the PT_MoveOpticalZoomAt() function in lens_set_zoom_point()
     #undef  CAM_USE_OPTICAL_MAX_ZOOM_STATUS     // Use ZOOM_OPTICAL_MAX to reset zoom_status when switching from digital to optical zoom in gui_std_kbd_process()
 
-    #define CAM_MARKET_ISO_BASE             100 // Base 'market' ISO value (SX40 & G1X use 200)
+    #define CAM_MARKET_ISO_BASE             100 // Base 'market' ISO value. Most (all?) DryOS R49 and later use 200, use tests/isobase.lua to check
     #undef  CAM_HAS_HI_ISO_AUTO_MODE            // Define if camera has 'HI ISO Auto' mode (as well as Auto ISO mode), needed for adjustment in user auto ISO menu 
 
     #define CAMERA_MIN_DIST         0           // Define min distance that can be set in _MoveFocusLensToDistance (allow override - e.g. G12 min dist = 1)
