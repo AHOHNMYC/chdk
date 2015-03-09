27a49110bcdbe7749a5b3669c87d9be33993a1e8
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-09 00:17:14 +0000

    keyboard rework - convert a620, A630, A640, A700, A710. UNTESTED

--- platform/a630/platform_kbd.h
+++ platform/a630/platform_kbd.h
@@ -1,4 +1,16 @@
 #ifndef PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+
+#define KEYS_MASK0 0x00000000
+#define KEYS_MASK1 0x00000000
+#define KEYS_MASK2 0x00001FFF
+
+#define SD_READONLY_IDX 2
+#define SD_READONLY_FLAG 0x20000
+
+#define USB_MASK 0x8000000
+#define USB_IDX 1
+
+// MMIO to read USB +5v directly, for get_usb_bit
+#define USB_MMIO 0xc0220204
+
 #endif
