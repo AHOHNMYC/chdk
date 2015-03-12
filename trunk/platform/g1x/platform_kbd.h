4a64fec9e585b6355381f4126c664917a0d9a71d
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-12 08:01:30 +0000

    keyboard rework - convert G1X

--- trunk/platform/g1x/platform_kbd.h
+++ trunk/platform/g1x/platform_kbd.h
@@ -1,4 +1,19 @@
 #ifndef PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+#define PLATFORM_KBD_H
+
+#define KEYS_MASK0          0x013FFC00
+#define KEYS_MASK1          0x00000000
+#define KEYS_MASK2          0x000C0000
+
+#define SD_READONLY_FLAG    0x00000002 // Found @0xff464d1c, levent 0x20a
+#define SD_READONLY_IDX     1
+
+#define USB_MASK            0x10000000 // Found @0xff464d94, levent 0x202
+#define USB_IDX             2
+
+#define BATTCOVER_FLAG      0x00000100 // Found @0xff464d24, levent 0x205
+#define BATTCOVER_IDX       2
+
+#define CAM_HAS_JOGDIAL     1
+
 #endif
