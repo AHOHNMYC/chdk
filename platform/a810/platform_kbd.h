cc8fb75e5ea22f8e6edb5092b3fd43bba5478314
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-13 05:59:21 +0000

    keyboard rework - convert a720, a810. UNTESTED

--- platform/a810/platform_kbd.h
+++ platform/a810/platform_kbd.h
@@ -1,4 +1,12 @@
 #ifndef PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+// override key and feather bits to avoid feather osd messing up chdk display in ALT mode
+#define KEYS_MASK0         (0x00003000) //Logic OR of group 0 Keymap values
+#define KEYS_MASK1         (0xBFF00000) //Logic OR of group 1 Keymap values
+#define KEYS_MASK2         (0x00000000) //Logic OR of group 2 Keymap values
+
+#define SD_READONLY_IDX 2
+#define SD_READONLY_FLAG 0x20000
+
+#define USB_MASK (0x40000)
+#define USB_IDX  2
 #endif
