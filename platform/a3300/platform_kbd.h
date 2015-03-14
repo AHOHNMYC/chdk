ea3c8e216fb6f8f03283916ff6e614248bb3d36c
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-14 20:20:51 +0000

    keyboard rework - a3000, a3200, a3300, a3400, a4000. UNTESTED

--- platform/a3300/platform_kbd.h
+++ platform/a3300/platform_kbd.h
@@ -1,4 +1,12 @@
 #ifndef PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+#define KEYS_MASK0 (0x00002000)
+#define KEYS_MASK1 (0x00000000)
+#define KEYS_MASK2 (0x0000FFFF)
+
+#define SD_READONLY_FLAG (0x00020000)
+#define SD_READONLY_IDX     2
+
+#define USB_MASK (0x04000000)
+#define USB_IDX             2
+
 #endif
