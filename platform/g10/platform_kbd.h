d623663496a59e347fab83eb76ac8eb76b3caa6d
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-14 23:22:50 +0000

    keyboard rework - G10, from waterwingz in http://chdk.setepontos.com/index.php?topic=650.msg121057#msg121057

--- platform/g10/platform_kbd.h
+++ platform/g10/platform_kbd.h
@@ -1,4 +1,16 @@
 #ifndef PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+#define PLATFORM_KBD_H
+
+
+#define KEYS_MASK0 (0x00000003)
+#define KEYS_MASK1 (0x000FFE18)
+#define KEYS_MASK2 (0x00000000) 
+
+#define SD_READONLY_FLAG    0x00020000  // Found @0xffb0f940, levent 0x90a
+#define SD_READONLY_IDX     2         
+#define USB_MASK            0x00040000  // Found @0xffb0f94c, levent 0x902
+#define USB_IDX             2    
+#define HOTSHOE_FLAG        0x10000000  // Found @0xffb0f7f8, levent 0x904
+#define HOTSHOE_IDX         0
+
 #endif
