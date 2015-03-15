1aa7df8b1572c7c50f5a2ab4fb517188370105ed
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-15 19:37:32 +0000

    keyboard rework - S90, S95, S110. UNTESTED. Plus cosmetic change to s100. From waterwingz in http://chdk.setepontos.com/index.php?topic=650.msg121084#msg121084 with minor adjustment

--- trunk/platform/s95/platform_kbd.h
+++ trunk/platform/s95/platform_kbd.h
@@ -1,4 +1,12 @@
 #ifndef PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+
+#define KEYS_MASK0 (0x00000000)
+#define KEYS_MASK1 (0x0000FF30)
+#define KEYS_MASK2 (0x00004B00)
+
+#define SD_READONLY_FLAG (0x20000)  	// in physw_status[2]
+#define SD_READONLY_IDX 2
+#define USB_MASK (0x4000000)  			// in physw_status[2]
+#define USB_IDX  2
+
 #endif
