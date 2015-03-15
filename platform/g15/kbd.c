49eaab9b963b134f1886d96538b7b3701859a93d
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-15 21:19:21 +0000

    keyboard rework - G7, G11, G15. UNTESTED

--- platform/g15/kbd.c
+++ platform/g15/kbd.c
@@ -1,39 +1,23 @@
 #include "lolevel.h"
 #include "platform.h"
-#include "core.h"
-#include "conf.h"
 #include "keyboard.h"
+#include "kbd_common.h"
 
-typedef struct {
-	short grp;
-	short hackkey;
-	long canonkey;
-} KeyMap;
-
-static long kbd_new_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
-static long kbd_prev_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
-static long kbd_mod_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+long kbd_new_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+long kbd_prev_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+long kbd_mod_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
 
 extern void _GetKbdState(long*);
-
-#define KEYS_MASK0 (0x003EFC00)
-#define KEYS_MASK1 (0x00000000)
-#define KEYS_MASK2 (0x000C8800)
-
-#define SD_READONLY_FLAG    0x00000002 // Found @0xff4a0da4, levent 0x20a
-#define SD_READONLY_IDX     1
-#define USB_MASK            0x10000000 // Found @0xff4a0e0c, levent 0x202
-#define USB_IDX             2
 
 int get_usb_bit() 
 {
 	long usb_physw[3];
 	usb_physw[USB_IDX] = 0;
 	_kbd_read_keys_r2(usb_physw);
-	return(( usb_physw[USB_IDX] & USB_MASK)==USB_MASK) ; 
+	return(( usb_physw[USB_IDX] & USB_MASK)==USB_MASK); 
 }
 
-static KeyMap keymap[] = {
+KeyMap keymap[] = {
     { 0, KEY_METERING        ,0x00000400 }, 
     { 0, KEY_AE_LOCK         ,0x00020000 }, 
     { 0, KEY_SET             ,0x00000800 }, // Found @0xff4a0d2c, levent 0x08
@@ -54,9 +38,11 @@ static KeyMap keymap[] = {
     { 0, 0, 0 }
 };
 
-long __attribute__((naked)) wrap_kbd_p1_f() ;
+long __attribute__((naked)) wrap_kbd_p1_f();
 
-static void __attribute__((noinline)) mykbd_task_proceed()
+// no stack manipulation needed here, since we create the task directly
+void __attribute__((noinline))
+mykbd_task()
 {
 	while (physw_run){
 		_SleepTask(physw_sleep_delay);
@@ -65,13 +51,6 @@ static void __attribute__((noinline)) mykbd_task_proceed()
 			_kbd_p2_f();
 		}
 	}
-}
-
-// no stack manipulation needed here, since we create the task directly
-void __attribute__((naked,noinline))
-mykbd_task()
-{
-    mykbd_task_proceed();
 
     _ExitTask();
 }
@@ -89,7 +68,7 @@ long __attribute__((naked,noinline)) wrap_kbd_p1_f()
 }
 
 // Set to 1 to disable jogdial events from being processed in firmware
-volatile int jogdial_stopped=0;
+int jogdial_stopped=0;
 
 // Pointer to stack location where jogdial task records previous and current
 // jogdial positions
@@ -109,126 +88,19 @@ void jogdial_control(int n)
 
 void my_kbd_read_keys()
 {
-	kbd_prev_state[0] = kbd_new_state[0];
-	kbd_prev_state[1] = kbd_new_state[1];
-	kbd_prev_state[2] = kbd_new_state[2];
-
-	_GetKbdState(kbd_new_state);
-	_kbd_read_keys_r2(kbd_new_state);
-
-	if (kbd_process() == 0){
-		// leave it alone...
-          physw_status[0] = kbd_new_state[0];
-          physw_status[1] = kbd_new_state[1];
-          physw_status[2] = kbd_new_state[2];
-          jogdial_control(0);
-
-	} else {
-		// override keys
-	 	physw_status[0] = (kbd_new_state[0] & (~KEYS_MASK0)) | (kbd_mod_state[0] & KEYS_MASK0);
-		physw_status[1] = (kbd_new_state[1] & (~KEYS_MASK1)) | (kbd_mod_state[1] & KEYS_MASK1);
-		physw_status[2] = (kbd_new_state[2] & (~KEYS_MASK2)) | (kbd_mod_state[2] & KEYS_MASK2);
-
-		if ((jogdial_stopped==0) && !camera_info.state.state_kbd_script_run)
-		{
-			jogdial_control(1);
-			get_jogdial_direction();
-		}
-		else if (jogdial_stopped && camera_info.state.state_kbd_script_run) jogdial_control(0);
-	}
-
-	//_kbd_read_keys_r2(physw_status);
-
-    physw_status[SD_READONLY_IDX] = physw_status[SD_READONLY_IDX] & ~SD_READONLY_FLAG;
-	if (conf.remote_enable) {
-		physw_status[USB_IDX] = physw_status[USB_IDX] & ~USB_MASK;
-	}
-
+    kbd_update_key_state();
+    kbd_update_physw_bits();
 }
 
+void kbd_fetch_data(long *dst)
+{
+    _GetKbdState(dst);
+    _kbd_read_keys_r2(dst);
+}
 
 /****************/
 
-void kbd_set_alt_mode_key_mask(long key)
-{
-}
-
-void kbd_key_press(long key)
-{
-	int i;
-
-	for (i=0;keymap[i].hackkey;i++){
-		if (keymap[i].hackkey == key)
-		{
-			kbd_mod_state[keymap[i].grp] &= ~keymap[i].canonkey;
-			return;
-		}
-	}
-}
-
-void kbd_key_release(long key)
-{
-	int i;
-	for (i=0;keymap[i].hackkey;i++){
-		if (keymap[i].hackkey == key){
-			kbd_mod_state[keymap[i].grp] |= keymap[i].canonkey;
-			return;
-		}
-	}
-}
-
-void kbd_key_release_all()
-{
-	kbd_mod_state[0] |= KEYS_MASK0;
-	kbd_mod_state[1] |= KEYS_MASK1;
-	kbd_mod_state[2] |= KEYS_MASK2;
-}
-
-long kbd_is_key_pressed(long key)
-{
-	int i;
-	for (i=0;keymap[i].hackkey;i++){
-		if (keymap[i].hackkey == key){
-			return ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0) ? 1:0;
-		}
-	}
-	return 0;
-}
-
-long kbd_is_key_clicked(long key)
-{
-	int i;
-	for (i=0;keymap[i].hackkey;i++){
-		if (keymap[i].hackkey == key){
-			return ((kbd_prev_state[keymap[i].grp] & keymap[i].canonkey) != 0) &&
-			       ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0);
-		}
-	}
-	return 0;
-}
-
-long kbd_get_pressed_key()
-{
-	int i;
-	for (i=0;keymap[i].hackkey;i++){
-		if ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0){
-			return keymap[i].hackkey;
-		}
-	}
-	return 0;
-}
-
-long kbd_get_clicked_key()
-{
-	int i;
-	for (i=0;keymap[i].hackkey;i++){
-		if (((kbd_prev_state[keymap[i].grp] & keymap[i].canonkey) != 0) &&
-		    ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0)) {
-			return keymap[i].hackkey;
-		}
-	}
-	return 0;
-}
+void kbd_set_alt_mode_key_mask(long key) { }
 
 static short new_jogdial = 0, old_jogdial = 0;
 
