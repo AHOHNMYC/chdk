ca3b6b70a5df4588466e88ffabfc910583e5954f
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-15 06:24:19 +0000

    keyboard rework - D20, D30. UNTESTED

--- trunk/platform/d20/kbd.c
+++ trunk/platform/d20/kbd.c
@@ -1,45 +1,23 @@
 #include "lolevel.h"
 #include "platform.h"
-#include "core.h"
-#include "conf.h"
 #include "keyboard.h"
-
-typedef struct {
-	short grp;
-	short hackkey;
-	long canonkey;
-} KeyMap;
+#include "kbd_common.h"
 
 long kbd_new_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
-static long kbd_prev_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
-static long kbd_mod_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+long kbd_prev_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+long kbd_mod_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
 
 extern void _GetKbdState(long*);
 
-
-// override key and feather bits to avoid feather osd messing up chdk display in ALT mode
-#define KEYS_MASK0         (0x00000000) //Logic OR of group 0 Keymap values
-#define KEYS_MASK1         (0x00000000) //Logic OR of group 1 Keymap values
-#define KEYS_MASK2         (0x000047FF) //Logic OR of group 2 Keymap values
-
-#define SD_READONLY_FLAG    0x00020000 // Found @0xff3a2994, levent 0x20a
-#define SD_READONLY_IDX     2
-#define USB_MASK            0x04000000 // Found @0xff3a29bc, levent 0x202
-#define USB_IDX             2
-
-#ifdef CAM_HAS_GPS
-int gps_key_trap=0 ;
-#endif
-
 int get_usb_bit()
 {
-        long usb_physw[3];
-        usb_physw[USB_IDX] = 0;
-        _kbd_read_keys_r2(usb_physw);
-        return(( usb_physw[USB_IDX] & USB_MASK)==USB_MASK) ;
+    long usb_physw[3];
+    usb_physw[USB_IDX] = 0;
+    _kbd_read_keys_r2(usb_physw);
+    return(( usb_physw[USB_IDX] & USB_MASK)==USB_MASK);
 }
 
-static KeyMap keymap[] = {
+KeyMap keymap[] = {
 	// Order IS important. kbd_get_pressed_key will walk down this table  
 	// and take the first matching mask. Notice that KEY_SHOOT_HALF is  
 	// always pressed if KEY_SHOOT_FULL is.
@@ -62,9 +40,7 @@ static KeyMap keymap[] = {
 };
 
 // NOP 
-void kbd_set_alt_mode_key_mask(long key)
-{
-}
+void kbd_set_alt_mode_key_mask(long key) { }
 
 long __attribute__((naked,noinline)) wrap_kbd_p1_f() {
 	
@@ -79,186 +55,24 @@ long __attribute__((naked,noinline)) wrap_kbd_p1_f() {
 	return 0; // shut up the compiler
 }
 
-static void __attribute__((noinline)) mykbd_task_proceed() {
-	
+void __attribute__((noinline)) mykbd_task() {
 	while (physw_run) {
 		_SleepTask(physw_sleep_delay);
 		if (wrap_kbd_p1_f() == 1) {   // autorepeat ?
 			_kbd_p2_f();
 		}
 	}
-}
-
-void __attribute__((naked,noinline)) mykbd_task() {
-	
-	mykbd_task_proceed();
 
 	_ExitTask();
 }
 
 void my_kbd_read_keys() {
-
-	kbd_prev_state[0] = kbd_new_state[0];
-	kbd_prev_state[1] = kbd_new_state[1];
-	kbd_prev_state[2] = kbd_new_state[2];
-
-	_GetKbdState(kbd_new_state);
-	_kbd_read_keys_r2(kbd_new_state);
-    
-#ifdef CAM_HAS_GPS
-    if (gps_key_trap > 0)
-    {
-        if (kbd_get_pressed_key() == gps_key_trap)
-        {
-            kbd_key_release(gps_key_trap);
-            kbd_key_press(0);
-            gps_key_trap = -1;
-            msleep(1000);
-        }
-    }
-#endif       
-
-	if (kbd_process() == 0) {
-		// we read keyboard state with _kbd_read_keys()
-		physw_status[0] = kbd_new_state[0];
-		physw_status[1] = kbd_new_state[1];
-		physw_status[2] = kbd_new_state[2];
-	} else {
-		// override keys
-		physw_status[0] = (kbd_new_state[0] | KEYS_MASK0) & (~KEYS_MASK0 | kbd_mod_state[0]); 
-		physw_status[1] = (kbd_new_state[1] | KEYS_MASK1) & (~KEYS_MASK1 | kbd_mod_state[1]); 
-		physw_status[2] = (kbd_new_state[2] | KEYS_MASK2) & (~KEYS_MASK2 | kbd_mod_state[2]);
-	}
-
-        if (conf.remote_enable) {
-                physw_status[USB_IDX] = physw_status[USB_IDX] & ~(SD_READONLY_FLAG | USB_MASK);
-        } else {
-                physw_status[USB_IDX] = physw_status[USB_IDX] & ~SD_READONLY_FLAG;
-        }
-
+    kbd_update_key_state();
+    kbd_update_physw_bits();
 }
 
-
-void kbd_key_press(long key) {
-	
-	int i;
-	
-	for (i=0;keymap[i].hackkey;i++) {
-		if (keymap[i].hackkey == key) {
-			kbd_mod_state[keymap[i].grp] &= ~keymap[i].canonkey;
-			return;
-		}
-	}
+void kbd_fetch_data(long *dst)
+{
+    _GetKbdState(dst);
+    _kbd_read_keys_r2(dst);
 }
-
-void kbd_key_release(long key) {
-	
-	int i;
-	
-	for (i=0;keymap[i].hackkey;i++) {
-		if (keymap[i].hackkey == key) {
-			kbd_mod_state[keymap[i].grp] |= keymap[i].canonkey;
-			return;
-		}
-	}
-}
-
-void kbd_key_release_all() {
-
-	kbd_mod_state[0] |= KEYS_MASK0;
-	kbd_mod_state[1] |= KEYS_MASK1;
-	kbd_mod_state[2] |= KEYS_MASK2;
-}
-
-long kbd_is_key_pressed(long key) {
-	
-	int i;
-	
-	for (i=0;keymap[i].hackkey;i++) {
-		if (keymap[i].hackkey == key) {
-			return ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0) ? 1:0;
-		}
-	}
-	return 0;
-}
-
-long kbd_is_key_clicked(long key) {
-	
-	int i;
-	
-	for (i=0;keymap[i].hackkey;i++) {
-		if (keymap[i].hackkey == key) {
-			return ((kbd_prev_state[keymap[i].grp] & keymap[i].canonkey) != 0) &&
-			((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0);
-		}
-	}
-	return 0;
-}
-
-long kbd_get_pressed_key() {
-	
-	int i;
-	
-	for (i=0;keymap[i].hackkey;i++) {
-		if ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0) {
-			return keymap[i].hackkey;
-		}
-	}
-	return 0;
-}
-
-long kbd_get_clicked_key() {
-	
-	int i;
-	
-	for (i=0;keymap[i].hackkey;i++) {
-		if (((kbd_prev_state[keymap[i].grp] & keymap[i].canonkey) != 0) &&
-		((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0)) {
-			return keymap[i].hackkey;
-		}
-	}
-	return 0;
-}
-
-
-#ifdef CAM_USE_ZOOM_FOR_MF
-long kbd_use_zoom_as_mf() {
-	
-	static long v;
-	static long zoom_key_pressed = 0;
-	
-	if (kbd_is_key_pressed(KEY_ZOOM_IN) && (mode_get()&MODE_MASK) == MODE_REC) {
-		get_property_case(PROPCASE_FOCUS_MODE, &v, 4);
-		if (v) {
-			kbd_key_release_all();
-			kbd_key_press(KEY_RIGHT);
-			zoom_key_pressed = KEY_ZOOM_IN;
-			return 1;
-		}
-	} else {
-		if (zoom_key_pressed==KEY_ZOOM_IN) {
-			kbd_key_release(KEY_RIGHT);
-			zoom_key_pressed = 0;
-			return 1;
-		}
-	}
-	
-	if (kbd_is_key_pressed(KEY_ZOOM_OUT) && (mode_get()&MODE_MASK) == MODE_REC) {
-		get_property_case(PROPCASE_FOCUS_MODE, &v, 4);
-		if (v) {
-			kbd_key_release_all();
-			kbd_key_press(KEY_LEFT);
-			zoom_key_pressed = KEY_ZOOM_OUT;
-			return 1;
-		}
-	} else {
-		if (zoom_key_pressed==KEY_ZOOM_OUT) {
-			kbd_key_release(KEY_LEFT);
-			zoom_key_pressed = 0;
-			return 1;
-		}
-	}
-
-	return 0;
-}
-#endif
