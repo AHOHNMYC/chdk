5217234b1def66f399aff4327d1b6271d21d8d09
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-17 05:36:21 +0000

    keyboard rework - ixus200_sd980, ixus220_elph300hs, ixus230_elph310hs, ixus255_elph330hs, ixus750_sd550 * ixus220_elph300 removed hard coded battery door override, now depends on OPT_RUN_WITH_BATT_COVER_OPEN * ixus230_elph310hs removed PRINT alias for playback key * ixus255_elph330hs removed spurious call to usb_remote_key

--- platform/ixus255_elph330hs/platform_kbd.h
+++ platform/ixus255_elph330hs/platform_kbd.h
@@ -1,5 +1,16 @@
 #ifndef PLATFORM_KBD_H
 #define PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+// Ignores power button
+#define KEYS_MASK0 (0x00000ce3)
+#define KEYS_MASK1 (0x00b00000)
+#define KEYS_MASK2 (0x00005000)
+
+#define SD_READONLY_FLAG    0x02000000 // Found @0xff5b8984, levent 0x20a
+#define SD_READONLY_IDX     1
+
+#define USB_MASK            0x10000000 // Found @0xff5b89c4, levent 0x202
+#define USB_IDX             2
+
+#define BATTCOVER_FLAG      0x04000000 // Found @0xff5b89b4, levent 0x205
+#define BATTCOVER_IDX       2
 #endif
--- platform/ixus750_sd550/platform_kbd.h
+++ platform/ixus750_sd550/platform_kbd.h
@@ -1,5 +1,15 @@
 #ifndef PLATFORM_KBD_H
 #define PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+
+#define KEYS_MASK0 0x00000000
+#define KEYS_MASK1 0x00000000
+#define KEYS_MASK2 0x0000affe
+
+#define SD_READONLY_FLAG (0x20000)
+#define SD_READONLY_IDX 2
+
+#define USB_MASK 0x40 
+#define USB_IDX 1
+
+#define USB_MMIO 0xc0220204
 #endif
