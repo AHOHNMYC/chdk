9de112efb859b8b65d2d9c48de85844120dc75da
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-18 05:48:42 +0000

    keyboard rework - ixus850_sd800, ixus860_sd870, ixus960_sd950, ixus970_sd980, ixus980_sd990 * ixus980_sd990 - added KEY_PLAYBACK to keymap and mask

--- platform/ixus980_sd990/platform_kbd.h
+++ platform/ixus980_sd990/platform_kbd.h
@@ -1,5 +1,13 @@
 #ifndef PLATFORM_KBD_H
 #define PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+#define KEYS_MASK0 (0x00000000)
+#define KEYS_MASK1 (0x80000000)
+#define KEYS_MASK2 (0x0FFF)
+
+#define SD_READONLY_FLAG (0x20000)
+#define SD_READONLY_IDX 2
+
+#define USB_MASK (0x40000)
+#define USB_IDX  2
+
 #endif
