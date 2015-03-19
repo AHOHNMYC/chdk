150cb88ad31641a74bfa738e9fd4ccf25be62981
srsa <srsa@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-19 19:18:24 +0000

    keyboard rework: a470

--- platform/a470/kbd.c
+++ platform/a470/kbd.c
@@ -3,30 +3,34 @@
 #include "core.h"
 #include "keyboard.h"
 #include "conf.h"
+#include "kbd_common.h"
 
-typedef struct {
-	short grp;
-	short hackkey;
-	long canonkey;
-} KeyMap;
+KeyMap keymap[] = {
+    /* tiny bug: key order matters. see kbd_get_pressed_key()
+     * for example
+     */
+//  { 1, KEY_POWER      ,     0x04000000 }, // levent 0x600 (uses inverted logic in physw_status)
+    { 2, KEY_UP         ,     0x00000001 },
+    { 2, KEY_DOWN       ,     0x00000002 },
+    { 2, KEY_LEFT       ,     0x00000008 },
+    { 2, KEY_RIGHT      ,     0x00000004 },
+    { 2, KEY_SET        ,     0x00000100 },
+    { 2, KEY_SHOOT_FULL ,     0x00000030 },
+    { 2, KEY_SHOOT_FULL_ONLY, 0x00000020 },
+    { 2, KEY_SHOOT_HALF ,     0x00000010 },
+    { 2, KEY_MENU       ,     0x00000400 },
+    { 2, KEY_PRINT      ,     0x00001000 },
+    { 0, 0, 0 }, // placeholder for fake key
+    { 0, 0, 0 }
+};
 
 long kbd_new_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
-static long kbd_prev_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
-static long kbd_mod_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+long kbd_prev_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+long kbd_mod_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
 
-static KeyMap keymap[];
-
-#define KEYS_MASK0 (0x00000000)
-static int keys_mask1 = 0x00000000;
-static int keys_inv1 = 0x00000000;
-#define KEYS_MASK2 (0x153F)
+int keys_mask1 = 0x00000000;
+int keys_inv1 = 0x00000000;
 static int set_fake_key=0;
-
-#define NEW_SS (0x2000)
-#define SD_READONLY_FLAG (0x20000)
-
-#define USB_MASK (0x40000)
-#define USB_IDX 2
 
 int get_usb_bit() 
 {
@@ -36,67 +40,28 @@ int get_usb_bit()
 	return(( usb_physw[USB_IDX] & USB_MASK)==USB_MASK) ; 
 }
 
-#ifndef MALLOCD_STACK
-static char kbd_stack[NEW_SS];
-#endif
-
 void kbd_fetch_data(long*);
-
-
 
 
 long __attribute__((naked)) wrap_kbd_p1_f() ;
 
-static void __attribute__((noinline)) mykbd_task_proceed()
+
+// task created directly, stack manipulation is not needed
+void __attribute__((naked,noinline))
+mykbd_task(long ua, long ub, long uc, long ud, long ue, long uf)
 {
+
     kbd_new_state[0] = physw_status[0];
     kbd_new_state[1] = physw_status[1] ^ keys_inv1;
     kbd_new_state[2] = physw_status[2];
 
     while (physw_run){
-	_SleepTask(10);
+        _SleepTask(10);
 
-	if (wrap_kbd_p1_f() == 1){ // autorepeat ?
-	    _kbd_p2_f();
-	}
+        if (wrap_kbd_p1_f() == 1){ // autorepeat ?
+            _kbd_p2_f();
+        }
     }
-}
-
-void __attribute__((naked,noinline))
-mykbd_task(long ua, long ub, long uc, long ud, long ue, long uf)
-{
-    /* WARNING
-     * Stack pointer manipulation performed here!
-     * This means (but not limited to):
-     *	function arguments destroyed;
-     *	function CAN NOT return properly;
-     *	MUST NOT call or use stack variables before stack
-     *	is setup properly;
-     *
-     */
-
-    register int i;
-    register long *newstack;
-
-#ifndef MALLOCD_STACK
-    newstack = (void*)kbd_stack;
-#else
-    newstack = malloc(NEW_SS);
-#endif
-
-    for (i=0;i<NEW_SS/4;i++)
-	newstack[i]=0xdededede;
-
-    asm volatile (
-	"MOV	SP, %0"
-	:: "r"(((char*)newstack)+NEW_SS)
-	: "memory"
-    );
-
-    mykbd_task_proceed();
-
-    /* function can be modified to restore SP here...
-     */
 
     _ExitTask();
 }
@@ -122,8 +87,6 @@ void my_kbd_read_keys()
     kbd_prev_state[1] = kbd_new_state[1];
     kbd_prev_state[2] = kbd_new_state[2];
 
-    _kbd_pwr_on();
-
     kbd_fetch_data(kbd_new_state);
 
     kbd_new_state[1] = kbd_new_state[1] ^ keys_inv1;
@@ -138,8 +101,8 @@ void my_kbd_read_keys()
 	physw_status[0] = (kbd_new_state[0] & (~KEYS_MASK0)) |
 			  (kbd_mod_state[0] & KEYS_MASK0);
 
-	physw_status[1] = ((kbd_new_state[1] & (~keys_mask1)) |
-			  (kbd_mod_state[1] & keys_mask1)) ^ keys_inv1;
+	physw_status[1] = ((kbd_new_state[1] & (~KEYS_MASK1)) |
+			  (kbd_mod_state[1] & KEYS_MASK1)) ^ keys_inv1;
 
 	physw_status[2] = (kbd_new_state[2] & (~KEYS_MASK2)) |
 			  (kbd_mod_state[2] & KEYS_MASK2);
@@ -147,11 +110,8 @@ void my_kbd_read_keys()
 
     _kbd_read_keys_r2(physw_status);
 
-    if (conf.remote_enable) {
-      physw_status[USB_IDX] = physw_status[USB_IDX] & ~(SD_READONLY_FLAG | USB_MASK);
-     }
-    else physw_status[USB_IDX] = physw_status[USB_IDX] & ~SD_READONLY_FLAG;
-    _kbd_pwr_off();
+    // usb and SD read-only are standard
+    kbd_update_physw_bits();
 
     if (set_fake_key & 0x10000000) {
         enable_extra_button((short)(set_fake_key & 0xffff));
@@ -159,139 +119,6 @@ void my_kbd_read_keys()
     }
 }
 
-
-
-void kbd_key_press(long key)
-{
-    int i;
-    for (i=0;keymap[i].hackkey;i++){
-	if (keymap[i].hackkey == key){
-	    kbd_mod_state[keymap[i].grp] &= ~keymap[i].canonkey;
-	    return;
-	}
-    }
-}
-
-void kbd_key_release(long key)
-{
-    int i;
-    for (i=0;keymap[i].hackkey;i++){
-	if (keymap[i].hackkey == key){
-	    kbd_mod_state[keymap[i].grp] |= keymap[i].canonkey;
-	    return;
-	}
-    }
-}
-
-void kbd_key_release_all()
-{
-  kbd_mod_state[0] |= KEYS_MASK0;
-  kbd_mod_state[1] |= keys_mask1;
-  kbd_mod_state[2] |= KEYS_MASK2;
-}
-
-long kbd_is_key_pressed(long key)
-{
-    int i;
-    for (i=0;keymap[i].hackkey;i++){
-	if (keymap[i].hackkey == key){
-	    return ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0) ? 1:0;
-	}
-    }
-    return 0;
-}
-
-long kbd_is_key_clicked(long key)
-{
-    int i;
-    for (i=0;keymap[i].hackkey;i++){
-	if (keymap[i].hackkey == key){
-	    return ((kbd_prev_state[keymap[i].grp] & keymap[i].canonkey) != 0) &&
-		    ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0);
-	}
-    }
-    return 0;
-}
-
-long kbd_get_pressed_key()
-{
-	int i;
-    for (i=0;keymap[i].hackkey;i++){
-	if ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0){
-	    return keymap[i].hackkey;
-	}
-    }
-    return 0;
-}
-
-long kbd_get_clicked_key()
-{
-    int i;
-    for (i=0;keymap[i].hackkey;i++){
-	if (((kbd_prev_state[keymap[i].grp] & keymap[i].canonkey) != 0) &&
-	    ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0)){
-	    return keymap[i].hackkey;
-	}
-    }
-    return 0;
-}
-
-long kbd_use_zoom_as_mf() {
-    static long v;
-    static long zoom_key_pressed = 0;
-
-    if (kbd_is_key_pressed(KEY_ZOOM_IN) && (mode_get()&MODE_MASK) == MODE_REC) {
-        get_property_case(PROPCASE_FOCUS_MODE, &v, 4);
-        if (v) {
-            kbd_key_release_all();
-            kbd_key_press(KEY_RIGHT);
-            zoom_key_pressed = KEY_ZOOM_IN;
-            return 1;
-        }
-    } else {
-        if (zoom_key_pressed==KEY_ZOOM_IN) {
-            kbd_key_release(KEY_RIGHT);
-            zoom_key_pressed = 0;
-            return 1;
-        }
-    }
-    if (kbd_is_key_pressed(KEY_ZOOM_OUT) && (mode_get()&MODE_MASK) == MODE_REC) {
-        get_property_case(PROPCASE_FOCUS_MODE, &v, 4);
-        if (v) {
-            kbd_key_release_all();
-            kbd_key_press(KEY_LEFT);
-            zoom_key_pressed = KEY_ZOOM_OUT;
-            return 1;
-        }
-    } else {
-        if (zoom_key_pressed==KEY_ZOOM_OUT) {
-            kbd_key_release(KEY_LEFT);
-            zoom_key_pressed = 0;
-            return 1;
-        }
-    }
-    return 0;
-}
-
-
-static KeyMap keymap[] = {
-    /* tiny bug: key order matters. see kbd_get_pressed_key()
-     * for example
-     */
-//	{ 1, KEY_POWER		, 0x04000000 }, // levent 0x600 (uses inverted logic in physw_status)
-	{ 2, KEY_UP			, 0x00000001 }, //
-	{ 2, KEY_DOWN		, 0x00000002 }, // 
-	{ 2, KEY_LEFT		, 0x00000008 }, // 
-	{ 2, KEY_RIGHT		, 0x00000004 }, //
-	{ 2, KEY_SET		, 0x00000100 }, //
-	{ 2, KEY_SHOOT_FULL	, 0x00000030 }, 
-    { 2, KEY_SHOOT_FULL_ONLY, 0x00000020 },
-	{ 2, KEY_SHOOT_HALF	, 0x00000010 },  
-	{ 2, KEY_MENU		, 0x00000400 }, 
-	{ 2, KEY_PRINT		, 0x00001000 }, //
-	{ 0, 0, 0 }, // placeholder
-	{ 0, 0, 0 }
-};
 
 void enable_extra_button(short key) {
 /*
