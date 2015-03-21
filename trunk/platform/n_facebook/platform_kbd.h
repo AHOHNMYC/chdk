f4e503a3104b5809dfef8f4ce3c7a084fafdf316
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-21 04:40:00 +0000

    keyboard rework - n_facebook. Copied form N, UNTESTED

--- trunk/platform/n_facebook/platform_kbd.h
+++ trunk/platform/n_facebook/platform_kbd.h
@@ -1,5 +1,23 @@
 #ifndef PLATFORM_KBD_H
 #define PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+
+#define KEYS_MASK0 0x30000000
+#define KEYS_MASK1 0x00000000
+#define KEYS_MASK2 0x00030330
+
+// Micro SD, no read only bit
+#define KBD_SKIP_READONLY_BIT
+//#define SD_READONLY_IDX  
+//#define SD_READONLY_FLAG 
+// uses SD door for autoboot. SD door is different from battery door
+#define SD_DOOR_OVERRIDE 1
+#define SD_DOOR_FLAG    0x00080000 // SD card door switch
+#define SD_DOOR_IDX     2
+
+#define USB_MASK 0x10000000
+#define USB_IDX  2
+
+// simulate video button with levents
+#define KBD_SIMULATE_VIDEO_KEY 1
+
 #endif
