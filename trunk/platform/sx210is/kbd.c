87bfbdbdc0c20bbc550d42ccaa374415152a6c0c
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-22 19:23:59 +0000

    keyboard rework - sx210is. UNTESTED converted to use GetKbdState, read_keys_r2

--- trunk/platform/sx210is/kbd.c
+++ trunk/platform/sx210is/kbd.c
@@ -1,276 +1,13 @@
 #include "lolevel.h"
 #include "platform.h"
-#include "core.h"
 #include "keyboard.h"
-#include "conf.h"
+#include "kbd_common.h"
 
-typedef struct {
-    short grp;
-    short hackkey;
-    long canonkey;
-} KeyMap;
+long kbd_new_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+long kbd_prev_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+long kbd_mod_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
 
-static long kbd_new_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
-static long kbd_prev_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
-static long kbd_mod_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
-static KeyMap keymap[];
-
-#define DELAY_TIMEOUT 10000
-
-#define KEYS_MASK0 (0x00802182)     // physw_status[0]
-//#define KEYS_MASK1 (0x000FC005)
-
-//#define KEYS_MASK1 (0x000FCF05)
-// override key and feather bits to avoid feather osd messing up chdk display in ALT mode
-#define KEYS_MASK1 (0x000FFC05)
-#define KEYS_MASK2 (0x00000000)  // physw_status[2]
-
-#define NEW_SS (0x2000)
-
-#define SD_READONLY_FLAG    0x00020000 // Found @0xffb65924, levent 0x90a
-#define SD_READONLY_IDX     2
-#define USB_MASK            0x00080000 // Found @0xffb6592c, levent 0x902
-#define USB_IDX             2
-
-#ifndef MALLOCD_STACK
-static char kbd_stack[NEW_SS];
-#endif
-
-
-int get_usb_bit() 
-{
-	long usb_physw[3];
-	usb_physw[USB_IDX] = 0;
-	_kbd_read_keys_r2(usb_physw);
-	return(( usb_physw[USB_IDX] & USB_MASK)==USB_MASK) ; 
-}
-
-
-volatile int jogdial_stopped=0;
-
-long __attribute__((naked)) wrap_kbd_p1_f();
-
-static void __attribute__((noinline)) mykbd_task_proceed() {
-    while (physw_run) {
-        _SleepTask(10);
-
-        if (wrap_kbd_p1_f() == 1) {   // autorepeat ?
-            _kbd_p2_f();
-        }
-    }
-}
-
-// no stack manipulation needed here, since we create the task directly
-void __attribute__((naked,noinline)) mykbd_task() {
-	register int i;
-	register long *newstack;
-
-#ifndef MALLOCD_STACK
-	newstack = (void*)kbd_stack;
-#else
-	newstack = malloc(NEW_SS);
-#endif
-
-	for (i=0;i<NEW_SS/4;i++)
-		newstack[i]=0xdededede;
-
-	asm volatile (
-		"MOV	SP, %0"
-		:: "r"(((char*)newstack)+NEW_SS)
-		: "memory"
-	);
-
-
-
-	mykbd_task_proceed();
-
-/* function can be modified to restore SP here...
-	 */
-
-	_ExitTask();
-}
-
-// ROM:FF83484C, like SX110
-long __attribute__((naked,noinline)) wrap_kbd_p1_f() {
-    asm volatile(
-                "STMFD   SP!, {R1-R5,LR}\n"
-                "MOV     R4, #0\n"
-                "BL      _kbd_read_keys\n"       // replaces kbd_fetch_data()
-                "BL      my_kbd_read_keys\n"     // +
-                "B       _kbd_p1_f_cont\n"       // continue at ROM:FF834858
-    );
-    return 0;   // shut up the compiler
-}
-
-// like SX110
-void my_kbd_read_keys() {
-    kbd_prev_state[0] = kbd_new_state[0];
-    kbd_prev_state[1] = kbd_new_state[1];
-    //kbd_prev_state[2] = kbd_new_state[2];   // nothing to override
-
-    // The following three lines replace the call to kbd_fetch_data()
-    kbd_new_state[0] = physw_status[0];
-    kbd_new_state[1] = physw_status[1];
-    //kbd_new_state[2] = physw_status[2];
-
-    if (kbd_process() == 0) {
-        // we read keyboard state with _kbd_read_keys()
-
-        jogdial_stopped=0;
-    } else {
-        // override keys
-        physw_status[0] = (kbd_new_state[0] | KEYS_MASK0) & (~KEYS_MASK0 | kbd_mod_state[0]);
-        physw_status[1] = (kbd_new_state[1] | KEYS_MASK1) & (~KEYS_MASK1 | kbd_mod_state[1]);
-        //physw_status[2] = (kbd_new_state[2] | KEYS_MASK2) & (~KEYS_MASK2 | kbd_mod_state[2]);
-
-        if ((jogdial_stopped==0) && !camera_info.state.state_kbd_script_run) {
-            jogdial_stopped=1;
-            get_jogdial_direction();
-        }
-        else if (jogdial_stopped && camera_info.state.state_kbd_script_run)
-            jogdial_stopped=0;
-    }
-
-    if (conf.remote_enable) {
-        physw_status[USB_IDX] = physw_status[USB_IDX] & ~(SD_READONLY_FLAG | USB_MASK);   // override USB and SD-Card Readonly Bits
-    }
-    else
-        physw_status[USB_IDX] = physw_status[USB_IDX] & ~SD_READONLY_FLAG;   // override SD-Card Readonly Bit
-}
-
-void kbd_key_press(long key) {
-    int i;
-    for (i=0;keymap[i].hackkey;i++){
-        if (keymap[i].hackkey == key){
-            kbd_mod_state[keymap[i].grp] &= ~keymap[i].canonkey;
-            return;
-        }
-    }
-}
-
-void kbd_key_release(long key) {
-    int i;
-    for (i=0;keymap[i].hackkey;i++) {
-        if (keymap[i].hackkey == key){
-            kbd_mod_state[keymap[i].grp] |= keymap[i].canonkey;
-            return;
-        }
-    }
-}
-
-void kbd_key_release_all() {
-    kbd_mod_state[0] |= KEYS_MASK0;
-    kbd_mod_state[1] |= KEYS_MASK1;
-    kbd_mod_state[2] |= KEYS_MASK2;
-}
-
-long kbd_is_key_pressed(long key) {
-    int i;
-    for (i=0;keymap[i].hackkey;i++) {
-        if (keymap[i].hackkey == key) {
-            return ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0) ? 1:0;
-        }
-    }
-    return 0;
-}
-
-long kbd_is_key_clicked(long key) {
-    int i;
-    for (i=0;keymap[i].hackkey;i++) {
-        if (keymap[i].hackkey == key){
-            return ((kbd_prev_state[keymap[i].grp] & keymap[i].canonkey) != 0) &&
-                ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0);
-        }
-    }
-    return 0;
-}
-
-long kbd_get_pressed_key() {
-    int i;
-    for (i=0;keymap[i].hackkey;i++) {
-        if ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0) {
-            return keymap[i].hackkey;
-        }
-    }
-    return 0;
-}
-
-long kbd_get_clicked_key() {
-    int i;
-    for (i=0;keymap[i].hackkey;i++) {
-        if (((kbd_prev_state[keymap[i].grp] & keymap[i].canonkey) != 0) && ((kbd_new_state[keymap[i].grp] & keymap[i].canonkey) == 0)) {
-            return keymap[i].hackkey;
-        }
-    }
-    return 0;
-}
-
-#ifdef CAM_USE_ZOOM_FOR_MF
-    long kbd_use_zoom_as_mf() {
-        static long v;
-        static long zoom_key_pressed = 0;
-
-        if (kbd_is_key_pressed(KEY_ZOOM_IN) && (mode_get()&MODE_MASK) == MODE_REC) {
-            get_property_case(PROPCASE_FOCUS_MODE, &v, 4);
-            if (v) {
-                kbd_key_release_all();
-                kbd_key_press(KEY_RIGHT);
-                zoom_key_pressed = KEY_ZOOM_IN;
-                return 1;
-            }
-        } else {
-            if (zoom_key_pressed==KEY_ZOOM_IN) {
-                kbd_key_release(KEY_RIGHT);
-                zoom_key_pressed = 0;
-                return 1;
-            }
-        }
-        if (kbd_is_key_pressed(KEY_ZOOM_OUT) && (mode_get()&MODE_MASK) == MODE_REC) {
-            get_property_case(PROPCASE_FOCUS_MODE, &v, 4);
-            if (v) {
-                kbd_key_release_all();
-                kbd_key_press(KEY_LEFT);
-                zoom_key_pressed = KEY_ZOOM_OUT;
-                return 1;
-            }
-        } else {
-            if (zoom_key_pressed==KEY_ZOOM_OUT) {
-                kbd_key_release(KEY_LEFT);
-                zoom_key_pressed = 0;
-                return 1;
-            }
-        }
-        return 0;
-    }
-#endif
-
-int Get_JogDial(void) {
-    return (*(int*)0xC0240104)>>16;     // 0xC0240000 + 0x104
-}
-
-static int new_jogdial=0, old_jogdial=0;
-
-long get_jogdial_direction(void) {
-    old_jogdial=new_jogdial;
-    new_jogdial=Get_JogDial();
-
-    if (old_jogdial<new_jogdial) {
-        //return JOGDIAL_LEFT;
-        return JOGDIAL_RIGHT;
-    } else if (old_jogdial>new_jogdial) {
-        return JOGDIAL_LEFT;
-        //return JOGDIAL_RIGHT;
-    } else {
-        return 0;
-    }
-}
-
-// NOP
-void kbd_set_alt_mode_key_mask(long key)
-{
-}
-
-static KeyMap keymap[] = {
+KeyMap keymap[] = {
 
 		//play button doesn't alter anything here...????
 
@@ -301,7 +38,7 @@ static KeyMap keymap[] = {
 	{ 1, KEY_DISPLAY         , 0x00020000 },  // ASM1989 tested (JH) - fffff -> dffff
 	{ 1, KEY_PRINT		     , 0x00080000 },  // ASM1989 tested (JH) - fffff -> 7ffff
 	{ 1, KEY_VIDEO		     , 0x00080000 },
-     	{ 0, 0, 0 }
+    { 0, 0, 0 }
 };
 /*
 In Review mode   physw_status      0      1      2
@@ -331,3 +68,124 @@ iddle video   <>   880a19b   <>   ffddf   <>   400000
 iddle none   <>   880a19b   <>   ffebf   <>   400000
 */
 
+#define NEW_SS (0x2000)
+
+static char kbd_stack[NEW_SS];
+
+int get_usb_bit() 
+{
+	long usb_physw[3];
+	usb_physw[USB_IDX] = 0;
+	_kbd_read_keys_r2(usb_physw);
+	return(( usb_physw[USB_IDX] & USB_MASK)==USB_MASK) ; 
+}
+
+
+int jogdial_stopped=0;
+
+long __attribute__((naked)) wrap_kbd_p1_f();
+
+static void __attribute__((noinline)) mykbd_task_proceed() {
+    while (physw_run) {
+        _SleepTask(10);
+
+        if (wrap_kbd_p1_f() == 1) {   // autorepeat ?
+            _kbd_p2_f();
+        }
+    }
+}
+
+void __attribute__((naked,noinline)) mykbd_task() {
+    /* WARNING
+     * Stack pointer manipulation performed here!
+     * This means (but not limited to):
+     *	function arguments destroyed;
+     *	function CAN NOT return properly;
+     *	MUST NOT call or use stack variables before stack
+     *	is setup properly;
+     *
+     */
+
+	register int i;
+	register long *newstack;
+
+	newstack = (void*)kbd_stack;
+
+	for (i=0;i<NEW_SS/4;i++)
+		newstack[i]=0xdededede;
+
+	asm volatile (
+		"MOV	SP, %0"
+		:: "r"(((char*)newstack)+NEW_SS)
+		: "memory"
+	);
+
+
+
+	mykbd_task_proceed();
+
+/* function can be modified to restore SP here...
+	 */
+
+	_ExitTask();
+}
+
+// ROM:FF83484C, like SX110
+long __attribute__((naked,noinline)) wrap_kbd_p1_f() {
+    asm volatile(
+                "STMFD   SP!, {R1-R5,LR}\n"
+                "MOV     R4, #0\n"
+//                "BL      _kbd_read_keys\n"
+                "BL      my_kbd_read_keys\n"     // +
+                "B       _kbd_p1_f_cont\n"       // continue at ROM:FF834858
+    );
+    return 0;   // shut up the compiler
+}
+
+extern void _GetKbdState(long *dst);
+
+void my_kbd_read_keys() {
+    kbd_update_key_state();
+    kbd_update_physw_bits();
+}
+
+void kbd_fetch_data(long *dst)
+{
+    _GetKbdState(dst);
+    _kbd_read_keys_r2(dst);
+}
+
+void jogdial_control(int n) {
+    // this camera did not have jog_position defined
+    /*
+    if (jogdial_stopped && !n) {
+        // If re-enabling jogdial set the task code current & previous positions to the actual
+        // dial positions so that the change won't get processed by the firmware
+        jog_position[0] = jog_position[2] = rear_dial_position;   // Rear dial
+    }
+    */
+    jogdial_stopped = n;
+}
+
+int Get_JogDial(void) {
+    return (*(int*)0xC0240104)>>16;     // 0xC0240000 + 0x104
+}
+
+static int new_jogdial=0, old_jogdial=0;
+
+long get_jogdial_direction(void) {
+    old_jogdial=new_jogdial;
+    new_jogdial=Get_JogDial();
+
+    if (old_jogdial<new_jogdial) {
+        //return JOGDIAL_LEFT;
+        return JOGDIAL_RIGHT;
+    } else if (old_jogdial>new_jogdial) {
+        return JOGDIAL_LEFT;
+        //return JOGDIAL_RIGHT;
+    } else {
+        return 0;
+    }
+}
+
+void kbd_set_alt_mode_key_mask(long key) { }
