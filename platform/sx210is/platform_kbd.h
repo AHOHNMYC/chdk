f46f2bb18b4e8907df4d5f5b4596f16ddd87ded3
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-22 19:23:59 +0000

    keyboard rework - sx210is. UNTESTED converted to use GetKbdState, read_keys_r2

--- platform/sx210is/platform_kbd.h
+++ platform/sx210is/platform_kbd.h
@@ -1,5 +1,14 @@
 #ifndef PLATFORM_KBD_H
 #define PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+#define KEYS_MASK0 (0x00802182)     // physw_status[0]
+// override key and feather bits to avoid feather osd messing up chdk display in ALT mode
+// manual confirms this camera has touch wheel feature
+#define KEYS_MASK1 (0x000FFC05)
+#define KEYS_MASK2 (0x00000000)  // physw_status[2]
+
+#define SD_READONLY_FLAG    0x00020000 // Found @0xffb65924, levent 0x90a
+#define SD_READONLY_IDX     2
+
+#define USB_MASK            0x00080000 // Found @0xffb6592c, levent 0x902
+#define USB_IDX             2
 #endif
