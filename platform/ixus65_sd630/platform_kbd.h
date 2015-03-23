bee75a1cdcd3f84ee3a3f94e3cbabcdaadb44f98
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-23 01:01:05 +0000

    keyboard rework - ixus65_sd630, UNTESTED * changed kbd_task to use sleep(10) instead of sleep(5) * use pwr on/off, GetKbdState, read_keys_r2

--- platform/ixus65_sd630/platform_kbd.h
+++ platform/ixus65_sd630/platform_kbd.h
@@ -1,5 +1,16 @@
 #ifndef PLATFORM_KBD_H
 #define PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+#define KEYS_MASK0 0x00000000
+#define KEYS_MASK1 0x00000000
+#define KEYS_MASK2 0x000017FF
+
+#define SD_READONLY_FLAG (0x20000)
+#define SD_READONLY_IDX 2
+
+#define USB_MASK 0x8000000
+#define USB_IDX  1
+
+// MMIO to read USB +5v directly, for get_usb_bit
+#define USB_MMIO 0xc0220204
+
 #endif
