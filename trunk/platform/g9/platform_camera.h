7a82130d222b55c29ce33cc0120b6b5e0706296a
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-27 23:48:41 +0000

    g9 update: keyboard reworked, filewritetask added, port was rewritten using code_gen. tested by DaveX 100g and 100i: http://chdk.setepontos.com/index.php?topic=12271.msg121369#msg121369

--- trunk/platform/g9/platform_camera.h
+++ trunk/platform/g9/platform_camera.h
@@ -76,6 +76,8 @@
     #define CAM_ACTIVE_AREA_X2              4080
     #define CAM_ACTIVE_AREA_Y2              3036
 
+    #define CAM_HAS_FILEWRITETASK_HOOK      1
+
     #define REMOTE_SYNC_STATUS_LED          0xC022006C  // specifies an LED that turns on while camera waits for USB remote to sync
 
     #define CAM_SD_OVER_IN_AF               1
