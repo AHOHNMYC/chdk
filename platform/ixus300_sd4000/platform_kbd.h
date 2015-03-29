766fca15c2dcc905e90eb6bca5156c9bef14b823
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-29 05:50:57 +0000

    keyboard rework - ixus200_sd4000. Converted to GetKbdState/read_keys_r2, use physw_sleep_delay. UNTESTED

--- platform/ixus300_sd4000/platform_kbd.h
+++ platform/ixus300_sd4000/platform_kbd.h
@@ -1,5 +1,13 @@
 #ifndef PLATFORM_KBD_H
 #define PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+#define KEYS_MASK0 (0x0000090F)     // physw_status[0]
+// override key and feather bits to avoid feather osd messing up chdk display in ALT mode
+#define KEYS_MASK1 (0x00000FF0)     // physw_status[1], override 0xF0 (keys) + 0xF00 (feather)
+#define KEYS_MASK2 (0x00000000)     // physw_status[2]
+
+#define SD_READONLY_FLAG    (0x00020000)    // SD-Card Lock Status (locked / unlocked)
+#define SD_READONLY_IDX     2
+
+#define USB_MASK            (0x00080000)    // USB-Power (triggered around 3,5V)
+#define USB_IDX  2
 #endif
