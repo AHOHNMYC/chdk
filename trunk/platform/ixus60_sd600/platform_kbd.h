4b511d7a32c6e3f55b8a27d073495e9b8b9b0467
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-29 23:37:47 +0000

    keyboard rework - ixus60_sd600, UNTESTED * changed kbd_task to use sleep(10) instead of sleep(5) * used pwr on/off, GetKbdState, read_keys_r2 * removed "feather" code

--- trunk/platform/ixus60_sd600/platform_kbd.h
+++ trunk/platform/ixus60_sd600/platform_kbd.h
@@ -1,5 +1,14 @@
 #ifndef PLATFORM_KBD_H
 #define PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+#define KEYS_MASK0 0
+#define KEYS_MASK1 0
+#define KEYS_MASK2 0x17FF
+
+#define SD_READONLY_FLAG (0x20000)
+#define SD_READONLY_IDX 2
+
+#define USB_MASK 0x8000000
+#define USB_IDX  1
+
+#define USB_MMIO 0xc0220204
 #endif
