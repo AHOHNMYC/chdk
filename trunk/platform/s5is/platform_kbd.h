677d4457b5ffb64bef6ae918d5b294d1991b6dcc
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-30 00:46:49 +0000

    keyboard rework - s5is Converted to GetKbdState/read_keys_r2. UNTESTED

--- trunk/platform/s5is/platform_kbd.h
+++ trunk/platform/s5is/platform_kbd.h
@@ -1,5 +1,17 @@
 #ifndef PLATFORM_KBD_H
 #define PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+#define KEYS_MASK0 (0x00000003)
+#define KEYS_MASK1 (0x7F3F7860)
+#define KEYS_MASK2 (0x00000000)
+
+#define SD_READONLY_FLAG (0x20000)
+#define SD_READONLY_IDX 2
+
+#define USB_MASK (0x200000)
+#define USB_IDX  2
+
+#define HOTSHOE_FLAG        0x00080000 // Found @0xffa9cec8, levent 0x904
+#define HOTSHOE_IDX         2
+
+#define KBD_ZOOM_FOR_MF_USE_MF_KEY 1
 #endif
