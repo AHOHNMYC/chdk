42ed71e3dfdbf50172b9be8049f75a4ad361c0c2
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-30 04:36:40 +0000

    keyboard rework - ixusw_sd430 UNTESTED * converted to pwr_on, pwr_off, GetKbdState. read_keys_r2 appears to be a nullsub on this cam * changed sleep in kbd_task to use 10, like original firmware

--- platform/ixusw_sd430/platform_kbd.h
+++ platform/ixusw_sd430/platform_kbd.h
@@ -1,5 +1,15 @@
 #ifndef PLATFORM_KBD_H
 #define PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+#define KEYS_MASK0 0
+#define KEYS_MASK1 0
+#define KEYS_MASK2 0x2FFE
+
+#define SD_READONLY_FLAG (0x80000000)
+#define SD_READONLY_IDX 1
+
+#define USB_MASK 0x400000
+#define USB_IDX  0
+
+// NOTE original port had a TODO on this value, may not be verified
+#define USB_MMIO 0xc0220200
 #endif
