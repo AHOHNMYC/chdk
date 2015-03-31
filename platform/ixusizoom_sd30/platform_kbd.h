afa835b20cb6508813ac7692c674d087bb3a9f04
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-03-31 04:49:29 +0000

    keyboard rework - ixusizoom_sd30 UNTESTED * converted to pwr_on, pwr_off, GetKbdState, read_keys_r2 * changed sleep in kbd_task to use 10, like original firmware * removed feather related code * USB remote code is normal, but remote probably isn't usable due to unique hardware http://chdk.setepontos.com/index.php?topic=11723.msg114947#msg114947

--- platform/ixusizoom_sd30/platform_kbd.h
+++ platform/ixusizoom_sd30/platform_kbd.h
@@ -1,5 +1,16 @@
 #ifndef PLATFORM_KBD_H
 #define PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+#define KEYS_MASK0 0
+#define KEYS_MASK1 0
+#define KEYS_MASK2 0x17FF
+
+#define SD_READONLY_FLAG (0x20000)
+#define SD_READONLY_IDX 2
+
+//USB port is special: http://chdk.setepontos.com/index.php?topic=11723.msg114947#msg114947
+// remote probably not useful on this camera, but USB status can be detected
+#define USB_MASK 0x08000000 	
+#define USB_IDX 1
+
+#define USB_MMIO 0xc0220204
 #endif
