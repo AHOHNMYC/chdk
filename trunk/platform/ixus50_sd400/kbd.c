aa904fd04e6300190f1a2cc2eb0032c2c2481788
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-04-12 03:40:48 +0000

    keyboard rework - ixus50_sd400, UNTESTED * uses KBD_CUSTOM_UPDATE_KEY_STATE, KBD_CUSTOM_UPDATE_PHYSW_BITS * added forced_usb_port support

--- trunk/platform/ixus50_sd400/kbd.c
+++ trunk/platform/ixus50_sd400/kbd.c
@@ -3,36 +3,36 @@
 #include "conf.h"
 #include "core.h"
 #include "keyboard.h"
-
-typedef struct {
-	long hackkey;
-	long canonkey;
-} KeyMap;
-
+#include "kbd_common.h"
 
 long kbd_new_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
-static long kbd_prev_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
-static long kbd_mod_state = 0xFFFFFFFF ;
+long kbd_prev_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+long kbd_mod_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
 
-static KeyMap keymap[];
+KeyMap keymap[] = {
+    /* tiny bug: key order matters. see kbd_get_pressed_key()
+     * for example
+     */
+	{1, KEY_UP	, 0x00000020 },
+	{1, KEY_DOWN	, 0x00000040 },
+	{1, KEY_LEFT	, 0x00000080 },
+	{1, KEY_RIGHT	, 0x00000100 },
+	{1, KEY_SET	, 0x00000200 },
+	{1, KEY_SHOOT_FULL, 0x00000006 }, // note 6 here!
+    {1, KEY_SHOOT_FULL_ONLY, 0x00000004 },
+	{1, KEY_SHOOT_HALF, 0x00000002 },
+	{1, KEY_ZOOM_IN	, 0x00000008 },
+	{1, KEY_ZOOM_OUT	, 0x00000010 },
+	{1, KEY_MENU	, 0x00000400 },
+	{1, KEY_DISPLAY	, 0x00000800 },
+	{1, KEY_PRINT	, 0x00002000 },
+	{0, 0, 0 }
+};
 
 #define NEW_SS (0x2000)
-#define SD_READONLY_FLAG (0x20000)
 
-#define USB_MASK 0x40 
-#define USB_IDX 1
-
-int get_usb_bit() 
-{
-    if ((*(int*)0xc0220204) & USB_MASK) return 1;
-    return 0;
-}
-
-#ifndef MALLOCD_STACK
 static char kbd_stack[NEW_SS];
-#endif
 
-extern void _platformsub_kbd_fetch_data(long*);
 long __attribute__((naked)) wrap_kbd_p1_f();
 void __attribute__((naked,noinline)) mykbd_task_proceed_2();
 extern void h_kbd_p2_f();
@@ -79,11 +79,7 @@ mykbd_task(long ua, long ub, long uc, long ud, long ue, long uf)
     register int i;
     register long *newstack;
 
-#ifndef MALLOCD_STACK
     newstack = (void*)kbd_stack;
-#else
-    newstack = malloc(NEW_SS);
-#endif
 
     for (i=0;i<NEW_SS/4;i++)
 	newstack[i]=0xdededede;
@@ -126,120 +122,20 @@ long __attribute__((naked,noinline)) wrap_kbd_p1_f()
     return 0; // shut up the compiler
 }
 
-
-
+// functions below called from assembler in boot.c
 long my_kbd_read_keys(long x)
 {
- 
  kbd_new_state[1]=x & 0xFFFF;
- if (kbd_process() == 0) return x; else return (kbd_new_state[1]&~0x2FFE) | (kbd_mod_state & 0x2FFE);
+ if (kbd_process() == 0) return x; else return (kbd_new_state[1]&~KEYS_MASK1) | (kbd_mod_state[1] & KEYS_MASK1);
 }
 
-void kbd_key_press(long key)
-{
-    int i;
-    for (i=0;keymap[i].hackkey;i++){
-	if (keymap[i].hackkey == key){
-	    kbd_mod_state &= ~keymap[i].canonkey;
-	    return;
-	}
-    }
-}
-
-void kbd_key_release(long key)
-{
-    int i;
-    for (i=0;keymap[i].hackkey;i++){
-	if (keymap[i].hackkey == key){
-	    kbd_mod_state |= keymap[i].canonkey;
-	    return;
-	}
-    }
-}
-
-void kbd_key_release_all()
-{
-    kbd_mod_state |= 0x2FFE;
-}
-
-long kbd_is_key_pressed(long key)
-{
-    int i;
-    for (i=0;keymap[i].hackkey;i++){
-	if (keymap[i].hackkey == key){
-	    return ((kbd_new_state[1] & keymap[i].canonkey) == 0) ? 1:0;
-	}
-    }
-    return 0;
-}
-
-long kbd_is_key_clicked(long key)
-{
-    int i;
-    for (i=0;keymap[i].hackkey;i++){
-	if (keymap[i].hackkey == key){
-	    return ((kbd_prev_state[1] & keymap[i].canonkey) != 0) &&
-		    ((kbd_new_state[1] & keymap[i].canonkey) == 0);
-	}
-    }
-    return 0;
-}
-
-long kbd_get_pressed_key()
-{
-    int i;
-    for (i=0;keymap[i].hackkey;i++){
-	if ((kbd_new_state[1] & keymap[i].canonkey) == 0){
-	    return keymap[i].hackkey;
-	}
-    }
-    return 0;
-}
-
-long kbd_get_clicked_key()
-{
-    int i;
-    for (i=0;keymap[i].hackkey;i++){
-	if (((kbd_prev_state[1] & keymap[i].canonkey) != 0) &&
-	    ((kbd_new_state[1] & keymap[i].canonkey) == 0)){
-	    return keymap[i].hackkey;
-	}
-    }
-    return 0;
-}
-
-long kbd_use_zoom_as_mf() {
-  return 0;
-}
-
+extern int forced_usb_port;
 int usb_power_status_override(int status){
+    if (forced_usb_port) {
+        return status | USB_MASK;
+    }
 	if (conf.remote_enable) {
 		return status &~USB_MASK;
 	}
     return status;
 }
-
-
-static KeyMap keymap[] = {
-    /* tiny bug: key order matters. see kbd_get_pressed_key()
-     * for example
-     */
-	{ KEY_UP	, 0x00000020 },
-	{ KEY_DOWN	, 0x00000040 },
-	{ KEY_LEFT	, 0x00000080 },
-	{ KEY_RIGHT	, 0x00000100 },
-	{ KEY_SET	, 0x00000200 },
-	{ KEY_SHOOT_FULL, 0x00000006 }, // note 6 here!
-    { KEY_SHOOT_FULL_ONLY, 0x00000004 },
-	{ KEY_SHOOT_HALF, 0x00000002 },
-	{ KEY_ZOOM_IN	, 0x00000008 },
-	{ KEY_ZOOM_OUT	, 0x00000010 },
-	{ KEY_MENU	, 0x00000400 },
-	{ KEY_DISPLAY	, 0x00000800 },
-	{ KEY_PRINT	, 0x00002000 },
-//	{ KEY_ERASE	, 0x00000800 },
-//        { KEY_DUMMY	, 0x10000000 },
-	{ 0, 0 }
-};
-
-
