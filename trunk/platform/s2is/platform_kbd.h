1f8d80a2cc8f14a0ae24fda202f99457b7f44865
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-04-12 05:35:27 +0000

    keyboard rework - s2is, UNTESTED * uses KBD_CUSTOM_UPDATE_KEY_STATE

--- trunk/platform/s2is/platform_kbd.h
+++ trunk/platform/s2is/platform_kbd.h
@@ -1,5 +1,18 @@
 #ifndef PLATFORM_KBD_H
 #define PLATFORM_KBD_H
-// TEMP ignore platform keyboard code for camera not yet converted to common
-#define KBD_CUSTOM_ALL
+// platform code handles getting / updating key state, very different from other platforms
+#define KBD_CUSTOM_UPDATE_KEY_STATE 1
+
+#define KEYS_MASK0 (0x00000003)
+#define KEYS_MASK1 (0x5f7f7038)
+#define KEYS_MASK2 (0x00000000)
+
+#define SD_READONLY_FLAG (0x20000)
+#define SD_READONLY_IDX 0
+
+// NOTE get_usb_bit uses physw_status bit, so USB remote sync will NOT work
+#define USB_MASK (8) 
+#define USB_IDX  2
+
+#define KBD_ZOOM_FOR_MF_USE_MF_KEY 1
 #endif
