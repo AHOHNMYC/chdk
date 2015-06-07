3a995972b7465eff3fa9ec8121e72a9ebe28a414
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-06-07 22:48:57 +0000

    ixusizoom_sd30 - update notes and physw bits comments

--- trunk/platform/ixusizoom_sd30/platform_kbd.h
+++ trunk/platform/ixusizoom_sd30/platform_kbd.h
@@ -8,8 +8,22 @@
 #define SD_READONLY_FLAG (0x20000)
 #define SD_READONLY_IDX 2
 
-//USB port is special: http://chdk.setepontos.com/index.php?topic=11723.msg114947#msg114947
-// remote probably not useful on this camera, but USB status can be detected
+/*
+USB port is special: http://chdk.setepontos.com/index.php?topic=11723.msg114947#msg114947
+To use USB remote with the proprietary CS-DC1 cradle you must either switch to record mode using
+switch_mode_usb or using the following event proc calls
+  call_event_proc'UI_RegistDebugEventProc'
+  call_event_proc'UiEvnt_StartDisguiseCradleStatus'
+
+Other cradle related bits in physw_status[1] from http://chdk.setepontos.com/index.php?topic=12382.msg122778#msg122778
+inside cradle  0xZ0@9AF80
+outside cradle 0xZ0@9FF80
+
+The first 0 is the USB bit which becomes 8 upon USB plug-in.
+Z and @ I put for values that fluctuate between two possible ones and seem unrelated. @: C/5   Z: 3/2
+Unplugging the power plug changes A to B.
+Plugging an earphone cable into the AV plug changes the 9 before the green A to an 8.
+*/
 #define USB_MASK 0x08000000 	
 #define USB_IDX 1
 
