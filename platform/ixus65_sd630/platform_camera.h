c58612fe56ebee28a2c83c6b52442b207a7a77e0
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-07-06 01:59:39 +0000

    ixus65_sd630: filewrite task support Tested by koshy: http://chdk.setepontos.com/index.php?topic=11831.msg123307#msg123307

--- platform/ixus65_sd630/platform_camera.h
+++ platform/ixus65_sd630/platform_camera.h
@@ -65,6 +65,8 @@
     #define PARAM_CAMERA_NAME               3       // parameter number for GetParameterData
     #define DNG_EXT_FROM                    ".DPS"
 
+    #define CAM_HAS_FILEWRITETASK_HOOK      1
+
 //    #define REMOTE_SYNC_STATUS_LED     0xC0xxyyyy        // specifies an LED that turns on while camera waits for USB remote to sync
 
     // Keyboard repeat and initial delays
