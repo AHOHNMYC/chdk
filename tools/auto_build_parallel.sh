854ca185f7f9b1ed3d3f81b537ff2035e9566959
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-09-20 22:19:26 +0000

    disable stubs building for d6 even if forced with batch rebuild, make auto_build_parallel skip copied subs, cleanup tools Makefile

--- tools/auto_build_parallel.sh
+++ tools/auto_build_parallel.sh
@@ -8,9 +8,11 @@
 # parallel version - starts each build in a seperate session (Windows only)
 while IFS=, read cam fw state srcfw skip
 do
-  if [ ${cam} != "CAMERA" ] && ( [ "$4" = "-noskip" ] || [ "${skip}" = "" ] ); then \
-    if [ "${state}" != "" ]; then state=_${state}; fi; \
-    if [ "${srcfw}" = "" ]; then srcfw=${fw}; fi; \
-    start $1 -s --no-print-directory TARGET_FW=${fw} PLATFORM=${cam} PLATFORMSUB=${srcfw} STATE=${state} SKIP_AUTOBUILD=${skip} $2 || exit 1; \
-  fi;
+  if [ ${cam} != "CAMERA" ] && ( [ "$4" = "-noskip" ] || [ "${skip}" = "" ] ); then
+    if [ "${state}" != "" ]; then state=_${state}; fi
+# skip copied firmwares
+    if [ "${srcfw}" = "" ]; then
+    	start $1 -s --no-print-directory TARGET_FW=${fw} PLATFORM=${cam} PLATFORMSUB=${fw} STATE=${state} SKIP_AUTOBUILD=${skip} $2 || exit 1;
+	fi
+  fi
 done < $3
