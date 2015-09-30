cfe479259c66c22d358d9bb24c7998d2017544ce
msl <msl@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-09-30 19:46:37 +0000

    * SX230  - CAMERA_MAX_DIST according to the SX220

--- trunk/platform/sx230hs/platform_camera.h
+++ trunk/platform/sx230hs/platform_camera.h
@@ -104,6 +104,8 @@
 
     #undef  CAMERA_MIN_DIST
     #define CAMERA_MIN_DIST                 99     // Override min subject distance
+    #undef  CAMERA_MAX_DIST
+    #define CAMERA_MAX_DIST                 2000000 // see SX220
 
     #define CAM_HAS_GPS                     1
 
