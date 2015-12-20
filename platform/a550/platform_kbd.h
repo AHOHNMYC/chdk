df2e9ca43a3427bd6c73c744f110c16006eb9a21
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2015-12-20 19:48:24 +0000

    a550 - fix alt mode interfering with mode dial setting, reported by winterspace in http://chdk.setepontos.com/index.php?topic=12702.0 - trunk 4315

--- platform/a550/platform_kbd.h
+++ platform/a550/platform_kbd.h
@@ -3,9 +3,10 @@
 // plaform specific keyboard defines, see core/kbd_common.c for documentation
 
 #define KEYS_MASK0 (0x00000000)
-// from original kbd.c code, no keymap keys are in group 1
+// original kbd.c code used 0xc0000000 for MASK1. This caused shooting mode to change when entering alt mode
 // possible copy/paste error from a560
-#define KEYS_MASK1 (0xc0000000)
+#define KEYS_MASK1 (0x00000000)
+// note bit 0x800 is not in key map, but camera reportedly doesn't boot if omitted
 #define KEYS_MASK2 (0x1FFF)
 
 #define SD_READONLY_IDX 2
