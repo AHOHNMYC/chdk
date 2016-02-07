703083bcf3b51514557e9b779ea67e4c8d50a172
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2016-02-07 02:13:50 +0000

    finsig_thumb2 - malloc_strictly

--- tools/finsig_thumb2.c
+++ tools/finsig_thumb2.c
@@ -2263,6 +2263,7 @@ sig_rule_t sig_rules_main[]={
 {sig_match_named,   "PTM_GetCurrentItem",       "PTM_GetCurrentItem_FW",},
 // TODO assumes CreateTask is in RAM, doesn't currently check
 {sig_match_named,   "hook_CreateTask",          "CreateTask",           SIG_NAMED_CLEARTHUMB},
+{sig_match_named,   "malloc_strictly",          "task_EvShel",          SIG_NAMED_NTH(2,SUB)},
 {sig_match_log_camera_event,"LogCameraEvent",   "task_StartupImage",},
 {sig_match_physw_misc, "physw_misc",            "task_PhySw"},
 {sig_match_kbd_read_keys, "kbd_read_keys",      "kbd_p1_f"},
