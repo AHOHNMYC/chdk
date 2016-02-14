621b125401f9c4187415ccce8dc6a42692cd4267
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2016-02-14 07:07:36 +0000

    g7x - initial work. No shooting related tasks or hooks are implemented. Display and PTP are usable.

---
+++ platform/g7x/kbd.c
@@ -0,0 +1,126 @@
+#include "lolevel.h"
+#include "platform.h"
+#include "keyboard.h"
+#include "kbd_common.h"
+
+long kbd_new_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+long kbd_prev_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+long kbd_mod_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+
+extern void _GetKbdState(long*);
+
+
+int get_usb_bit() {
+    long usb_physw[3];
+    usb_physw[USB_IDX] = 0;
+    _kbd_read_keys_r2(usb_physw);
+    return(( usb_physw[USB_IDX] & USB_MASK)==USB_MASK) ;
+}
+
+KeyMap keymap[] = {
+    { 0, KEY_SHOOT_FULL      ,0x00040001 }, // Found @0xfc613714, levent 0x01
+    { 0, KEY_SHOOT_HALF      ,0x00040000 }, // Found @0xfc61378c, levent 0x00
+    { 0, KEY_SHOOT_FULL_ONLY ,0x00000001 }, // Found @0xfc613714, levent 0x01
+    { 0, KEY_ZOOM_OUT        ,0x00000002 }, // Found @0xfc61371c, levent 0x04
+    { 0, KEY_ZOOM_IN         ,0x00000004 }, // Found @0xfc613724, levent 0x03
+    { 0, KEY_VIDEO           ,0x00000008 }, // Found @0xfc61372c, levent 0x02
+    { 0, KEY_MENU            ,0x00000010 }, // Found @0xfc613734, levent 0x14
+    { 0, KEY_UP              ,0x00000020 }, // Found @0xfc61373c, levent 0x06
+    { 0, KEY_DOWN            ,0x00000040 }, // Found @0xfc613744, levent 0x07
+    { 0, KEY_RIGHT           ,0x00000080 }, // Found @0xfc61374c, levent 0x09
+    { 0, KEY_LEFT            ,0x00000100 }, // Found @0xfc613754, levent 0x08
+    { 0, KEY_SET             ,0x00000200 }, // Found @0xfc61375c, levent 0x0a
+    { 0, KEY_ERASE           ,0x00000400 }, // ring func / erase, from watching  physw
+    { 0, KEY_PLAYBACK        ,0x00000800 }, // Found @0xfc61376c, levent 0x101
+//    { 0, KEY_WIFI           ,0x00010000 }, // wifi button, from watching physw
+//    { 0, KEY_POWER           ,0x00020000 }, // Found @0xfc613784, levent 0x100
+    { 0, 0, 0 }
+};
+// ev comp and mode dial is in word 1, lower bits, encoding unclear
+// flash open word 0, 0x00004000 = open
+// no battery door switch, cam runs with door open, no physw change
+
+long __attribute__((naked,noinline)) wrap_kbd_p1_f() {
+
+    asm volatile(
+        "push    {r1-r7, lr}\n"
+        "movs    r4, #0\n"
+        "bl      my_kbd_read_keys\n"
+        "b       _kbd_p1_f_cont\n"
+    );
+
+    return 0;
+}
+
+// no stack manipulation needed here, since we create the task directly
+void __attribute__((noinline)) mykbd_task() {
+    extern void kbd_p2_f_my();
+
+    while (physw_run) {
+        _SleepTask(physw_sleep_delay);
+
+        if (wrap_kbd_p1_f() == 1) {             // autorepeat ?
+            _kbd_p2_f();
+// TODO SX280
+//            kbd_p2_f_my();                      // replacement of _kbd_p2_f (in sub/<fwver>/boot.c)
+        }
+    }
+
+    _ExitTask();
+}
+
+// TODO
+#if 0
+// sx280 jogdial hw counter (19 bits) 0xd9854004
+// 0x7fff8 .. 0x7fffc .. 0 (start pos) .. 4
+// intermediate positions are also available, but they are ignored by the fw for a good reason
+
+int jogdial_stopped=0;
+
+extern long jog_position[2];
+extern long jog_hw_pos;
+
+int get_jogdial_counter() {
+    int p;
+    p = jog_hw_pos & 0x7fffc;
+    if (p > 0x3fffc) {
+        p |= 0xfff80000;
+    }
+    return p;
+}
+
+long get_jogdial_direction(void) {
+    static int new_jogdial=0, old_jogdial=0;
+    
+    old_jogdial=new_jogdial;
+    new_jogdial=get_jogdial_counter();
+    if (old_jogdial>new_jogdial) return JOGDIAL_LEFT; 
+    else if (old_jogdial<new_jogdial) return JOGDIAL_RIGHT;
+    else return 0;
+}
+
+int handle_jogdial() {
+    // return 0 to prevent fw jogdial handler
+    if (jogdial_stopped) {
+        // update jog position in RAM
+        jog_position[0] = jog_position[1] = get_jogdial_counter();
+        return 0;
+    }
+    return 1;
+}
+
+void jogdial_control(int c) {
+    jogdial_stopped = c;
+}
+#endif
+
+void my_kbd_read_keys() {
+    kbd_update_key_state();
+    kbd_update_physw_bits();
+}
+
+void kbd_fetch_data(long *dst)
+{
+    _GetKbdState(dst);
+    _kbd_read_keys_r2(dst);
+}
---
+++ platform/g7x/lib.c
@@ -0,0 +1,237 @@
+#include "platform.h"
+#include "lolevel.h"
+
+static char* frame_buffer[2];
+
+void vid_bitmap_refresh() {
+    extern int full_screen_refresh;
+    extern void _ScreenUnlock();
+    extern void _ScreenLock();
+    extern void _displaybusyonscreen();
+    extern void _undisplaybusyonscreen();
+
+    
+    // clears perfectly but blinks and is asynchronous
+    _displaybusyonscreen();
+    _undisplaybusyonscreen();
+    
+    // stuff commented on sx280
+    /*
+    // completely ineffective
+    extern void _refresh_bitmap_buf_from(int, int);
+    _refresh_bitmap_buf_from(0,0);
+    */
+
+    /*
+    // flips the active buffer but doesn't erase areas not occupied by the canon osd
+    _ScreenLock();
+    full_screen_refresh |= 3;
+    _ScreenUnlock();
+    */
+
+    /*
+    //ineffective, like screenlock/unlock, crashed movie rec once
+    extern void _RefreshPhysicalScreen();
+    int saved_abdcplus4c = *(int*)(0xabdc+0x4c);
+    *(int*)(0xabdc+0x4c) = 0;
+    _RefreshPhysicalScreen(0);
+    *(int*)(0xabdc+0x4c) = saved_abdcplus4c;
+    */
+
+    //DisplayPhysicalScreenCBR NG
+    //Window_EmergencyRefresh NG crash
+}
+
+void shutdown() {
+//***TODO***
+/*
+    volatile long *p = (void*)0xC022001C;
+
+    asm(
+        "MRS     R1, CPSR\n"
+        "AND     R0, R1, #0x80\n"
+        "ORR     R1, R1, #0x80\n"
+        "MSR     CPSR_cf, R1\n"
+        :::"r1","r0"
+    );
+
+    *p = 0x44;  // power off.
+*/
+    while(1);
+}
+
+#define LED_PR 0xd20b0994 // green LED on the back
+
+void debug_led(int state) {
+    volatile long *p = (void*)LED_PR;
+    *p = ((state) ? 0x4d0002 : 0x4c0003);
+}
+
+// Power Led = first entry in table (led 0)
+// AF Assist Lamp = second entry in table (led 1)
+void camera_set_led(int led, int state, int bright) {
+    static char led_table[2]={0,4};
+    _LEDDrive(led_table[led%sizeof(led_table)], state<=1 ? !state : state);
+}
+
+void shutdown_soft() {
+    _PostLogicalEventForNotPowerType(0x1005,0); // PressOffButton
+}
+
+int get_flash_params_count(void) {
+    return 0xde; // @0xfc0afeb8
+}
+
+void JogDial_CW(void) {
+    _PostLogicalEventToUI(0x872, 1);    //RotateJogDialRight
+}
+
+void JogDial_CCW(void) {
+    _PostLogicalEventToUI(0x873, 1);    //RotateJogDialLeft
+}
+
+extern  int     active_raw_buffer;
+extern  char*   raw_buffers[];
+
+char *hook_raw_image_addr()
+{
+    return (char *)0x4427bfa0; // CRAW BUF @fc4f1ff6, TODO unverified, probably multiple
+//    return raw_buffers[(active_raw_buffer&1)];
+}
+
+/*
+char *hook_alt_raw_image_addr()
+{
+    return raw_buffers[((active_raw_buffer&1)^1)];
+}
+*/
+
+extern char active_viewport_buffer;
+extern void* viewport_buffers[];
+
+void *vid_get_viewport_fb() {
+    return (void*)0x43334300; // "first" viewport adr, "VRAM Address  : %p", contains d6 uyvy
+// TODO sx280 values
+    // Return first viewport buffer - for case when vid_get_viewport_live_fb not defined
+    //return (void*)0x43115100; // uyvy buffers with signed(?) chroma components
+    // return (void*)0x4b25fc00; // uyvy buffers (more than 4), pixel format compatible with earlier DIGIC uyvy
+}
+void *vid_get_viewport_fb_d()    { return (void*)0x43334300; } // TODO
+
+void *vid_get_viewport_live_fb() {
+    return 0; //TODO
+}
+
+int vid_get_viewport_width() {
+    extern int _GetVRAMHPixelsSize();
+// TODO: this is the actual width, pixel format is uyvy (16bpp)
+    return _GetVRAMHPixelsSize();
+}
+
+long vid_get_viewport_height() {
+    extern int _GetVRAMVPixelsSize();
+// TODO return half height?
+    return _GetVRAMVPixelsSize();
+}
+
+// Y multiplier for cameras with 480 pixel high viewports (CHDK code assumes 240)
+int vid_get_viewport_yscale() {
+    return 2;
+}
+
+int vid_get_viewport_yoffset() {
+    // this seems to be always 0, buffer always begins with actual display data (widescreen or not)
+    return 0;
+}
+
+int vid_get_viewport_display_xoffset() {
+    return 0;
+}
+
+int vid_get_viewport_display_yoffset() {
+    return 0;
+}
+
+extern int active_bitmap_buffer;
+extern char* bitmap_buffer[];
+
+void *vid_get_bitmap_fb() {
+    return (void *)0x41441000; // from sub_fc0f7c21, alt 4153e200
+}
+
+// TODO
+// Functions for PTP Live View system
+int vid_get_viewport_display_xoffset_proper()   { return vid_get_viewport_display_xoffset() * 2; }
+int vid_get_viewport_display_yoffset_proper()   { return vid_get_viewport_display_yoffset() * 2; }
+int vid_get_viewport_width_proper()             { return vid_get_viewport_width(); }
+int vid_get_viewport_height_proper()            { return vid_get_viewport_height() * 2; }
+int vid_get_viewport_fullscreen_height()        { return 480; }
+int vid_get_palette_type()                      { return 3; }
+int vid_get_palette_size()                      { return 256 * 4; }
+
+void *vid_get_bitmap_active_buffer() {
+    return bitmap_buffer[active_bitmap_buffer&1];
+}
+
+// the opacity buffer defines opacity for the bitmap overlay's pixels
+// found near BmpDDev.c line 215 assert fc0f7b58
+volatile char *opacity_buffer[2] = {(char*)0x4163b400, (void*)0x416b9d00};
+
+void *vid_get_bitmap_active_palette() {
+    return (void*)0x8000; // just to return something valid, no palette needed on this cam
+}
+
+#if 0
+#ifdef CAM_SUPPORT_BITMAP_RES_CHANGE
+/*
+ * needed because bitmap buffer resolutions change when an external display is used
+ * an extra screen erase doesn't seem to be needed
+ */
+void update_screen_dimensions() {
+    // see sub_fc18618a in 102b and 102c for the values
+    extern int displaytype;
+    static int old_displaytype = -1;
+
+    if (old_displaytype == displaytype) {
+        return;
+    }
+    old_displaytype = displaytype;
+
+    switch(displaytype) {
+        case 0:
+        case 3:
+        case 4:
+        case 5:
+            // lcd
+            camera_screen.width = camera_screen.physical_width = camera_screen.buffer_width = 640;
+            camera_screen.height = camera_screen.buffer_height = 480;
+            camera_screen.size = camera_screen.buffer_size = 640*480;
+            break;
+        case 1:
+        case 2:
+        case 8:
+        case 9:
+            // tv-out
+            camera_screen.width = 720;
+            camera_screen.physical_width = camera_screen.buffer_width = 736;
+            camera_screen.height = camera_screen.buffer_height = 480;
+            camera_screen.size = 720*480;
+            camera_screen.buffer_size = 736*480;
+            break;
+        case 6:
+        case 7:
+            // hdmi
+            camera_screen.width = camera_screen.physical_width = camera_screen.buffer_width = 960;
+            camera_screen.height = camera_screen.buffer_height = 540;
+            camera_screen.size = camera_screen.buffer_size = 960*540;
+            break;
+    }
+}
+#endif
+#endif
+
+char *camera_jpeg_count_str()
+{
+    extern char jpeg_count_str[];
+	return jpeg_count_str;
+}
