0077e127352ad0e9195b88c291489d4c4d64d9a0
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2016-07-17 00:42:31 +0000

    ixus140_elph130 - enable CAM_NEED_SET_ZOOM_DELAY, to avoid crash using set_zoom with mf or aflock enabled - trunk 4663

--- branches/release-1_4/platform/ixus140_elph130/platform_camera.h
+++ branches/release-1_4/platform/ixus140_elph130/platform_camera.h
@@ -146,6 +146,7 @@
     #define CAM_USE_OPTICAL_MAX_ZOOM_STATUS 1       // Use ZOOM_OPTICAL_MAX to reset zoom_status when switching from digital to optical zoom
     #define CAM_USE_ALT_SET_ZOOM_POINT      1       // Define to use the alternate code in lens_set_zoom_point()
     #define CAM_USE_ALT_PT_MoveOpticalZoomAt 1      // Define to use the PT_MoveOpticalZoomAt() function in lens_set_zoom_point()
+    #define CAM_NEED_SET_ZOOM_DELAY          150    // Define to add a delay after setting the zoom position
 
     #define CAM_IS_VID_REC_WORKS                1   // is_video_recording() function works
 
