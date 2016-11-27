295dfc4c6a4cb2147df33a8cb9fd357422676373
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2016-11-27 02:08:26 +0000

    EOS M3 - use common compat_check + copy_core code, remove sx2xx variants from bin_comp_fix.h

--- trunk/platform/m3/sub/101a/bin_comp_fix.h
+++ trunk/platform/m3/sub/101a/bin_comp_fix.h
@@ -5,7 +5,4 @@ ver_sig_t ver_sigs[]={
 };
 pid_sig_t pid_sigs[]={
     {(short *)0xfdf20040, 12953}, // eos m3
-    {(short *)0xfdf60040, 12895}, // sx280hs
-    {(short *)0xfdf60040, 12896}, // sx270hs
-    {(short *)0xfdf60040, 12899}, // sx275hs
 };
