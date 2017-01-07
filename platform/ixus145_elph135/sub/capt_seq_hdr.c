8f2c3840854764b82d351f8ce6c65c16f0fb0a26
nafraf <nafraf@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2017-01-07 00:06:26 +0000

    ixus145:elph135: make dark frame control work correctly (without lag). Test and comments: https://chdk.setepontos.com/index.php?topic=12144.msg130889#msg130889

--- platform/ixus145_elph135/sub/capt_seq_hdr.c
+++ platform/ixus145_elph135/sub/capt_seq_hdr.c
@@ -3,7 +3,7 @@
 #include "core.h"
 
 #define NR_AUTO (0)                 // have to explictly reset value back to 0 to enable auto
-static long *nrflag = (long*)(0xA0DC+0xC); // ixus145 100c Found @ 0xffa1efb0 (0xA0DC) & 0xffa1efb4 (0xC) 
+static long *nrflag = (long*)(0xa0e0); // found by trial and error near 0xa0dc referenced near 0xffa1efb4 (0xa0dc + 0x4)
 //#define PAUSE_FOR_FILE_COUNTER 200  // Enable delay in capt_seq_hook_raw_here to ensure file counter is updated
 
 #include "../../../generic/capt_seq.c"
