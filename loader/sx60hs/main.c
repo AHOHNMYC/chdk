5a327cb549a3976aea05aa8507f332b4c2c4a278
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2017-04-09 23:01:00 +0000

    sx60hs update from 62ndidiot in https://chdk.setepontos.com/index.php?topic=650.msg132207#msg132207 * Standardize the loader code * Changes the direction of the jogdial * adds CAM_QUALITY_OVERRIDE

--- loader/sx60hs/main.c
+++ loader/sx60hs/main.c
@@ -1,38 +1,25 @@
-//#include "check_compat.c"
+#include "../generic/check_compat.c"
 
 extern long *blob_chdk_core;
 extern long blob_chdk_core_size;
 
 void __attribute__((noreturn)) my_restart()
 {
-//    check_compat();
+    check_compat();
 
-    // light up green LED
-//    *(int*)0xd20b0884 = 0x4d0002;
-    // LED done
     long *dst = (long*)MEMISOSTART;
     const long *src = blob_chdk_core;
     long length = (blob_chdk_core_size + 3) >> 2;
 
-    if (src < dst && dst < src + length) {
-        /* Have to copy backwards */
-        src += length;
-        dst += length;
-        while (length--) {
-            *--dst = *--src;
-        }
-    } else {
-        while (length--) {
-            *dst++ = *src++;
-        }
-    }
+    core_copy(src, dst, length);
+
+
      *(volatile unsigned *)(0x4ffc)=0x12345678;
 
-//    *(int*)0xd20b0884 = 0x4d0002;
     asm volatile ( // fc095c80 102b/102c
     "mov     r1, %1\n"
     "mov     r0, %0\n"
-    "ldr     r2, =0xfc1361cf\n" // address is for 100f
+    "ldr     r2, =0xfc1361cf\n" // address is for 100b, 100c, 100f
     "blx     r2\n"              // caching related routine called at fw startup
     "mov     r0, %0\n"
     "add     r0, r0, #1\n"
