d2b2d2b461b4f4f40ce84e846e07e9864ba5aad9
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2017-06-14 04:22:28 +0000

    ixus300_sd4000 - trunk 4828 * add play button to key mask * remove KEY_PRINT alias * remove UP+LEFT alt key option * remove invalid DNG over USB code

--- platform/ixus300_sd4000/platform_kbd.h
+++ platform/ixus300_sd4000/platform_kbd.h
@@ -1,7 +1,7 @@
 #ifndef PLATFORM_KBD_H
 #define PLATFORM_KBD_H
 // plaform specific keyboard defines, see core/kbd_common.c for documentation
-#define KEYS_MASK0 (0x0000090F)     // physw_status[0]
+#define KEYS_MASK0 (0x0000490F)     // physw_status[0]
 // override key and feather bits to avoid feather osd messing up chdk display in ALT mode
 #define KEYS_MASK1 (0x00000FF0)     // physw_status[1], override 0xF0 (keys) + 0xF00 (feather)
 #define KEYS_MASK2 (0x00000000)     // physw_status[2]
