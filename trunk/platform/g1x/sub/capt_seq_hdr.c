6a10e4a3a42f1e7af417f6702f05bb8ff3bd8ee3
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2017-07-08 07:06:31 +0000

    Fix to make rawopint work better on G1X. Stops the camera from shooting at 1/2 ISO and cooking the JPG/CR2 data in some circumstances.

--- trunk/platform/g1x/sub/capt_seq_hdr.c
+++ trunk/platform/g1x/sub/capt_seq_hdr.c
@@ -6,3 +6,11 @@
 #define NR_AUTO (0)                 // have to explictly reset value back to 0 to enable auto
 
 #include "../../../generic/capt_seq.c"
+
+void __attribute__((naked,noinline)) block_sv_cooking()
+{
+    // On G1X (possibly others), when Tv >= 1s and ISO >= 400, then the camera shoots at 1/2 ISO and cooks the JPG/CR2 image data to compensate
+    // Setting this property blocks the firmware from doing this.
+    static short v = 1;
+    set_property_case(77, &v, sizeof(v));
+}
