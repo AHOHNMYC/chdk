8ccae440663703b6a52525c2e8aef156d836dd40
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2017-07-09 01:42:35 +0000

    remove unused CAM_DETECT_SCREEN_ERASE

--- platform/sx500is/platform_camera.h
+++ platform/sx500is/platform_camera.h
@@ -90,8 +90,6 @@
                                                           // Used to enabled bracketing in custom timer, required on many recent cameras
                                                           // see http://chdk.setepontos.com/index.php/topic,3994.405.html
 
-    #define CAM_DETECT_SCREEN_ERASE                     1 // Turn on guard pixels to detect screen erase and redraw CHDK buttons and menus
-
     #undef  CAM_USE_ZOOM_FOR_MF
     #define CAM_NEED_SET_ZOOM_DELAY                   300 // Define to add a delay after setting the zoom position
     #define CAM_USE_OPTICAL_MAX_ZOOM_STATUS             1 // Use ZOOM_OPTICAL_MAX to reset zoom_status when switching from digital to optical zoom
