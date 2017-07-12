ae70bf3c9e9f2fa39d73388d5128dde79c714069
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2017-07-12 04:15:55 +0000

    m3 - turn off CAM_SD_OVER_IN_*, update notes based on discussion in https://chdk.setepontos.com/index.php?topic=12542.400

--- platform/m3/platform_camera.h
+++ platform/m3/platform_camera.h
@@ -137,8 +137,10 @@
     #undef  REMOTE_SYNC_STATUS_LED
     // #define REMOTE_SYNC_STATUS_LED              0xC022C30C  // TODO specifies an LED that turns on while camera waits for USB remote to sync
  
-    #define CAM_SD_OVER_IN_AF  1
-    #define CAM_SD_OVER_IN_MF  1
+    // subject distance override currently not available
+    #undef CAM_SD_OVER_IN_AFL
+    #undef CAM_SD_OVER_IN_AF
+    #undef CAM_SD_OVER_IN_MF
 
 // ------- MY -----------
 //    #define CAM_TOUCHSCREEN_UI	1
