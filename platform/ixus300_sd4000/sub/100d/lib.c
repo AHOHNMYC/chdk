8c7363a40f02575fc0aa0c4b7f244fccd4ee105d
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2017-07-12 03:15:45 +0000

    ixus300_sd4000 - move theoretically sub-specific functions back to sub lib.c

--- platform/ixus300_sd4000/sub/100d/lib.c
+++ platform/ixus300_sd4000/sub/100d/lib.c
@@ -1,3 +1,8 @@
 #include "platform.h"
+char *hook_raw_image_addr() {
+    return (char*) (*(int*)(0x2CCC + 0xC)? 0x46000000 : 0x4132C0A0);    // looks like every RAW is valid
+}
 
-// unused - all functions are in ixus300_sd4000/lib.c as they are not firmware specific
+char *camera_jpeg_count_str() {
+    return (char*)0xA15B8;
+}
