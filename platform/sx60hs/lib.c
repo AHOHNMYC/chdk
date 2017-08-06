dc79db7d9c47fdfb82295eb3c4a78b1336b983f6
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2017-08-06 04:27:16 +0000

    sx60hs - use new display refresh method (https://chdk.setepontos.com/index.php?topic=12788.msg133958#msg133958), enabled hotshoe override, from 62ndidiot in https://chdk.setepontos.com/index.php?topic=650.msg134161#msg134161

--- platform/sx60hs/lib.c
+++ platform/sx60hs/lib.c
@@ -12,47 +12,11 @@ extern char* raw_buffers_canon_raw[];
 extern char* raw_buffers_jpeg[];
 
 void vid_bitmap_refresh() {
-    extern int full_screen_refresh;
-    extern void _ScreenUnlock();
-    extern void _ScreenLock();
-    extern void _displaybusyonscreen();
-    extern void _undisplaybusyonscreen();
 
-    
-    // clears perfectly but blinks and is asynchronous
-    _displaybusyonscreen();
-    _undisplaybusyonscreen();
-/*    _LogCameraEvent(0x20,"lc  avb:%d gdt:%d ",
-                   active_bitmap_buffer,
-                   displaytype 
-                   );
-*/     
-    // stuff commented on sx280
-    /*
-    // completely ineffective
-    extern void _refresh_bitmap_buf_from(int, int);
-    _refresh_bitmap_buf_from(0,0);
-    */
-
-    /*
-    // flips the active buffer but doesn't erase areas not occupied by the canon osd
-    _ScreenLock();
-    full_screen_refresh |= 3;
-    _ScreenUnlock();
-    */
-
-    /*
-    //ineffective, like screenlock/unlock, crashed movie rec once
-    extern void _RefreshPhysicalScreen();
-    int saved_abdcplus4c = *(int*)(0xabdc+0x4c);
-    *(int*)(0xabdc+0x4c) = 0;
-    _RefreshPhysicalScreen(0);
-    *(int*)(0xabdc+0x4c) = saved_abdcplus4c;
-    */
-
-    //DisplayPhysicalScreenCBR NG
-    //Window_EmergencyRefresh NG crash
-
+    extern int active_bitmap_buffer;
+    extern void _transfer_src_overlay(int);
+    _transfer_src_overlay(0);
+    _transfer_src_overlay(1);
 }
 
 void shutdown() {
