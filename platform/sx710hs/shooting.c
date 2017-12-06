d94ab88ca7c8fde50509441ddef2748afcea6e32
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2017-12-06 06:26:41 +0000

    sx710 - change propset to 9, most values seem to match. Implement shooting.c tables

--- platform/sx710hs/shooting.c
+++ platform/sx710hs/shooting.c
@@ -9,42 +9,20 @@
 
 #define PARAM_FILE_COUNTER      0x1        // see comments in ixus140 port
 
-// TODO
 // aperture values recorded at wide angle
 const ApertureSize aperture_sizes_table[] = {
-    { 9,  171, "1.8" },
-    { 10, 192, "2.0" },
-    { 11, 224, "2.2" },
-    { 12, 256, "2.5" },
-    { 13, 288, "2.8" },
-    { 14, 320, "3.2" },
-    { 15, 352, "3.5" },
-    { 16, 369, "4.0" },
-    { 17, 401, "4.5" },
-    { 18, 433, "5.0" },
-    { 19, 465, "5.6" },
-    { 20, 497, "6.3" },
-    { 21, 529, "7.1" },
-    { 22, 561, "8.0" },
-    { 23, 593, "9.0" },
-    { 24, 625, "10.0" },
-    { 25, 657, "11.0" },
+    { 9,  330, "3.2" },
+    { 10, 352, "3.5" },
+    { 11, 384, "4.0" },
+    { 12, 416, "4.5" },
+    { 13, 448, "5.0" },
+    { 14, 480, "5.6" },
+    { 15, 512, "6.3" },
+    { 16, 544, "7.1" },
+    { 17, 576, "8.0" },
 };
 
-// note camera supports up to 250s shutter
 const ShutterSpeed shutter_speeds_table[] = {
-    { -24, -768, "250", 250000000 },
-    { -23, -736, "200", 200000000 },
-    { -22, -704, "160", 160000000 },
-    { -21, -672, "125", 125000000 },
-    { -20, -640, "100", 100000000 },
-    { -19, -608, "80",   80000000 },
-    { -18, -576, "60",   60000000 },
-    { -17, -544, "50",   50000000 },
-    { -16, -512, "40",   40000000 },
-    { -15, -480, "30",   30000000 },
-    { -14, -448, "25",   25000000 },
-    { -13, -416, "20",   20000000 },
     { -12, -384, "15",   15000000 },
     { -11, -352, "13",   13000000 },
     { -10, -320, "10",   10000000 },
@@ -91,40 +69,28 @@ const ShutterSpeed shutter_speeds_table[] = {
     {  31,  992, "1/1250",    800 },
     {  32, 1024, "1/1600",    625 },
     {  33, 1056, "1/2000",    500 },
+    {  34, 1088, "1/2500",    400 },
+    {  35, 1120, "1/3200",    313 },
 };
 
 const ISOTable iso_table[] = {
 	{  0,    0, " Auto", -1},
-	{  1,  125,   "125", -1},
-	{  2,  160,   "160", -1},
+	{  1,   80,    "80", -1},
+	{  2,  100,   "100", -1},
 	{  3,  200,   "200", -1},
-	{  4,  250,   "250", -1},
-	{  5,  320,   "320", -1},
-	{  6,  400,   "400", -1},
-	{  7,  500,   "500", -1},
-	{  8,  640,   "640", -1},
-	{  9,  800,   "800", -1},
-	{ 10, 1000,  "1000", -1},
-	{ 11, 1250,  "1250", -1},
-	{ 12, 1600,  "1600", -1},
-	{ 13, 2000,  "2000", -1},
-	{ 14, 2500,  "2500", -1},
-	{ 15, 3200,  "3200", -1},
-	{ 16, 4000,  "4000", -1},
-	{ 17, 5000,  "5000", -1},
-	{ 18, 6400,  "6400", -1},
-	{ 19, 8000,  "8000", -1},
-	{ 20,10000, "10000", -1},
-	{ 21,12800, "12800", -1},
+	{  4,  400,   "400", -1},
+	{  5,  800,   "800", -1},
+	{  6, 1600,  "1600", -1},
+	{  7, 3200,  "3200", -1},
 };
 
-// TODO from g7x with unknown removed
 const CapturemodeMap modemap[] = {
 { MODE_AUTO                 ,32768 },
 { MODE_M                    ,32770 },
 { MODE_AV                   ,32771 },
 { MODE_TV                   ,32772 },
 { MODE_P                    ,32773 },
+{ MODE_LIVE                 ,33345 }, // Creative shot, various sub modes all use same number
 { MODE_FISHEYE              ,8759 },
 { MODE_MINIATURE            ,8760 },
 { MODE_TOY_CAMERA           ,8763 },
@@ -133,16 +99,20 @@ const CapturemodeMap modemap[] = {
 { MODE_SUPER_VIVID          ,8754 },
 { MODE_POSTER_EFFECT        ,8755 },
 { MODE_PORTRAIT             ,16406 },
-{ MODE_FACE_SELF_TIMER      ,16948 },
 { MODE_SMART_SHUTTER        ,16949 }, // smile detection
 { MODE_WINK_SELF_TIMER      ,16950 },
+{ MODE_FACE_SELF_TIMER      ,16948 },
 { MODE_NIGHT_SCENE          ,16953 }, // handheld night scene
+{ MODE_LOWLIGHT             ,16429 },
 { MODE_SNOW                 ,16414 },
 { MODE_FIREWORK             ,16416 },
 { MODE_CREATIVE_EFFECT      ,33299 }, // Creative shot, various sub modes all use same number
 { MODE_HYBRID_AUTO          ,33298 }, // like movie digest
-{ MODE_VIDEO_STD             ,2643 }, // standard video on dial, regardless of res or FPS
+{ MODE_SPORTS               ,33288 },
+//{                         ,33297 }, // ? seems to be related to face ID
+{ MODE_VIDEO_STD             ,2643 }, // standard video on dial
 { MODE_VIDEO_IFRAME_MOVIE    ,2650 },
+//{ MODE_VIDEO_SHORT_CLIP    ,2652 }, // TODO
 };
 
 #include "../generic/shooting.c"
