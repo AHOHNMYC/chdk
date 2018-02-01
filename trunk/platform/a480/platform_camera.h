41a001959e20e0793890c0232a95928a642bff82
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2018-02-01 21:35:33 +0000

    a480, a490 - enable jpeg quality override, from waterwingz in https://chdk.setepontos.com/index.php?topic=650.msg136059#msg136059

--- trunk/platform/a480/platform_camera.h
+++ trunk/platform/a480/platform_camera.h
@@ -87,3 +87,5 @@
     #undef  CAM_DEFAULT_MENU_CURSOR_FG
     #define CAM_DEFAULT_MENU_CURSOR_BG  IDX_COLOR_RED      // Override menu cursor colors
     #define CAM_DEFAULT_MENU_CURSOR_FG  IDX_COLOR_WHITE    // Override menu cursor colors
+
+    #define CAM_QUALITY_OVERRIDE                1   // https://chdk.setepontos.com/index.php?topic=13342
