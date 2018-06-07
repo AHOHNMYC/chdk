b35942466ed787eecbb3aee7e724ca0f19611ad4
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2018-06-07 05:15:07 +0000

    ixus90_sd790 - fix overrides not being applied for quick press, from koshy in https://chdk.setepontos.com/index.php?topic=13447.msg137320#msg137320

--- platform/ixus90_sd790/sub/100d/capt_seq.c
+++ platform/ixus90_sd790/sub/100d/capt_seq.c
@@ -220,10 +220,19 @@ void __attribute__((naked,noinline)) capt_seq_task() //#fs
 
 "loc_FF85E374:\n" //                               ; CODE XREF: task_CaptSeqTask+4Cj
         "BL      sub_FF93C168\n"
+        // only do quick press hack if overrides are active
+        "BL      captseq_hack_override_active\n"      // returns 1 if tv or sv override in effect
+        "STR     R0,[SP,#-4]!\n" // push return value
         "BL      shooting_expo_param_override\n" // +
         "BL      sub_FF85C438\n"
-        "LDR     R0, [R6,#0x24]\n"
-        "CMP     R0, #0\n"
+        "LDR     R0,[SP],#4\n" // pop override hack
+        "CMP     R0, #1\n"     // +
+        "MOVEQ   R0, #0\n"     // +
+        "STREQ   R0, [R6,#0x24]\n" // fixes overrides behavior at short shutter press
+        "LDRNE   R0, [R6,#0x24]\n" // modified NE
+        "CMPNE   R0, #0\n"         // modified NE
+//      "LDR     R0, [R6,#0x24]\n"  // above patch makes these lines redundant
+//      "CMP     R0, #0\n"
         "BEQ     loc_FF85E51C\n"
         "BL      sub_FF85DC58\n"
         "MOV     R4, R0\n"
