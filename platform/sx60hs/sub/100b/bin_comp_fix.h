80555a14deb885458734a7b38bf9357600ea63cf
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2018-07-06 23:41:15 +0000

    sx60hs - remove no longer needed bin_comp_fix.h

--- platform/sx60hs/sub/100b/bin_comp_fix.h
+++
@@ -1,8 +0,0 @@
-#define DEBUG_DELAY 10000000
-
-ver_sig_t ver_sigs[]={
-    {(const char *)0xfc15c1f9, "GM1.00B"},
-};
-pid_sig_t pid_sigs[]={
-    {(short *)0xfdf20040, 12954}, // sx60hs near PRODUCT_ID 0x329a Ver.013.003
-};
