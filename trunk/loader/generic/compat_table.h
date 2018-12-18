35ddad2d77031f048be3561b95bf5cc92573d1dc
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2018-12-18 07:00:32 +0000

    ixus285_elph360hs - 100b port from blackhole in https://chdk.setepontos.com/index.php?topic=13604.msg138738#msg138738 autobuild disabled

--- trunk/loader/generic/compat_table.h
+++ trunk/loader/generic/compat_table.h
@@ -96,6 +96,7 @@
 { 12957,4,0xd20b0994,0xfff20040 }, // g7x
 { 12959,3,0xc022d1fc,0xfffe0040 }, // sx530hs
 #elif (NEED_ENCODED_DISKBOOT == 16)
+{ 12989,3,0xc022d1fc,0xfffe0270 }, // ixus285_elph360hs
 { 12991,3,0xc022d1fc,0xfffe0270 }, // sx420is
 { 12993,3,0xc022d1fc,0xffff0270 }, // ixus175_elph180
 #elif (NEED_ENCODED_DISKBOOT == 17)
