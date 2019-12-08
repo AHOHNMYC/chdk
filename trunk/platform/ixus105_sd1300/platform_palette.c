c18f6c576eef89675f44312fee74d46a529bb2c2
srsa <srsa@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2019-12-08 13:14:58 +0000

    ixus105_sd1300: improve rec mode palette, fix viewport dimensions (in most cases) by koshy in https://chdk.setepontos.com/index.php?topic=5720.msg142059#msg142059

--- trunk/platform/ixus105_sd1300/platform_palette.c
+++ trunk/platform/ixus105_sd1300/platform_palette.c
@@ -34,23 +34,23 @@ unsigned char rec_colors[] =
 {
         COLOR_TRANSPARENT,          // Placeholder for script colors
         COLOR_BLACK,                // Placeholder for script colors
-        0x11,                       // White
-        0x22,                       // Red
-        0x2E,                       // Dark Red
-        0x21,                       // Light Red
-        0x55,                       // Green
-        0x25,                       // Dark Green
-        0x51,                       // Light Green
-        0xDF,                       // Blue
-        0xDF,                       // Dark Blue
-        0xDD,                       // Light Blue / Cyan
-        0x3F,                       // Grey
-        0x44,                       // Dark Grey
-        0x1F,                       // Light Grey
-        0xEE,                       // Yellow
-        0x6F,                       // Dark Yellow
-        0x66,                       // Light Yellow
-        0x44,                       // Transparent Dark Grey
-        0x66,                       // Magenta
+        0x01,                       // White
+        0x66,                       // Red
+        0x66,                       // Dark Red --- there is none this is the same red
+        0x1E,                       // Light Red --- there is none this is a dark orange
+        0x69,                       // Green
+        0x6B,                       // Dark Green
+        0x5F,                       // Light Green
+        0x61,                       // Blue
+        0x67,                       // Dark Blue
+        0x68,                       // Light Blue / Cyan
+        0x12,                       // Grey
+        0x16,                       // Dark Grey
+        0x0E,                       // Light Grey
+        0x60,                       // Yellow
+        0x4C,                       // Dark Yellow
+        0x6C,                       // Light Yellow
+        0xF1,                       // Transparent Dark Grey
+        0x41,                       // Magenta --- there is none this is an orange
 };
 
