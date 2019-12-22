743ed20f7d4e4c89081614c40bf1511d94c33ee2
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2019-12-22 08:38:44 +0000

    sx730hs - add untested port for firmware 100c with 100b as copy, autobuild disabled. Switch 100d to ALPHA

--- trunk/loader/sx730hs/main.c
+++ trunk/loader/sx730hs/main.c
@@ -40,9 +40,9 @@ void __attribute__((noreturn)) my_restart()
     asm volatile ( 
     "mov     r1, %1\n"
     "mov     r0, %0\n"
-    "ldr     r2, =0xfc0db23f\n" // function called in startup after ROM->RAM code copy, sx730 100d
-    "blx     r2\n"
-
+//    "ldr     r2, =0xfc0db23f\n" // function called in startup after ROM->RAM code copy, sx730 100d
+//    "blx     r2\n"
+    "bl cache_func_my\n" // different address on 100c, copied inline
     // start execution at MEMISOSTART in thumb mode
     "mov     r0, %0\n"
     "add     r0, r0, #1\n"
@@ -51,4 +51,37 @@ void __attribute__((noreturn)) my_restart()
     );
     while(1);
 }
-
+// 100d -f=chdk -s=0xfc0db23f -eret=2
+void __attribute__((naked,noinline)) cache_func_my() {
+    asm volatile (
+"    cmp.w   r1, #0x4000\n"
+"    bhs     loc_fc0db260\n"
+"    dsb     sy\n"
+"    add     r1, r0\n"
+"    bic     r0, r0, #0x1f\n"
+"loc_fc0db24e:\n"
+"    mcr     p15, #0, r0, c7, c10, #1\n"
+"    add.w   r0, r0, #0x20\n"
+"    cmp     r0, r1\n"
+"    blo     loc_fc0db24e\n"
+"    dsb     sy\n"
+"    bx      lr\n"
+"loc_fc0db260:\n"
+"    dsb     sy\n"
+"    mov.w   r1, #0\n"
+"loc_fc0db268:\n"
+"    mov.w   r0, #0\n"
+"loc_fc0db26c:\n"
+"    orr.w   r2, r1, r0\n"
+"    mcr     p15, #0, r2, c7, c10, #2\n"
+"    add.w   r0, r0, #0x20\n"
+"    cmp.w   r0, #0x1000\n"
+"    bne     loc_fc0db26c\n"
+"    add.w   r1, r1, #0x40000000\n"
+"    cmp     r1, #0\n"
+"    bne     loc_fc0db268\n"
+"    dsb     sy\n"
+"    bx      lr\n"
+".ltorg\n"
+    );
+}
