833e61f74fd15ec1c82ba513239c8f88b3677f53
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2020-01-10 02:05:41 +0000

    sx610hs - add filewrite code. Untested and disabled pending testing. Test build in https://chdk.setepontos.com/index.php?topic=13355.msg142240#msg142240

---
+++ trunk/platform/sx610hs/sub/filewrite_hdr.c
@@ -0,0 +1,72 @@
+#include "lolevel.h"
+#include "platform.h"
+
+#ifdef CAM_HAS_FILEWRITETASK_HOOK
+// debug
+#define FILEWRITE_DEBUG_LOG 1
+extern void _LogCameraEvent(int id,const char *fmt,...);
+
+typedef struct {
+    unsigned int address;
+    unsigned int length;
+} cam_ptp_data_chunk; //camera specific structure
+
+#define MAX_CHUNKS_FOR_JPEG 7 // filewritetask is prepared for this many chunks
+
+
+// comments below from elph180, firmware code appears very similar, offsets verified
+/*
+ * fwt_data_struct: defined here as it's camera dependent
+ * unneeded members are designated with unkn
+ * file_offset, full_size, seek_flag only needs to be defined for cameras with CAM_FILEWRITETASK_SEEK/CAM_FILEWRITETASK_MULTIPASS
+ * pdc is always required
+ * name is not currently used
+ */
+typedef struct
+{
+    int unkn1; // message number
+    int file_offset;
+    int maybe_full_size; // maybe, on g7x wasn't always full size
+    int unkn2, unkn3;
+    int unkn4;
+    cam_ptp_data_chunk pdc[MAX_CHUNKS_FOR_JPEG];
+    int maybe_seek_flag;      // 0x2083 jpeg, 0x100 create dir, 0x200 CacheFlush
+    int unkn5, unkn6;
+    char name[32];      // offset from start 0x5c, from Open case
+} fwt_data_struct;
+/*
+observed message sequences
+JPEG
+12 maybe_full_size = 0x004437ac (=actual size), seek_flag = 0x2083
+ 0 chunk 0-0x004437ac
+ 1 chunk 0-0
+ 7 close
+
+new directory
+11 seek_flag = 0x100
+before m 12 for shot, after raw hook in capt_seq
+
+after shot
+10 A/ seek_flag 0x200
+*/
+
+#include "../../../generic/filewrite.c"
+
+#ifdef FILEWRITE_DEBUG_LOG
+void log_fwt_msg(fwt_data_struct *fwd)
+{
+    int m=fwd->unkn1;
+    _LogCameraEvent(0x60,"fw m:%d o:0x%08x fs:0x%08x sf:0x%x",m,fwd->file_offset,fwd->maybe_full_size,fwd->maybe_seek_flag);
+    _LogCameraEvent(0x60,"fw %s",fwd->name);
+    if(m >= 0 && m <=6) {
+        _LogCameraEvent(0x60,"fw chunk adr:0x%08x l:0x%08x",fwd->pdc[m].address,fwd->pdc[m].length);
+    }
+    _LogCameraEvent(0x60,"fw u %08x %08x %08x %08x %08x %08x",fwd->unkn2,fwd->unkn3,fwd->unkn4,fwd->unkn5,fwd->unkn6);
+}
+
+void log_fwt_start(void)
+{
+    _LogCameraEvent(0x60,"fw start");
+}
+#endif
+
