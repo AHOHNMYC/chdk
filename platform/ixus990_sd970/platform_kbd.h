8ffb7f4cda4bb8998c869494778f2fdad9b8280c
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2020-02-11 06:48:09 +0000

    ixus990_sd970 - update key mask based on https://chdk.setepontos.com/index.php?topic=13966.msg142656#msg142656

--- platform/ixus990_sd970/platform_kbd.h
+++ platform/ixus990_sd970/platform_kbd.h
@@ -2,9 +2,9 @@
 #define PLATFORM_KBD_H
 // plaform specific keyboard defines, see core/kbd_common.c for documentation
 
-#define KEYS_MASK0 (0x0000000D) // for sd970
+#define KEYS_MASK0 (0x0000000C) // for sd970
 #define KEYS_MASK1 (0x00000000) // for sd970
-#define KEYS_MASK2 (0x00000FFF) // for sd970
+#define KEYS_MASK2 (0x00000BFF) // for sd970
 
 #define SD_READONLY_FLAG    0x00020000 // sd970: Found @0xffaf1330, levent 0x90a
 #define SD_READONLY_IDX     2
