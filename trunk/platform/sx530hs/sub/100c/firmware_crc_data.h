0174cc00abb31ece695f893e234caa2d9e826b97
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2020-07-27 21:01:23 +0000

    sx530hs - add firmware 110a as a copy of 100c, tested by adnan_ in https://chdk.setepontos.com/index.php?topic=14077.msg143872#msg143872

--- trunk/platform/sx530hs/sub/100c/firmware_crc_data.h
+++ trunk/platform/sx530hs/sub/100c/firmware_crc_data.h
@@ -5,13 +5,19 @@ const firmware_crc_block_t firmware_100c_crc32[]={
     { (const char *)0xff8b1358,    0x49854, 0x7dae91df }, // RAM data, RAM code
 };
 
+const firmware_crc_block_t firmware_110a_crc32[]={
+    { (const char *)0xff010000,   0x62af10, 0x9467859c }, // ROMCODE
+    { (const char *)0xff8b1358,    0x49854, 0x7dae91df }, // RAM data, RAM code
+};
+
 firmware_crc_sub_t firmware_crc_list[]={
     { "GM1.00C", firmware_100c_crc32 },
+    { "GM1.10A", firmware_110a_crc32 },
 };
 
 const firmware_crc_desc_t firmware_crc_desc={
     (const char *)0xff0ae9f5, // firmware_ver_ptr
     firmware_crc_list,
-    1,  // sub_count
+    2,  // sub_count
     2,  // block_count
 };
