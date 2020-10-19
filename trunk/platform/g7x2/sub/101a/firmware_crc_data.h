749e59327f510b9d354e1526fb2edfd97474af2a
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2020-10-19 00:15:22 +0000

    update firmware CRCs to include g7x2 101b

--- trunk/platform/g7x2/sub/101a/firmware_crc_data.h
+++ trunk/platform/g7x2/sub/101a/firmware_crc_data.h
@@ -6,13 +6,20 @@ const firmware_crc_block_t firmware_101a_crc32[]={
     { (const char *)0xe0fd3770,    0x519bc, 0x3464921f }, // RAM data, RAM code, TCM code
 };
 
+const firmware_crc_block_t firmware_101b_crc32[]={
+    { (const char *)0xe0020000,   0x5b81d4, 0x7c4c3d01 }, // ROMCODE
+    { (const char *)0xe0e79d0c,    0xe0e18, 0xf81ac2c9 }, // zico_0, zico_1, zico_2
+    { (const char *)0xe0fd3770,    0x519bc, 0x3464921f }, // RAM data, RAM code, TCM code
+};
+
 firmware_crc_sub_t firmware_crc_list[]={
     { "GM1.01A", firmware_101a_crc32 },
+    { "GM1.01B", firmware_101b_crc32 },
 };
 
 const firmware_crc_desc_t firmware_crc_desc={
     (const char *)0xe0503c4d, // firmware_ver_ptr
     firmware_crc_list,
-    1,  // sub_count
+    2,  // sub_count
     3,  // block_count
 };
