15f43f9c5ad98fd918b47144773387bd2ae7e2f5
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2020-11-27 01:43:24 +0000

    Update firmware crc data for IXUS 220, IXUS 1000 and SX 500.

--- platform/ixus220_elph300hs/sub/101c/firmware_crc_data.h
+++ platform/ixus220_elph300hs/sub/101c/firmware_crc_data.h
@@ -5,6 +5,11 @@ const firmware_crc_block_t firmware_101c_crc32[]={
     { (const char *)0xffc8ab50,     0xeb9c, 0x603110a7 }, // RAM data
 };
 
+const firmware_crc_block_t firmware_101d_crc32[]={
+    { (const char *)0xff810000,   0x390112, 0xea387165 }, // ROMCODE
+    { (const char *)0xffc8ab50,     0xeb9c, 0x603110a7 }, // RAM data
+};
+
 const firmware_crc_block_t firmware_101e_crc32[]={
     { (const char *)0xff810000,   0x390112, 0x1726021c }, // ROMCODE
     { (const char *)0xffc8ab50,     0xeb9c, 0x603110a7 }, // RAM data
@@ -12,12 +17,13 @@ const firmware_crc_block_t firmware_101e_crc32[]={
 
 firmware_crc_sub_t firmware_crc_list[]={
     { "GM1.01C", firmware_101c_crc32 },
+    { "GM1.01D", firmware_101d_crc32 },
     { "GM1.01E", firmware_101e_crc32 },
 };
 
 const firmware_crc_desc_t firmware_crc_desc={
     (const char *)0xff894efd, // firmware_ver_ptr
     firmware_crc_list,
-    2,  // sub_count
+    3,  // sub_count
     2,  // block_count
 };
--- platform/sx500is/sub/100c/firmware_crc_data.h
+++ platform/sx500is/sub/100c/firmware_crc_data.h
@@ -10,14 +10,20 @@ const firmware_crc_block_t firmware_100d_crc32[]={
     { (const char *)0xff4d317c,    0x1d834, 0xdbc91b04 }, // RAM data, RAM code
 };
 
+const firmware_crc_block_t firmware_100e_crc32[]={
+    { (const char *)0xff000000,   0x39d5b8, 0x6846ce0b }, // ROMCODE
+    { (const char *)0xff4d317c,    0x1d834, 0xdbc91b04 }, // RAM data, RAM code
+};
+
 firmware_crc_sub_t firmware_crc_list[]={
     { "GM1.00C", firmware_100c_crc32 },
     { "GM1.00D", firmware_100d_crc32 },
+    { "GM1.00E", firmware_100e_crc32 },
 };
 
 const firmware_crc_desc_t firmware_crc_desc={
     (const char *)0xff087351, // firmware_ver_ptr
     firmware_crc_list,
-    2,  // sub_count
+    3,  // sub_count
     2,  // block_count
 };
