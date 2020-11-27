f5bed5cf108dadc3fe6e180bec588b1795d6a0d0
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2020-11-27 23:49:11 +0000

    update firmware CRCs with some additional dumps

--- platform/g5x/sub/101d/firmware_crc_data.h
+++ platform/g5x/sub/101d/firmware_crc_data.h
@@ -14,14 +14,22 @@ const firmware_crc_block_t firmware_101e_crc32[]={
     { (const char *)0xfceacb1c,    0x57235, 0x8816c760 }, // RAM data, RAM code, TCM code
 };
 
+const firmware_crc_block_t firmware_110a_crc32[]={
+    { (const char *)0xfc020000,   0x65753c, 0xe1d9034a }, // ROMCODE
+    { (const char *)0xfc677774,    0x51430, 0xa8ab9a14 }, // omar_0, omar_1
+    { (const char *)0xfcc91490,   0x122c58, 0x3d3bd74f }, // zico_0, zico_1, zico_2
+    { (const char *)0xfceacb1c,    0x57235, 0x8816c760 }, // RAM data, RAM code, TCM code
+};
+
 firmware_crc_sub_t firmware_crc_list[]={
     { "GM1.01D", firmware_101d_crc32 },
     { "GM1.01E", firmware_101e_crc32 },
+    { "GM1.10A", firmware_110a_crc32 },
 };
 
 const firmware_crc_desc_t firmware_crc_desc={
     (const char *)0xfc454739, // firmware_ver_ptr
     firmware_crc_list,
-    2,  // sub_count
+    3,  // sub_count
     4,  // block_count
 };
--- platform/ixus1000_sd4500/sub/100d/firmware_crc_data.h
+++ platform/ixus1000_sd4500/sub/100d/firmware_crc_data.h
@@ -5,13 +5,19 @@ const firmware_crc_block_t firmware_100d_crc32[]={
     { (const char *)0xffc56cc8,     0xee20, 0x2aeeedb8 }, // RAM data
 };
 
+const firmware_crc_block_t firmware_100b_crc32[]={
+    { (const char *)0xff810000,   0x37bd6a, 0xd456be6c }, // ROMCODE
+    { (const char *)0xffc56cc8,     0xee20, 0xd06f011d }, // RAM data
+};
+
 firmware_crc_sub_t firmware_crc_list[]={
     { "GM1.00D", firmware_100d_crc32 },
+    { "GM1.00B", firmware_100b_crc32 },
 };
 
 const firmware_crc_desc_t firmware_crc_desc={
     (const char *)0xff894e35, // firmware_ver_ptr
     firmware_crc_list,
-    1,  // sub_count
+    2,  // sub_count
     2,  // block_count
 };
