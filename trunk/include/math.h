a486b67802674dd955b13c5e80712979dcc6429b
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2020-12-11 21:31:30 +0000

    Remove very old lib/math library that is no longer needed.

--- trunk/include/math.h
+++ trunk/include/math.h
@@ -6,20 +6,6 @@
 // Note: used in modules and platform independent code. 
 // Do not add platform dependent stuff in here (#ifdef/#endif compile options or camera dependent values)
 
-#ifndef __VFP_FP__
-extern double d2d(double value);
-#else
-#define d2d(v) (v)
-#endif
-/*
-extern float logf(float v);
-extern float sqrtf(float v);
-extern float powf(float x, float y);
-extern float fabsf(float x);
-extern float scalbnf(float x, int n);
-extern float copysignf(float x, float y);
-*/
-
 #define M_PI		3.14159265358979323846
 
 extern double log(double x);
