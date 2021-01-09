4dd495d8ff2eccdb57be8058f572ccf13340b584
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2021-01-09 05:39:33 +0000

    ixusizoom_sd30 - add firmware 100e as a copy of 100g, thanks to Caefix in https://chdk.setepontos.com/index.php?topic=2724.msg144938#msg144938

--- platform/ixusizoom_sd30/sub/100g/firmware_crc_data.h
+++ platform/ixusizoom_sd30/sub/100g/firmware_crc_data.h
@@ -4,18 +4,23 @@ const firmware_crc_block_t firmware_100g_crc32[]={
     { (const char *)0xff810000,   0x2e66b0, 0x6e2432e4 }, // ROMCODE, RAM data
 };
 
+const firmware_crc_block_t firmware_100e_crc32[]={
+    { (const char *)0xff810000,   0x2e66b0, 0x120dcdb8 }, // ROMCODE, RAM data
+};
+
 const firmware_crc_block_t firmware_100f_crc32[]={
     { (const char *)0xff810000,   0x2e66b0, 0x9598b6f0 }, // ROMCODE, RAM data
 };
 
 firmware_crc_sub_t firmware_crc_list[]={
     { "GM1.00G", firmware_100g_crc32 },
+    { "GM1.00E", firmware_100e_crc32 },
     { "GM1.00F", firmware_100f_crc32 },
 };
 
 const firmware_crc_desc_t firmware_crc_desc={
     (const char *)0xff81b27d, // firmware_ver_ptr
     firmware_crc_list,
-    2,  // sub_count
+    3,  // sub_count
     1,  // block_count
 };
