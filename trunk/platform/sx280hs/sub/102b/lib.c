8a98d4532b4b48a772a5a4282a945ad4348ba269
srsa <srsa@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2021-04-08 21:04:47 +0000

    sx280: add support for recording GPS data in DNGs

--- trunk/platform/sx280hs/sub/102b/lib.c
+++ trunk/platform/sx280hs/sub/102b/lib.c
@@ -1,21 +1,17 @@
 #include "platform.h"
+#include "gps.h"
 
 char *camera_jpeg_current_filename() {
-     return (void*)0xD922C;                 //TODO: incorrect
+     return (void*)0xE6A18;
 }
 
 char *camera_jpeg_count_str()    { return (char*)0x000e4308; }
 
-#ifdef CAM_HAS_GPS // TODO
-char * camera_jpeg_current_latitude() {     //asm1989 way to calculate it is camera_jpeg_current_filename  + 0x78
-    return (void*)0xD92A4;
-}
-
-char * camera_jpeg_current_longitude() {    //asm1989 way to calculate it is camera_jpeg_current_filename  + 0x94
-    return (void*)0xD92C0;
-}
-
-char * camera_jpeg_current_height() {
-    return (void*)0xD909C;
+#ifdef CAM_HAS_GPS
+char *camera_jpeg_current_gps()
+{
+    static gps_img_data gid;
+    // a compatible fw struct has not been found yet, returning empty struct
+    return (void*)&gid;
 }
 #endif
