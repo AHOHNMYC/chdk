c73a516609d3f8baea136a2f9c109be74322bf84
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2021-05-05 04:31:37 +0000

    ixus240_elph320hs - update viewport buffer functions based on report by Caefix in https://chdk.setepontos.com/index.php?topic=9005.msg145837#msg145837, do not attempt to set CHDK custom colors if palette is NULL (note, other possible palette issues not fixed)

--- trunk/platform/ixus240_elph320hs/lib.c
+++ trunk/platform/ixus240_elph320hs/lib.c
@@ -82,21 +82,40 @@ void camera_set_led(int led, int state, __attribute__ ((unused))int bright) {
 extern char active_viewport_buffer;
 extern void* viewport_buffers[];
 
+// Y multiplier for cameras with 480 pixel high viewports (CHDK code assumes 240)
+// camera appears to use 240 when recording FHD video and Fisheye mode, 480 otherwise
+int vid_get_viewport_yscale() {
+    if (camera_info.state.mode_play) {
+        return 2;
+    }
+    if (is_video_recording()) {
+        // FHD video
+        if(shooting_get_prop(PROPCASE_VIDEO_RESOLUTION) == 5) {
+            return 1;
+        }
+        return 2;
+    }
+    if (camera_info.state.mode_shooting == MODE_FISHEYE) {
+        return 1;
+    }
+    return 2;
+}
+
 void *vid_get_viewport_fb()
 {
     // Return first viewport buffer - for case when vid_get_viewport_live_fb not defined
-    // Offset the return value because the viewport is left justified instead of centered on this camera
     return viewport_buffers[0];
 }
 
 void *vid_get_viewport_live_fb()
 {
-    if (MODE_IS_VIDEO(mode_get()) || (get_movie_status()==VIDEO_RECORD_IN_PROGRESS))
-        return viewport_buffers[0];     // Video only seems to use the first viewport buffer.
+    // only 4 buffers used while video recording
+    if (camera_info.state.mode_video || is_video_recording())
+        return viewport_buffers[(active_viewport_buffer-1)&3];
 
     // Hopefully return the most recently used viewport buffer so that motion detect, histogram, zebra and edge overly are using current image data
     // Offset the return value because the viewport is left justified instead of centered on this camera
-    return viewport_buffers[(active_viewport_buffer-1)&3];
+    return viewport_buffers[(active_viewport_buffer-1)&7];
 }
 
 void *vid_get_viewport_fb_d()
@@ -117,7 +136,7 @@ void *vid_get_bitmap_fb()
 
 int vid_get_viewport_width()
 {
-    if ((mode_get() & MODE_MASK) == MODE_PLAY)
+    if (camera_info.state.mode_play)
     {
         return 480;
     }
@@ -125,17 +144,47 @@ int vid_get_viewport_width()
     return _GetVRAMHPixelsSize() >> 1;
 }
 
-long vid_get_viewport_height(){ return 240; }
+
+long vid_get_viewport_height()
+{
+    if(camera_info.state.mode_play) {
+        return 240;
+    }
+    extern int _GetVRAMVPixelsSize();
+    // FHD video is apparently 180 on a 240 display
+    int h = _GetVRAMVPixelsSize();
+    if (vid_get_viewport_yscale() == 1) {
+        return h;
+    }
+
+    return h>>1;
+}
+
+int vid_get_viewport_display_xoffset()
+{
+    if (camera_info.state.mode_play)
+    {
+        return 0;
+    }
+    else
+    {
+        // viewport width offset table for each image size
+        // 0 = 4:3, 1 = 16:9, 2 = 3:2, 3 = 1:1
+        static long vp_w[5] = { 60, 0, 36, 104 };                // should all be even values for edge overlay
+        return vp_w[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
+    }
+}
 
 // Functions for PTP Live View system
 
 int vid_get_viewport_display_xoffset_proper()   { return vid_get_viewport_display_xoffset() * 2; }
-int vid_get_viewport_height_proper()            { return 480; }
+int vid_get_viewport_height_proper()            { return vid_get_viewport_height()*vid_get_viewport_yscale(); }
 int vid_get_viewport_buffer_width_proper()      { return 960; }
-int vid_get_viewport_fullscreen_height()        { return 480; }
+int vid_get_viewport_fullscreen_height()        { return 240*vid_get_viewport_yscale(); }
+
 int vid_get_palette_type()                      { return 3; }
 int vid_get_palette_size()                      { return 256 * 4; }
-int vid_get_aspect_ratio()                      { return 1; }
+int vid_get_aspect_ratio()                      { return 1; } /* LV_ASPECT_16_9 */
 
 void *vid_get_bitmap_active_buffer()
 {
@@ -146,7 +195,12 @@ void *vid_get_bitmap_active_palette()
 {
     extern int active_palette_buffer;
     extern char* palette_buffer[];
-    return (palette_buffer[active_palette_buffer]+4);
+    char *p = palette_buffer[active_palette_buffer];
+    // if null, don't add +4
+    if(!p) {
+        return NULL;
+    }
+    return (p+4);
 }
 
 #ifdef CAM_LOAD_CUSTOM_COLORS
@@ -155,29 +209,33 @@ void load_chdk_palette()
 {
     extern int active_palette_buffer;
     // Only load for the standard record and playback palettes
-    //rec=0, set menu=4, play=5, menu=6
+    // on most cams rec=0, set menu=4, play=5, menu=6
+    // but per https://chdk.setepontos.com/index.php?topic=9005.msg145877#msg145877
+    // playback may actually be 16, menu 12
     if ((active_palette_buffer == 0) || (active_palette_buffer == 5))
     {
         int *pal = (int*)vid_get_bitmap_active_palette();
-        if (pal[CHDK_COLOR_BASE+0] != 0x33ADF62)
-        {
-            pal[CHDK_COLOR_BASE+0]  = 0x33ADF62;  // Red
-            pal[CHDK_COLOR_BASE+1]  = 0x326EA40;  // Dark Red
-            pal[CHDK_COLOR_BASE+2]  = 0x34CD57F;  // Light Red
-            pal[CHDK_COLOR_BASE+3]  = 0x373BFAE;  // Green
-            pal[CHDK_COLOR_BASE+4]  = 0x34BD6CA;  // Dark Green
-            pal[CHDK_COLOR_BASE+5]  = 0x395AB95;  // Light Green
-            pal[CHDK_COLOR_BASE+6]  = 0x34766F0;  // Blue
-            pal[CHDK_COLOR_BASE+7]  = 0x31250F3;  // Dark Blue
-            pal[CHDK_COLOR_BASE+8]  = 0x37F408F;  // Cyan
-            pal[CHDK_COLOR_BASE+9]  = 0x3512D5B;  // Magenta
-            pal[CHDK_COLOR_BASE+10] = 0x3A9A917;  // Yellow
-            pal[CHDK_COLOR_BASE+11] = 0x3819137;  // Dark Yellow
-            pal[CHDK_COLOR_BASE+12] = 0x3DED115;  // Light Yellow
+        if(pal) {
+            if (pal[CHDK_COLOR_BASE+0] != 0x33ADF62)
+            {
+                pal[CHDK_COLOR_BASE+0]  = 0x33ADF62;  // Red
+                pal[CHDK_COLOR_BASE+1]  = 0x326EA40;  // Dark Red
+                pal[CHDK_COLOR_BASE+2]  = 0x34CD57F;  // Light Red
+                pal[CHDK_COLOR_BASE+3]  = 0x373BFAE;  // Green
+                pal[CHDK_COLOR_BASE+4]  = 0x34BD6CA;  // Dark Green
+                pal[CHDK_COLOR_BASE+5]  = 0x395AB95;  // Light Green
+                pal[CHDK_COLOR_BASE+6]  = 0x34766F0;  // Blue
+                pal[CHDK_COLOR_BASE+7]  = 0x31250F3;  // Dark Blue
+                pal[CHDK_COLOR_BASE+8]  = 0x37F408F;  // Cyan
+                pal[CHDK_COLOR_BASE+9]  = 0x3512D5B;  // Magenta
+                pal[CHDK_COLOR_BASE+10] = 0x3A9A917;  // Yellow
+                pal[CHDK_COLOR_BASE+11] = 0x3819137;  // Dark Yellow
+                pal[CHDK_COLOR_BASE+12] = 0x3DED115;  // Light Yellow
 
-            extern char palette_control;
-            palette_control = 1;
-            vid_bitmap_refresh();
+                extern char palette_control;
+                palette_control = 1;
+                vid_bitmap_refresh();
+            }
         }
     }
 }
