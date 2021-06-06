8d195ae36070f59526253daf20752c3848c46cc3
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2021-06-06 22:46:46 +0000

    ghidra datatypes - add some ximr structs and functions

--- trunk/tools/ghidra_scripts/datatypes/fw_types.h
+++ trunk/tools/ghidra_scripts/datatypes/fw_types.h
@@ -181,3 +181,54 @@ struct controller_s {
 };
 #endif
 
+// dry52 may be different per https://chdk.setepontos.com/index.php?topic=12788.msg145589#msg145589
+#if defined(CAM_DRYOS_REL) && CAM_DRYOS_REL > 52
+// Ximr layer
+typedef struct {
+    unsigned char   unk1[7];
+    unsigned char   scale;
+    unsigned int    unk2;
+    unsigned short  color_type;
+    unsigned short  visibility;
+    unsigned short  unk3;
+    unsigned short  src_y;
+    unsigned short  src_x;
+    unsigned short  src_h;
+    unsigned short  src_w;
+    unsigned short  dst_y;
+    unsigned short  dst_x;
+    unsigned short  enabled;
+    unsigned int    marv_sig;
+    unsigned int    bitmap;
+    unsigned int    opacity;
+    unsigned int    color;
+    unsigned int    width;
+    unsigned int    height;
+    unsigned int    unk4;
+} ximr_layer;
+
+// Ximr context
+typedef struct {
+    unsigned short  unk1;
+    unsigned short  width1;
+    unsigned short  height1;
+    unsigned short  unk2[17];
+    unsigned int    output_marv_sig;
+    unsigned int    output_buf;
+    unsigned int    output_opacitybuf;
+    unsigned int    output_color;
+    int             buffer_width;
+    int             buffer_height;
+    unsigned int    unk3[2];
+    ximr_layer      layers[8];
+    unsigned int    unk4[24];
+    unsigned char   denomx;
+    unsigned char   numerx;
+    unsigned char   denomy;
+    unsigned char   numery;
+    unsigned int    unk5;
+    short           width;
+    short           height;
+    unsigned int    unk6[27];
+} ximr_context;
+#endif // CAM_DRYOS_REL
