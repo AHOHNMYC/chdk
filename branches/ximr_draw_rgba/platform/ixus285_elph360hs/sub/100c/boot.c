359ebd9bcdc0ec44a2c8cdbc5d67b791aae6207f
philmoz <philmoz@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2022-09-14 01:57:11 +0000

    Merge trunk changes.

--- branches/ximr_draw_rgba/platform/ixus285_elph360hs/sub/100c/boot.c
+++ branches/ximr_draw_rgba/platform/ixus285_elph360hs/sub/100c/boot.c
@@ -263,7 +263,7 @@ asm volatile (
 "    STR     R0, [R1] \n"
 "    MOV     R1, #0x78 \n"
 "    ADD     R0, SP, #4 \n"
-"    BL      sub_006DC0D0 \n"
+"    BL      sub_006DC0D0 /*_bzero*/ \n"
 "    MOV     R0, #0xA0000 \n"
 "    STR     R0, [SP, #8] \n"
 
